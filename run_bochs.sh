157ff4271b8e573129899fac4015d6927b2d85f5
MRNIU <a6813140@hotmail.com>
2018-04-04 22:35:34 +0800

    This script will setup a loopback device, run bochs on it, then disconnect it.

---
+++ run_bochs.sh
@@ -0,0 +1,13 @@
+# Copy from JamesM's kernel development tutorials: 
+# (http://www.jamesmolloy.co.uk/tutorial_html/1.-Environment%20setup.html). 
+
+# This file is apart in MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel). 
+
+#!/bin/bash
+
+# run_bochs.sh
+# mounts the correct loopback device, runs bochs, then unmounts.
+
+sudo /sbin/losetup /dev/loop0 floppy.img
+sudo bochs -f bochsrc.txt
+sudo /sbin/losetup -d /dev/loop0
