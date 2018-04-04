2bb6dd96cdc1fc47646b9cfe799f42dd07511fbc
MRNIU <a6813140@hotmail.com>
2018-04-04 22:37:27 +0800

    Poke new kernel binary into the floppy image file.

---
+++ update_image.sh
@@ -0,0 +1,13 @@
+# Copy from JamesM's kernel development tutorials: 
+# (http://www.jamesmolloy.co.uk/tutorial_html/1.-Environment%20setup.html). 
+
+# This file is apart in MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel). 
+
+
+#!/bin/bash
+
+sudo losetup /dev/loop0 floppy.img
+sudo mount /dev/loop0 /mnt
+sudo cp src/kernel /mnt/kernel
+sudo umount /dev/loop0
+sudo losetup -d /dev/loop0
