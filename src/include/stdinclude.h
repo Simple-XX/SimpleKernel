483be3abc1f7a8f706dbfa3d92981d109c8d2a59
MRNIU <a6813140@hotmail.com>
2018-05-31 18:03:54 +0800

    撤销之前的操作，添加 TODO

---
+++ src/include/stdinclude.h
@@ -0,0 +1,15 @@
+
+// This file is apart of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
+
+// stdinclude.h for MRNIU/SimpleKernel.
+
+#ifndef _STDINCLUDE_H
+#define _STDINCLUDE_H
+
+
+#include "stdarg.h"
+#include "stddef.h"
+#include "stdint.h"
+
+
+#endif
