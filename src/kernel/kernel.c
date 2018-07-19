d0b63ddd9f5ebecfcf7f2db2b4764b35e23192df
MRNIU <a6813140@hotmail.com>
2018-07-20 00:22:21 +0800

    Update

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -6,9 +6,10 @@
 #include "headers.h"
 
 // 内核入口
-void kernel_main(void)
+void kernel_main(multiboot_t *mboot_ptr)
 {
 	// 系统初始化
+	debug_init();
 	terminal_init();	// 控制台初始化
 	gdt_init();	// GDT 初始化
 	idt_init();	// IDT 初始化
@@ -20,11 +21,9 @@ void kernel_main(void)
 	printk("terminal_color: %d\n", terminal_color);
 	printk("current cursor pos: %d\n", terminal_getcursorpos());
 
-	asm("int $0x4");
-	asm("int $0x13");
+	//print_cur_status();
+	//print_stack_trace();
 
-
-	printk("int 0x03 successful!");
 
 	return;
 }
