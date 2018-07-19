fbaa2340d875d2b790e8d9341a213f2bfaab349c
MRNIU <a6813140@hotmail.com>
2018-07-20 00:18:08 +0800

    为 debug 服务的头文件，包括 elf 结构和 multiboot 结构

---
+++ src/include/debug.h
@@ -0,0 +1,22 @@
+
+// This file is a part of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
+// Based on http://wiki.0xffffff.org/posts/hurlex-5.html
+// debug.h for MRNIU/SimpleKernel.
+
+
+#ifndef _DEBUG_H_
+#define _DEBUG_H_
+
+#include "stdarg.h"
+#include "stddef.h"
+#include "stdio.h"
+#include "string.h"
+#include "elf.h"
+#include "multiboot.h"
+
+
+void debug_init(void);
+
+
+
+#endif
