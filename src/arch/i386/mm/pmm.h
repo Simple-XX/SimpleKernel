4c8ee4913bd02e57b2da4078c0452f57ae1263de
MRNIU <a6813140@hotmail.com>
2018-07-20 00:21:12 +0800

    内存管理，虚拟内存与物理内存，目前仅为 debug 服务

---
+++ src/arch/i386/mm/pmm.h
@@ -0,0 +1,13 @@
+
+// This file is a part of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
+
+// pmm.h for MRNIU/SimpleKernel.
+
+
+#ifndef _PMM_H_
+#define _PMM_H_
+
+void show_memory_map();
+
+
+#endif
---
+++ src/arch/i386/mm/vmm.c
@@ -0,0 +1,4 @@
+
+// This file is a part of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
+
+// vmm.c for MRNIU/SimpleKernel.
