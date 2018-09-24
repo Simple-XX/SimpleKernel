7eda75bc9bce60e8ad20fb01e4f0d5e51ab2ca27
MRNIU <a6813140@hotmail.com>
2018-09-24 23:06:32 +0800

    正在解决 GP 错误，脑壳疼

--- src/kernel/drv/kb.c
+++ src/kernel/drv/kb.c
@@ -8,75 +8,41 @@
 #include "port.hpp"
 
 
-static unsigned char keyboard_map[128] =
-{
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
-  '9', '0', '-', '=', '\b',	/* Backspace */
-  '\t',			/* Tab */
-  'q', 'w', 'e', 'r',	/* 19 */
-  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
-    0,			/* 29   - Control */
-  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
- '\'', '`',   0,		/* Left shift */
- '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
-  'm', ',', '.', '/',   0,				/* Right shift */
-  '*',
-    0,	/* Alt */
-  ' ',	/* Space bar */
-    0,	/* Caps lock */
-    0,	/* 59 - F1 key ... > */
-    0,   0,   0,   0,   0,   0,   0,   0,
-    0,	/* < ... F10 */
-    0,	/* 69 - Num lock*/
-    0,	/* Scroll Lock */
-    0,	/* Home key */
-    0,	/* Up Arrow */
-    0,	/* Page Up */
-  '-',
-    0,	/* Left Arrow */
-    0,
-    0,	/* Right Arrow */
-  '+',
-    0,	/* 79 - End key*/
-    0,	/* Down Arrow */
-    0,	/* Page Down */
-    0,	/* Insert Key */
-    0,	/* Delete Key */
-    0,   0,   0,
-    0,	/* F11 Key */
-    0,	/* F12 Key */
-    0,	/* All other keys are undefined */
-};
+const char *sc_name[] = { "ERROR", "Esc", "1", "2", "3", "4", "5", "6",
+    "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Q", "W", "E",
+        "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter", "Lctrl",
+        "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`",
+        "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".",
+        "/", "RShift", "Keypad *", "LAlt", "Spacebar"};
+const char sc_ascii[] = { '?', '?', '1', '2', '3', '4', '5', '6',
+    '7', '8', '9', '0', '-', '=', '?', '?', 'Q', 'W', 'E', 'R', 'T', 'Y',
+        'U', 'I', 'O', 'P', '[', ']', '?', '?', 'A', 'S', 'D', 'F', 'G',
+        'H', 'J', 'K', 'L', ';', '\'', '`', '?', '\\', 'Z', 'X', 'C', 'V',
+        'B', 'N', 'M', ',', '.', '/', '?', '?', '?', ' '};
 
+#define SC_MAX 57
+static char key_buffer[256];
+void keyboard_handler(pt_regs_t * regs){
+  uint8_t scancode = inb(KB_DATA);
+  if(scancode>SC_MAX)
+    return;
+  if(scancode == KB_BACKSPACE)
+    backspace(key_buffer);
+  else if(scancode == KB_ENTER){
+    printk("\n");
+    key_buffer[0] = '\0';
+  } else{
+    char letter = sc_ascii[(int)scancode];
+    char str[2]={letter,'\0'};
+    append(key_buffer, letter);
+    printk("%s\n", str);
+  }
+  UNUSED(regs);
+}
 
 void keyboard_init(void){
   printk("kb init\n");
 	/* 0xFD is 11111101 - enables only IRQ1 (keyboard)*/
 	outb(0x21 , 0xFD);
   register_interrupt_handler(IRQ1, &keyboard_handler);
-}
-
-void keyboard_handler(pt_regs_t * regs){
-  unsigned char status;
-	char keycode;
-	/* write EOI */
-	outb(0x20, 0x20);
-	status = inb(KB_STATUS);
-	/* Lowest bit of status will be set if buffer is not empty */
-  if(status!=0){
-    printk("%d", status);
-  }
-	if (status & 0x01) {
-    keycode = inb(KB_DATA);
-		if(keycode < 0)
-      printk("eooreddsd");
-			return;
-		if(keycode == KB_ENTER) {
-      printk("\n");
-			return;
-		}
-    printk("%c", keyboard_map[(unsigned char) keycode]);
-		//vidptr[current_loc++] = 0x07;
-	}
-  UNUSED(regs);
 }
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -4,7 +4,7 @@
 // kernel.c for MRNIU/SimpleKernel.
 
 #include "kernel.h"
-
+#include "assert.h"
 
 // 内核入口
 void kernel_main()
@@ -14,10 +14,9 @@ void kernel_main()
 	terminal_init();	// 控制台初始化
 	gdt_init();	// GDT 初始化
 	idt_init();	// IDT 初始化
-	clock_init();	// 时钟初始化
-	keyboard_init(); // 键盘初始化
+	//clock_init();	// 时钟初始化
+	//keyboard_init(); // 键盘初始化
 	showinfo();
-	//asm("int $1");
 
 	while(1);
 
