01934e02034b5ddd9a1a4ab5ff65a07a8763e20a
MRNIU <a6813140@hotmail.com>
2018-12-19 22:48:19 +0800

    Try to mm. 这里写起来有点吃力，先去翻翻书再来

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -6,6 +6,8 @@
 #include "kernel.h"
 #include "assert.h"
 
+extern void tmp();
+
 // 内核入口
 void kernel_main(void);
 void kernel_main(){
@@ -14,11 +16,10 @@ void kernel_main(){
 		terminal_init();       // 控制台初始化
 		gdt_init();       // GDT 初始化
 		idt_init();       // IDT 初始化
+
 		clock_init();       // 时钟初始化
 		keyboard_init();       // 键盘初始化
 		showinfo();
-
-		// init_A20();
 
 
 		while(1);
