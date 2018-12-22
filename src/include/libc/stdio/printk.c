bbfd8d8025cae4ff81bc5acbc480da7f78a7c4e4
MRNIU <a6813140@hotmail.com>
2018-12-22 14:29:07 +0800

    Try to improve boot code

--- src/include/libc/stdio/printk.c
+++ src/include/libc/stdio/printk.c
@@ -18,27 +18,91 @@ extern void terminal_setcolor(unsigned char color);
 extern unsigned char terminal_getcolor(void);
 
 int printk(const char * fmt, ...){
-  va_list args;
-  int i;
-  va_start(args, fmt);
-  i=vsprintf(buf, fmt, args);
-  va_end(args);
-  terminal_writestring(buf);
-  return i;
+		va_list args;
+		int i;
+		va_start(args, fmt);
+		i=vsprintf(buf, fmt, args);
+		va_end(args);
+		terminal_writestring(buf);
+		return i;
 }
 
 int printk_color(unsigned char color, const char *fmt, ...){
-  va_list args;
-  int i;
-  va_start(args, fmt);
-  i=vsprintf(buf, fmt, args);
-  va_end(args);
-  unsigned char old_color = terminal_getcolor();
-  terminal_setcolor(color);
-  terminal_writestring(buf);
-  terminal_setcolor(old_color);
-  return i;
+		va_list args;
+		int i;
+		va_start(args, fmt);
+		i=vsprintf(buf, fmt, args);
+		va_end(args);
+		unsigned char old_color = terminal_getcolor();
+		terminal_setcolor(color);
+		terminal_writestring(buf);
+		terminal_setcolor(old_color);
+		return i;
 }
+
+// /* Format a string and print it on the screen, just like the libc
+//    function printf. */
+// void
+// printf (const char *format, ...)
+// {
+//   char **arg = (char **) &format;
+//   int c;
+//   char buf[20];
+//
+//   arg++;
+//
+//   while ((c = *format++) != 0)
+//   {
+//     if (c != '%')
+//       putchar (c);
+//     else
+//     {
+//       char *p, *p2;
+//       int pad0 = 0, pad = 0;
+//
+//       c = *format++;
+//       if (c == '0')
+//       {
+//         pad0 = 1;
+//         c = *format++;
+//       }
+//
+//       if (c >= '0' && c <= '9')
+//       {
+//         pad = c - '0';
+//         c = *format++;
+//       }
+//
+//       switch (c)
+//       {
+//       case 'd':
+//       case 'u':
+//       case 'x':
+//         itoa (buf, c, *((int *) arg++));
+//         p = buf;
+//         goto string;
+//         break;
+//
+//       case 's':
+//         p = *arg++;
+//         if (!p)
+//           p = "(null)";
+//
+// string:
+//         for (p2 = p; *p2; p2++);
+//         for (; p2 < p + pad; p2++)
+//           putchar (pad0 ? '0' : ' ');
+//         while (*p)
+//           putchar (*p++);
+//         break;
+//
+//       default:
+//         putchar (*((int *) arg++));
+//         break;
+//       }
+//     }
+//   }
+// }
 
 
 #endif
