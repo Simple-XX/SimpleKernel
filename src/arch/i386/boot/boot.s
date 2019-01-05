7cefbd9e9f7dd763ad066bc4caa2b7739a9463be
MRNIU <a6813140@hotmail.com>
2019-01-06 07:23:54 +0800

    Still wirking on multiboot2... Maybe I have a stuck here. I have check boot.s and debug.carefully, but it never work as expected. I feel down...

--- src/arch/i386/boot/boot.s
+++ src/arch/i386/boot/boot.s
@@ -2,27 +2,72 @@
 # This file is a part of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
 # boot.s for MRNIU/SimpleKernel.
 
+# multiboot2 定义
+.set  MULTIBOOT_SEARCH,                        32768
+.set  MULTIBOOT_HEADER_ALIGN,                  8
+.set  MULTIBOOT2_HEADER_MAGIC,                 0xe85250d6
+.set  MULTIBOOT2_BOOTLOADER_MAGIC,             0x36d76289
+.set  MULTIBOOT_MOD_ALIGN,                     0x00001000
+.set  MULTIBOOT_INFO_ALIGN,                    0x00000008
+
+.set  MULTIBOOT_TAG_ALIGN,                  8
+.set  MULTIBOOT_TAG_TYPE_END,               0
+.set  MULTIBOOT_TAG_TYPE_CMDLINE,           1
+.set  MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME,  2
+.set  MULTIBOOT_TAG_TYPE_MODULE,            3
+.set  MULTIBOOT_TAG_TYPE_BASIC_MEMINFO,     4
+.set  MULTIBOOT_TAG_TYPE_BOOTDEV,           5
+.set  MULTIBOOT_TAG_TYPE_MMAP,              6
+.set  MULTIBOOT_TAG_TYPE_VBE,               7
+.set  MULTIBOOT_TAG_TYPE_FRAMEBUFFER,       8
+.set  MULTIBOOT_TAG_TYPE_ELF_SECTIONS,      9
+.set  MULTIBOOT_TAG_TYPE_APM,               10
+.set  MULTIBOOT_TAG_TYPE_EFI32,             11
+.set  MULTIBOOT_TAG_TYPE_EFI64,             12
+.set  MULTIBOOT_TAG_TYPE_SMBIOS,            13
+.set  MULTIBOOT_TAG_TYPE_ACPI_OLD,          14
+.set  MULTIBOOT_TAG_TYPE_ACPI_NEW,          15
+.set  MULTIBOOT_TAG_TYPE_NETWORK,           16
+.set  MULTIBOOT_TAG_TYPE_EFI_MMAP,          17
+.set  MULTIBOOT_TAG_TYPE_EFI_BS,            18
+.set  MULTIBOOT_TAG_TYPE_EFI32_IH,          19
+.set  MULTIBOOT_TAG_TYPE_EFI64_IH,          20
+.set  MULTIBOOT_TAG_TYPE_LOAD_BASE_ADDR,    21
+
+.set  MULTIBOOT_HEADER_TAG_END,  0
+.set  MULTIBOOT_HEADER_TAG_INFORMATION_REQUEST,  1
+.set  MULTIBOOT_HEADER_TAG_ADDRESS,  2
+.set  MULTIBOOT_HEADER_TAG_ENTRY_ADDRESS,  3
+.set  MULTIBOOT_HEADER_TAG_CONSOLE_FLAGS,  4
+.set  MULTIBOOT_HEADER_TAG_FRAMEBUFFER,  5
+.set  MULTIBOOT_HEADER_TAG_MODULE_ALIGN,  6
+.set  MULTIBOOT_HEADER_TAG_EFI_BS,        7
+.set  MULTIBOOT_HEADER_TAG_ENTRY_ADDRESS_EFI32,  8
+.set  MULTIBOOT_HEADER_TAG_ENTRY_ADDRESS_EFI64,  9
+.set  MULTIBOOT_HEADER_TAG_RELOCATABLE,  10
+
+.set  MULTIBOOT_ARCHITECTURE_I386,  0
+.set  MULTIBOOT_ARCHITECTURE_MIPS32,  4
+.set  MULTIBOOT_HEADER_TAG_OPTIONAL, 1
+
+.set  MULTIBOOT_LOAD_PREFERENCE_NONE, 0
+.set  MULTIBOOT_LOAD_PREFERENCE_LOW, 1
+.set  MULTIBOOT_LOAD_PREFERENCE_HIGH, 2
+
+.set  MULTIBOOT_CONSOLE_FLAGS_CONSOLE_REQUIRED, 1
+.set  MULTIBOOT_CONSOLE_FLAGS_EGA_TEXT_SUPPORTED, 2
+
 # 设置栈大小
 .set STACK_SIZE, 0x4000
-# multiboot2 魔数字
-.set MULTIBOOT2_HEADER_MAGIC, 0xE85250D6
-# i386 架构标识
-.set MULTIBOOT_ARCHITECTURE_I386, 0
-# multiboot2 头结束标志
-.set MULTIBOOT_HEADER_TAG_END, 0
-
-.set MULTIBOOT_TAG_TYPE_ELF_SECTIONS, 9
-.set MULTIBOOT_HEADER_TAG_OPTIONAL, 1
-.set MULTIBOOT_HEADER_TAG_ADDRESS, 2
-.set MULTIBOOT_HEADER_TAG_ENTRY_ADDRESS, 3
-.set MULTIBOOT_HEADER_TAG_FRAMEBUFFER,5
 
 .text
-.align 8
-.global start
-.global multiboot2_info
-.global multiboot2_magic
+.global start, _start
 .extern kernel_main
+
+start:
+_start:
+  jmp multiboot_entry
+
 
 # multiboot2 文件头
 .align 8
@@ -32,55 +77,51 @@ multiboot_header:
   .long multiboot_header_end - multiboot_header
   .long -(MULTIBOOT2_HEADER_MAGIC + MULTIBOOT_ARCHITECTURE_I386 + multiboot_header_end - multiboot_header)
 
-# 添加其它内容在此，见 Multiboot2 Specification version 2.0.pdf
+# 添加其它内容在此，详细信息见 Multiboot2 Specification version 2.0.pdf
 
-/*
-+-------------------+
-u32     | type = 9          |
-u32     | size              |
-u32     | num               |
-u32     | entsize           |
-u32     | shndx             |
-u32     | reserved          |
-varies  | section headers   |
-		 +-------------------+
-*/
-/*
-# 这里原文有错误，见 https://github.com/rust-osdev/multiboot2-elf64/blob/master/README.md
+# multiboot2 information request
 .align 8
-elf_symbols_tag:
-	.long MULTIBOOT_TAG_TYPE_ELF_SECTIONS
-	.long elf_symbols_tag_end - elf_symbols_tag
-	.long
-	.long
-	.long
-	.long
-
+mbi_tag_start:
+  .short MULTIBOOT_HEADER_TAG_INFORMATION_REQUEST
+  .short MULTIBOOT_HEADER_TAG_OPTIONAL
+  .long mbi_tag_end - mbi_tag_start
+  .long MULTIBOOT_TAG_TYPE_CMDLINE
+  .long MULTIBOOT_TAG_TYPE_MODULE
+  .long MULTIBOOT_TAG_TYPE_BOOTDEV
+  .long MULTIBOOT_TAG_TYPE_MMAP
+  .long MULTIBOOT_TAG_TYPE_ELF_SECTIONS
+  .long MULTIBOOT_TAG_TYPE_APM
 .align 8
-elf_symbols_tag_end:
-*/
-
-
-
-.align 8
+mbi_tag_end:
 	.short MULTIBOOT_HEADER_TAG_END
   .short 0
   .long 8
-.align 8
 multiboot_header_end:
 
-#
-.align 8
-start:
-.align 8
 multiboot_entry:
 	# 设置栈地址
-  mov $(stack + STACK_SIZE), %esp
-  push $0
+  mov $stack_top, %esp
+  pushl $0
   popf
 	# multiboot2_info 结构体指针
-  push %ebx
+  pushl %ebx
 	# 魔数
-	push %eax
+	pushl %eax
   call kernel_main
-.comm   stack, STACK_SIZE
+  cli
+1:
+  hlt
+  jmp 1b
+  ret
+
+.size _start, . - _start
+
+.section .bss
+
+.align 8
+stack_bottom:
+  .skip STACK_SIZE
+stack_top:
+
+edata:
+end:
--- src/arch/i386/debug/debug.c
+++ src/arch/i386/debug/debug.c
@@ -25,224 +25,95 @@ void print_cur_status(){
 		++round;
 }
 
-
-/*  kernel.c - the C part of the kernel */
-/*  Copyright (C) 1999, 2010  Free Software Foundation, Inc.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-
-/*  Macros. */
-
-/*  Some screen stuff. */
-/*  The number of columns. */
-#define COLUMNS                 80
-/*  The number of lines. */
-#define LINES                   24
-/*  The attribute of an character. */
-#define ATTRIBUTE               7
-/*  The video memory address. */
-#define VIDEO                   0xB8000
-
-/*  Variables. */
-/*  Save the X position. */
-static int xpos;
-/*  Save the Y position. */
-static int ypos;
-/*  Point to the video memory. */
-static volatile unsigned char *video;
-
 /*  Forward declarations. */
-void debug233 (unsigned long magic, unsigned long addr);
+void debug233 (uint64_t magic, uint64_t addr);
 
 /*  Check if MAGIC is valid and print the Multiboot information structure
    pointed by ADDR. */
-void
-debug233 (unsigned long magic, unsigned long addr)
-{
+
+void debug233 (uint64_t magic, uint64_t addr){
 		struct multiboot_tag *tag;
-		unsigned size;
-
-		printk("debug233\n");
+		unsigned long size;
+		size = *(unsigned long *) addr;
+		uint32_t tmp233=addr;
+		printk ("addr: %X\n", addr);
+		printk ("size: %X\n", size);
+		printk ("tmp233: %X\n", tmp233);
 		/*  Am I booted by a Multiboot-compliant boot loader? */
-		if (magic != MULTIBOOT2_BOOTLOADER_MAGIC)
-		{
+		if (magic != MULTIBOOT2_BOOTLOADER_MAGIC) {
 				printk ("Invalid magic number: 0x%x\n", (unsigned) magic);
+				printk("%X\n", MULTIBOOT2_BOOTLOADER_MAGIC);
+				return;
+		}
+		unsigned long tmp=addr;
+		if (tmp & 7) {
+				printk ("Unaligned addr: 0x%08x\n", addr);
+				printk ("Unaligned tmp: 0x%08x\n", tmp);
 				return;
 		}
 
-		if (addr & 7)
-		{
-				printk ("Unaligned mbi: 0x%x\n", addr);
-				return;
-		}
-
-		size = *(unsigned *) addr;
 		printk ("Announced mbi size 0x%x\n", size);
 
 
 		tag = (struct multiboot_tag *) (addr + 8);
-		printk("tag type: %x\n", tag->type);
-		printk("tag size: %x\n", tag->size);
+		printk("tag type: %X\n", tag->type);
+		printk("tag size: %X\n", tag->size);
 
 
-		// for (tag = (struct multiboot_tag *) (addr + 8);
-		//      tag->type != MULTIBOOT_TAG_TYPE_END;
-		//      tag = (struct multiboot_tag *) ((uint8_t *) tag
-		//                                      + ((tag->size + 7) & ~7)))
-		// {
-		//   printk ("Tag 0x%x, Size 0x%x\n", tag->type, tag->size);
-		//   switch (tag->type)
-		//   {
-		//   case MULTIBOOT_TAG_TYPE_CMDLINE:
-		//     printk ("Command line = %s\n",
-		//             ((struct multiboot_tag_string *) tag)->string);
-		//     break;
-		//   case MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME:
-		//     printk ("Boot loader name = %s\n",
-		//             ((struct multiboot_tag_string *) tag)->string);
-		//     break;
-		//   case MULTIBOOT_TAG_TYPE_MODULE:
-		//     printk ("Module at 0x%x-0x%x. Command line %s\n",
-		//             ((struct multiboot_tag_module *) tag)->mod_start,
-		//             ((struct multiboot_tag_module *) tag)->mod_end,
-		//             ((struct multiboot_tag_module *) tag)->cmdline);
-		//     break;
-		//   case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:
-		//     printk ("mem_lower = %uKB, mem_upper = %uKB\n",
-		//             ((struct multiboot_tag_basic_meminfo *) tag)->mem_lower,
-		//             ((struct multiboot_tag_basic_meminfo *) tag)->mem_upper);
-		//     break;
-		//   case MULTIBOOT_TAG_TYPE_BOOTDEV:
-		//     printk ("Boot device 0x%x,%u,%u\n",
-		//             ((struct multiboot_tag_bootdev *) tag)->biosdev,
-		//             ((struct multiboot_tag_bootdev *) tag)->slice,
-		//             ((struct multiboot_tag_bootdev *) tag)->part);
-		//     break;
-		//   case MULTIBOOT_TAG_TYPE_MMAP:
-		//   {
-		//     multiboot_memory_map_t *mmap;
-		//
-		//     printk ("mmap\n");
-		//
-		//     for (mmap = ((struct multiboot_tag_mmap *) tag)->entries;
-		//          (uint8_t *) mmap
-		//          < (uint8_t *) tag + tag->size;
-		//          mmap = (multiboot_memory_map_t *)
-		//                 ((unsigned long) mmap
-		//                  + ((struct multiboot_tag_mmap *) tag)->entry_size))
-		//       printk (" base_addr = 0x%x%x,"
-		//               " length = 0x%x%x, type = 0x%x\n",
-		//               (unsigned) (mmap->addr >> 32),
-		//               (unsigned) (mmap->addr & 0xffffffff),
-		//               (unsigned) (mmap->len >> 32),
-		//               (unsigned) (mmap->len & 0xffffffff),
-		//               (unsigned) mmap->type);
-		//   }
-		//   break;
-		//   case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
-		//   {
-		//     uint32_t color;
-		//     unsigned i;
-		//     struct multiboot_tag_framebuffer *tagfb
-		//       = (struct multiboot_tag_framebuffer *) tag;
-		//     void *fb = (void *) (unsigned long) tagfb->common.framebuffer_addr;
-		//
-		//     switch (tagfb->common.framebuffer_type)
-		//     {
-		//     case MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED:
-		//     {
-		//       unsigned best_distance, distance;
-		//       struct multiboot_color *palette;
-		//
-		//       palette = tagfb->framebuffer_palette;
-		//
-		//       color = 0;
-		//       best_distance = 4*256*256;
-		//
-		//       for (i = 0; i < tagfb->framebuffer_palette_num_colors; i++)
-		//       {
-		//         distance = (0xff - palette[i].blue)
-		//                    * (0xff - palette[i].blue)
-		//                    + palette[i].red * palette[i].red
-		//                    + palette[i].green * palette[i].green;
-		//         if (distance < best_distance)
-		//         {
-		//           color = i;
-		//           best_distance = distance;
-		//         }
-		//       }
-		//     }
-		//     break;
-		//
-		//     case MULTIBOOT_FRAMEBUFFER_TYPE_RGB:
-		//       color = ((1 << tagfb->framebuffer_blue_mask_size) - 1)
-		//               << tagfb->framebuffer_blue_field_position;
-		//       break;
-		//
-		//     case MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT:
-		//       color = '\\' | 0x0100;
-		//       break;
-		//
-		//     default:
-		//       color = 0xffffffff;
-		//       break;
-		//     }
-		//
-		//     for (i = 0; i < tagfb->common.framebuffer_width
-		//          && i < tagfb->common.framebuffer_height; i++)
-		//     {
-		//       switch (tagfb->common.framebuffer_bpp)
-		//       {
-		//       case 8:
-		//       {
-		//         uint8_t *pixel = fb
-		//                          + tagfb->common.framebuffer_pitch * i + i;
-		//         *pixel = color;
-		//       }
-		//       break;
-		//       case 15:
-		//       case 16:
-		//       {
-		//         uint16_t *pixel
-		//           = fb + tagfb->common.framebuffer_pitch * i + 2 * i;
-		//         *pixel = color;
-		//       }
-		//       break;
-		//       case 24:
-		//       {
-		//         uint32_t *pixel
-		//           = fb + tagfb->common.framebuffer_pitch * i + 3 * i;
-		//         *pixel = (color & 0xffffff) | (*pixel & 0xff000000);
-		//       }
-		//       break;
-		//
-		//       case 32:
-		//       {
-		//         uint32_t *pixel
-		//           = fb + tagfb->common.framebuffer_pitch * i + 4 * i;
-		//         *pixel = color;
-		//       }
-		//       break;
-		//       }
-		//     }
-		//     break;
-		//   }
-		//
-		//   }
-		// }
+		for (tag = (struct multiboot_tag *) (addr + 8);
+		     tag->type != MULTIBOOT_TAG_TYPE_END;
+		     tag = (struct multiboot_tag *) ((uint8_t *) tag
+		                                     + ((tag->size + 7) & ~7)))
+		{
+				printk ("Tag 0x%x, Size 0x%x\n", tag->type, tag->size);
+				switch (tag->type) {
+				case MULTIBOOT_TAG_TYPE_CMDLINE:
+						printk ("Command line = %s\n",
+						        ((struct multiboot_tag_string *) tag)->string);
+						break;
+				case MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME:
+						printk ("Boot loader name = %s\n",
+						        ((struct multiboot_tag_string *) tag)->string);
+						break;
+				case MULTIBOOT_TAG_TYPE_MODULE:
+						printk ("Module at 0x%x-0x%x. Command line %s\n",
+						        ((struct multiboot_tag_module *) tag)->mod_start,
+						        ((struct multiboot_tag_module *) tag)->mod_end,
+						        ((struct multiboot_tag_module *) tag)->cmdline);
+						break;
+				case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:
+						printk ("mem_lower = %uKB, mem_upper = %uKB\n",
+						        ((struct multiboot_tag_basic_meminfo *) tag)->mem_lower,
+						        ((struct multiboot_tag_basic_meminfo *) tag)->mem_upper);
+						break;
+				case MULTIBOOT_TAG_TYPE_BOOTDEV:
+						printk ("Boot device 0x%x,%u,%u\n",
+						        ((struct multiboot_tag_bootdev *) tag)->biosdev,
+						        ((struct multiboot_tag_bootdev *) tag)->slice,
+						        ((struct multiboot_tag_bootdev *) tag)->part);
+						break;
+				case MULTIBOOT_TAG_TYPE_MMAP: {
+						multiboot_memory_map_t *mmap;
+
+						printk ("mmap\n");
+
+						for (mmap = ((struct multiboot_tag_mmap *) tag)->entries;
+						     (uint8_t *) mmap
+						     < (uint8_t *) tag + tag->size;
+						     mmap = (multiboot_memory_map_t *)
+						            ((unsigned long) mmap
+						             + ((struct multiboot_tag_mmap *) tag)->entry_size))
+								printk (" base_addr = 0x%x%x,"
+								        " length = 0x%x%x, type = 0x%x\n",
+								        (unsigned) (mmap->addr >> 32),
+								        (unsigned) (mmap->addr & 0xffffffff),
+								        (unsigned) (mmap->len >> 32),
+								        (unsigned) (mmap->len & 0xffffffff),
+								        (unsigned) mmap->type);
+				}
+				break;
+				}
+		}
 		tag = (struct multiboot_tag *) ((uint8_t *) tag
 		                                + ((tag->size + 7) & ~7));
 		printk ("Total mbi size 0x%x\n", (unsigned) tag - addr);
