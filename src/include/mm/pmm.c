8001bc7f266087ef5442435bf670880bac63d8be
MRNIU <a6813140@hotmail.com>
2019-01-14 10:35:54 +0800

    update to grub2

---
+++ src/include/mm/pmm.c
@@ -0,0 +1,17 @@
+
+// This file is a part of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
+
+// pmm.c for MRNIU/SimpleKernel.
+
+#include "mm/pmm.h"
+
+
+
+
+
+
+
+// void show_memory_map(){
+//
+//   return;
+// }
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -8,50 +8,54 @@
 // 内核入口
 void kernel_main()
 {
-	// 系统初始化
-	debug_init();
-	terminal_init();	// 控制台初始化
-	gdt_init();	// GDT 初始化
-	idt_init();	// IDT 初始化
-	clock_init();	// 时钟初始化
+		// 系统初始化
+		debug_init();
+		console_init(); // 控制台初始化
+		gdt_init(); // GDT 初始化
+		idt_init(); // IDT 初始化
+		clock_init(); // 时钟初始化
 
-	// 输出一些基本信息
-	printk_color(magenta ,"Welcome to my kernel.\n");
-	printk_color(light_red ,"kernel in memory start: 0x%08X\n", kern_start);
-	printk_color(light_red ,"kernel in memory end: 0x%08X\n", kern_end);
-	printk_color(light_red ,"kernel in memory size: %d KB, %d pages\n",
-							(kern_end - kern_start) / 1024, (kern_end - kern_start) / 1024 / 4);
 
-  cpu_sti();
-	printk_color(white, "sti\n");
-  if(canint())
-		printk_color(white, "interrupt accept!\n");
-	else
-		printk_color(light_red, "interrupt not accept!\n");
 
-	//cpu_cli();// close intr
-	for(int i=0;i<5;i++){
+		// 输出一些基本信息
+		printk_color(magenta,"Welcome to my kernel.\n");
+		printk_color(light_red,"kernel in memory start: 0x%08X\n", kernel_start);
+		printk_color(light_red,"kernel in memory end: 0x%08X\n", kernel_end);
+		printk_color(light_red,"kernel in memory size: %d KB, %d pages\n",
+		             (kernel_end - kernel_start) / 1024, (kernel_end - kernel_start) / 1024 / 4);
+
+
+
+		cpu_sti();
+		printk_color(white, "sti\n");
 		if(canint())
-			printk_color(white, "before hlt, interrupt accept!\n");
+				printk_color(white, "interrupt accept!\n");
 		else
-			printk_color(light_red, "before hlt, interrupt not accept!\n");
+				printk_color(light_red, "interrupt not accept!\n");
 
-		cpu_hlt();
+		//cpu_cli();// close intr
+		for(int i=0; i<5; i++) {
+				if(canint())
+						printk_color(white, "before hlt, interrupt accept!\n");
+				else
+						printk_color(light_red, "before hlt, interrupt not accept!\n");
+
+				cpu_hlt();
+
+				if(canint())
+						printk_color(white, "after hlt, interrupt accept!\n\n");
+				else
+						printk_color(light_red, "after hlt, interrupt not accept!\n\n");
+		}
+		//cpu_hlt();
+		//cpu_cli();
 
 		if(canint())
-			printk_color(white, "after hlt, interrupt accept!\n\n");
+				printk_color(white, "interrupt accept!\n");
 		else
-			printk_color(light_red, "after hlt, interrupt not accept!\n\n");
-	}
-	//cpu_hlt();
-	//cpu_cli();
-
-	if(canint())
-		printk_color(white, "interrupt accept!\n");
-	else
-		printk_color(light_red, "interrupt not accept!\n");
+				printk_color(light_red, "interrupt not accept!\n");
 
 
-	printk_color(white, "\nEnd.\n");
-	return;
+		printk_color(white, "\nEnd.\n");
+		return;
 }
