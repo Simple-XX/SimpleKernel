a3d431aa4d7e4159ef58bff16c5aad2d8658f76a
MRNIU <a6813140@hotmail.com>
2019-01-14 10:35:54 +0800

    update to grub2

--- src/include/libc/string.h
+++ src/include/libc/string.h
@@ -7,6 +7,8 @@
 #define _STRING_H_
 
 #include "stdint.h"
+#include "stddef.h"
+
 
 extern void memcpy(void *dest, void *src, uint32_t len);
 extern void memset(void *dest, uint8_t val, uint32_t len);
@@ -14,8 +16,11 @@ extern void bzero(void *dest, uint32_t len);
 extern int8_t strcmp(const char *src, const char *dest); // 字符串比较
 extern char *strcpy(char *dest, const char *src);
 extern char *strncpy(char *dest, const char *src, uint32_t len);
-extern char *strcat(char *dest, const char *src);
+extern char *strcat(char *dest, const char *src); // 字符串合并
 extern size_t strlen(const char *src);  // length of string
+extern void backspace(char *src);
+extern void append(char *src, char dest);
+
 
 
 #endif
