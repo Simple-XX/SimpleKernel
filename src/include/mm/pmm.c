407517150737706ca847387259469aafd3a0a5f4
MRNIU <a6813140@hotmail.com>
2019-01-22 23:58:52 +0800

    继续处理内存管理 这里有些卡进度，先去看看书再来

--- src/include/mm/pmm.c
+++ src/include/mm/pmm.c
@@ -4,39 +4,30 @@
 // pmm.c for MRNIU/SimpleKernel.
 
 #include "mm/pmm.h"
-#include "multiboot2.h"
 
-void pmm_init() {
-		uint64_t mmap_addr=mmap_entries->addr;
-		uint64_t mmap_length=mmap_entries->len;
-		uint64_t mmap_end=mmap_addr+mmap_length;
-		for(uint64_t mmap = mmap_addr; mmap < mmap_end; mmap++) {
+// 物理页帧数组长度
+static uint32_t phy_pages_count;
+
+// 可用物理内存页起始地址
+static uint32_t pmm_addr_start;
+
+// 可用物理内存页结束地址
+static uint32_t pmm_addr_end;
+
+
+void pmm_init(struct multiboot_tag *tag) {
+		multiboot_memory_map_entry_t * mmap;
+		mmap = ((struct multiboot_tag_mmap *) tag)->entries;
+		for (; (uint8_t *) mmap< (uint8_t *) tag + tag->size;
+		     mmap = (multiboot_memory_map_entry_t *)((unsigned long) mmap + ((struct multiboot_tag_mmap *) tag)->entry_size)) {
 				// 如果是可用内存
-				if (mmap_entries->type == MULTIBOOT_MEMORY_AVAILABLE
-				    && (mmap_addr&0xffffffff) == 0x100000) {
+				if ((unsigned) mmap->type == MULTIBOOT_MEMORY_AVAILABLE
+				    && (unsigned) (mmap->addr & 0xffffffff) == 0x100000)
 						// 把内核结束位置到结束位置的内存段，按页存储到页管理栈里
-						uint64_t page_addr = (mmap_addr&0xffffffff) + (uint64_t)(kernel_end - kernel_start);
-						uint64_t length = (mmap_addr&0xffffffff) + (mmap_length&0xffffffff);
-
-						// while (page_addr < length && page_addr <= PMM_MAX_SIZE) {
-						//   pmm_free_page(page_addr);
-						//   page_addr += PMM_PAGE_SIZE;
-						//   phy_page_count++;
-						// }
-				}
+						// TODO
+						;
 		}
-		printk("-----------------------\n");
-		printk("%x, %x\n", phy_page_count, pmm_stack);
-		printk("-----------------------\n");
-}
-
-uint64_t pmm_alloc_page(){
-		if (pmm_alloc_page!=0)
-				printk("out of memory!\n");
-		uint64_t page = pmm_stack[pmm_stack_top--];
-		return page;
-}
-
-void pmm_free_page(uint64_t p){
-		pmm_stack[++pmm_stack_top] = p;
+		printk_color(COL_INFO, "[INFO] ");
+		printk ("pmm_init\n");
+		return;
 }
