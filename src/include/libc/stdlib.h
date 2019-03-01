cde5257a4772ba3d40801d29408b3c84ebe315d6
MRNIU <a6813140@hotmail.com>
2019-03-01 23:18:46 +0800

    linked list finished, no test yet.

---
+++ src/include/libc/stdlib.h
@@ -0,0 +1,21 @@
+
+// This file is a part of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
+//
+// stdlib.h for MRNIU/SimpleKernel.
+#ifndef _STDLIB_H_
+#define _STDLIB_H_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
