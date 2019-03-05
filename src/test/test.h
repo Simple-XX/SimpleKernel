95f24b80d2fa1ef80d110f7f4fd9f52823155f77
MRNIU <a6813140@hotmail.com>
2019-03-05 11:19:10 +0800

    Add test/ for test functions. Fixed some warnings. Add comments. Ready to vmm.

---
+++ src/test/test.h
@@ -0,0 +1,22 @@
+
+// This file is a part of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
+
+// test.h for MRNIU/SimpleKernel.
+
+#ifndef _TEST_H_
+#define _TEST_H_
+
+#include "stdint.h"
+#include "stdbool.h"
+#include "stdio.h"
+#include "multiboot2.h"
+#include "debug.h"
+#include "mm/pmm.h"
+#include "mm/mm.h"
+#include "intr/clock.h"
+#include "drv/keyboard.h"
+
+bool test(void);
+bool test_pmm(void);
+
+#endif
