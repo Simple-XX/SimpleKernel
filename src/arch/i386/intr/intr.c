5ea9c323991fe8cd2ca472a651a2d03222ddd4d3
MRNIU <a6813140@hotmail.com>
2019-04-27 18:47:42 +0800

    - 初步完成虚拟内存管理，现在内核位于内存 3Gib 处(0xC0000000)，仍然有 bug。

--- src/arch/i386/intr/intr.c
+++ src/arch/i386/intr/intr.c
@@ -247,7 +247,34 @@ void general_protection(pt_regs_t * regs){
 }
 
 void page_fault(pt_regs_t * regs){
-		die("Page Fault.", regs->old_esp, regs->int_no);
+		// die("Page Fault.", regs->old_esp, regs->int_no);
+		uint32_t cr2;
+		asm volatile ("mov %%cr2,%0" : "=r" (cr2));
+
+		printk("Page fault at 0x%x, virtual faulting address 0x%x\n", regs->eip, cr2);
+		printk("Error code: %x\n", regs->err_code);
+
+		// bit 0 为 0 指页面不存在内存里
+		if ( !(regs->err_code & 0x1))
+				printk_color(red, "Because the page wasn't present.\n");
+		// bit 1 为 0 表示读错误，为 1 为写错误
+		if (regs->err_code & 0x2)
+				printk_color(red, "Write error.\n");
+		else
+				printk_color(red, "Read error.\n");
+		// bit 2 为 1 表示在用户模式打断的，为 0 是在内核模式打断的
+		if (regs->err_code & 0x4)
+				printk_color(red, "In user mode.\n");
+		else
+				printk_color(red, "In kernel mode.\n");
+		// bit 3 为 1 表示错误是由保留位覆盖造成的
+		if (regs->err_code & 0x8)
+				printk_color(red, "Reserved bits being overwritten.\n");
+		// bit 4 为 1 表示错误发生在取指令的时候
+		if (regs->err_code & 0x10)
+				printk_color(red, "The fault occurred during an instruction fetch.\n");
+
+		while (1);
 }
 
 void enable_irq(uint32_t irq_no){
