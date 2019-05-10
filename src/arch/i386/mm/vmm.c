094627904bd24e87ff3e2bedeb285c24903b23fa
MRNIU <a6813140@hotmail.com>
2019-05-10 23:52:08 +0800

    - 重新实现，参考了优秀的设计，下一步将 kernel.c 中的 init 部分改写为汇编

--- src/arch/i386/mm/vmm.c
+++ src/arch/i386/mm/vmm.c
@@ -11,10 +11,10 @@
 #include "cpu.hpp"
 
 // 内核页目录区域
-pgd_t pgd_kern[PGD_SIZE] __attribute__ ((aligned(PAGE_SIZE)));
+uint32_t pgd_kern[PGD_SIZE] __attribute__ ((aligned(PAGE_SIZE)));
 
 // 内核页表区域
-static pte_t pte_kern[PTE_COUNT][PTE_SIZE] __attribute__ ((aligned(PAGE_SIZE)));
+static uint32_t pte_kern[PTE_COUNT][PTE_SIZE] __attribute__ ((aligned(PAGE_SIZE)));
 
 void vmm_init(void) {
 		// 0xC0000000 这个地址在页目录的索引
@@ -44,21 +44,21 @@ void switch_pgd(uint32_t pd){
 		asm volatile ("mov %0, %%cr3" : : "r" (pd));
 }
 
-void map(pgd_t *pgd_now, uint32_t va, uint32_t pa, uint32_t flags){
+void map(uint32_t *pgd_now, uint32_t va, uint32_t pa, uint32_t flags){
 		uint32_t pgd_idx = PGD_INDEX(va);
 		uint32_t pte_idx = PTE_INDEX(va);
 
-		pte_t *pte = (pte_t *)(pgd_now[pgd_idx] & PAGE_MASK);
+		uint32_t *pte = (uint32_t *)(pgd_now[pgd_idx] & PAGE_MASK);
 		if (!pte) {
-				pte = (pte_t *)pmm_alloc_page();
+				pte = (uint32_t *)pmm_alloc_page();
 				pgd_now[pgd_idx] = (uint32_t)pte | PAGE_PRESENT | PAGE_WRITE;
 
 				// 转换到内核线性地址并清 0
-				pte = (pte_t *)((uint32_t)pte + PAGE_OFFSET);
+				pte = (uint32_t *)((uint32_t)pte + PAGE_OFFSET);
 				bzero(pte, PAGE_SIZE);
 		} else {
 				// 转换到内核线性地址
-				pte = (pte_t *)((uint32_t)pte + PAGE_OFFSET);
+				pte = (uint32_t *)((uint32_t)pte + PAGE_OFFSET);
 		}
 
 		pte[pte_idx] = (pa & PAGE_MASK) | flags;
@@ -68,18 +68,18 @@ void map(pgd_t *pgd_now, uint32_t va, uint32_t pa, uint32_t flags){
 		__native_flush_tlb_single(va);
 }
 
-void unmap(pgd_t *pgd_now, uint32_t va){
+void unmap(uint32_t *pgd_now, uint32_t va){
 		uint32_t pgd_idx = PGD_INDEX(va);
 		uint32_t pte_idx = PTE_INDEX(va);
 
-		pte_t *pte = (pte_t *)(pgd_now[pgd_idx] & PAGE_MASK);
+		uint32_t *pte = (uint32_t *)(pgd_now[pgd_idx] & PAGE_MASK);
 
 		if (!pte) {
 				return;
 		}
 
 		// 转换到内核线性地址
-		pte = (pte_t *)((uint32_t)pte + PAGE_OFFSET);
+		pte = (uint32_t *)((uint32_t)pte + PAGE_OFFSET);
 
 		pte[pte_idx] = 0;
 
@@ -88,17 +88,17 @@ void unmap(pgd_t *pgd_now, uint32_t va){
 		__native_flush_tlb_single(va);
 }
 
-uint32_t get_mapping(pgd_t *pgd_now, uint32_t va, uint32_t *pa){
+uint32_t get_mapping(uint32_t *pgd_now, uint32_t va, uint32_t *pa){
 		uint32_t pgd_idx = PGD_INDEX(va);
 		uint32_t pte_idx = PTE_INDEX(va);
 
-		pte_t *pte = (pte_t *)(pgd_now[pgd_idx] & PAGE_MASK);
+		uint32_t *pte = (uint32_t *)(pgd_now[pgd_idx] & PAGE_MASK);
 		if (!pte) {
 				return 0;
 		}
 
 		// 转换到内核线性地址
-		pte = (pte_t *)((uint32_t)pte + PAGE_OFFSET);
+		pte = (uint32_t *)((uint32_t)pte + PAGE_OFFSET);
 
 		// 如果地址有效而且指针不为NULL，则返回地址
 		if (pte[pte_idx] != 0 && pa) {
