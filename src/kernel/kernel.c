249cd793e3e3dfc17a66a2c5421bea02d55b0a7e
MRNIU <a6813140@hotmail.com>
2019-09-05 17:12:19 +0800

    pmm ready

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -9,16 +9,17 @@
 #define DEBUG 1
 
 // 内核入口
+// 指针是 32 位的
 void kernel_main(uint32_t magic, uint32_t addr) {
     console_init();     // 控制台初始化
     gdt_init();      // GDT 初始化
     idt_init();     // IDT 初始化
     clock_init();     // 时钟初始化
     keyboard_init();     // 键盘初始化
-    // multiboot2_init(magic, addr); // 包括 pmm_init 与 vmm_init
+    multiboot2_init(magic, addr); // 包括 pmm_init 与 vmm_init
     // debug_init(magic, addr);
     showinfo();
-    // test();
+    test();
 
 
     while(1);
