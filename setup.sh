fc1a3a78045425353b6665a360ca7e4d312aa783
MRNIU <a6813140@hotmail.com>
2019-09-05 17:12:19 +0800

    pmm ready

--- setup.sh
+++ setup.sh
@@ -29,60 +29,60 @@ iso_folder='./iso/'
 OS=0
 which_os=`uname -s`
 if [ ${which_os} == "Darwin" ]; then
-  OS=0
-elif [ ${which_os} == "Darwin" ]; then
-  OS=1
+    OS=0
+elif [ ${which_os} == "Linux" ]; then
+    OS=1
 fi
 
 # 检测环境，如果没有安装需要的软件，则安装
 if ! [ -x "$(command -v bochs)" ]; then
-  echo 'Error: bochs is not installed.'
-  echo 'Install bochs...'
-  if [ ${OS} == 0 ]; then
-    brew install bochs
-  elif [ ${OS} == 1 ]; then
-    shell ${tool}/bochs.sh
-  fi
+    echo 'Error: bochs is not installed.'
+    echo 'Install bochs...'
+    if [ ${OS} == 0 ]; then
+        brew install bochs
+    elif [ ${OS} == 1 ]; then
+        shell ${tool}/bochs.sh
+    fi
 fi
 
 if ! [ -x "$(command -v i386-elf-ld)" ]; then
-  echo 'Error: i386-elf-binutils is not installed.'
-  echo 'Install i386-elf-binutils...'
-  if [ ${OS} == 0 ]; then
-    brew install i386-elf-binutils
-  elif [ ${OS} == 1 ]; then
-    shell ${tool}/i386-elf-binutils.sh
-  fi
+    echo 'Error: i386-elf-binutils is not installed.'
+    echo 'Install i386-elf-binutils...'
+    if [ ${OS} == 0 ]; then
+        brew install i386-elf-binutils
+    elif [ ${OS} == 1 ]; then
+        shell ${tool}/i386-elf-binutils.sh
+    fi
 fi
 
 if ! [ -x "$(command -v i386-elf-gcc)" ]; then
-  echo 'Error: i386-elf-gcc is not installed.'
-  echo 'Install i386-elf-gcc...'
-  if [ ${OS} == 0 ]; then
-    brew install i386-elf-gcc
-  elif [ ${OS} == 1 ]; then
-    shell ${tool}/i386-elf-gcc.sh
-  fi
+    echo 'Error: i386-elf-gcc is not installed.'
+    echo 'Install i386-elf-gcc...'
+    if [ ${OS} == 0 ]; then
+        brew install i386-elf-gcc
+    elif [ ${OS} == 1 ]; then
+        shell ${tool}/i386-elf-gcc.sh
+    fi
 fi
 
 if ! [ -x "$(command -v i386-elf-grub-file)" ]; then
-  echo 'Error: i386-elf-grub is not installed.'
-  echo 'Install i386-elf-grub...'
-  if [ ${OS} == 0 ]; then
-    brew install i386-elf-grub
-  elif [ ${OS} == 1 ]; then
-    shell ${tool}/i386-elf-grub.sh
-  fi
+    echo 'Error: i386-elf-grub is not installed.'
+    echo 'Install i386-elf-grub...'
+    if [ ${OS} == 0 ]; then
+        brew install i386-elf-grub
+    elif [ ${OS} == 1 ]; then
+        shell ${tool}/i386-elf-grub.sh
+    fi
 fi
 
 if ! [ -x "$(command -v xorriso)" ]; then
-  echo 'Error: xorriso is not installed.'
-  echo 'Install xorriso...'
-  if [ ${OS} == 0 ]; then
-    brew install xorriso
-  elif [ ${OS} == 1 ]; then
-    shell ${tool}/xorriso.sh
-  fi
+    echo 'Error: xorriso is not installed.'
+    echo 'Install xorriso...'
+    if [ ${OS} == 0 ]; then
+        brew install xorriso
+    elif [ ${OS} == 1 ]; then
+        shell ${tool}/xorriso.sh
+    fi
 fi
 
 # 重新编译
@@ -91,27 +91,12 @@ make remake
 cd ../
 
 if i386-elf-grub-file --is-x86-multiboot2 ${img}; then
-  echo Multiboot2 Confirmed!
+    echo Multiboot2 Confirmed!
 else
-  echo the file is not multiboot
-  exit
+    echo the file is not multiboot
+    exit
 fi
 
-# 把 boot.img 挂载到当前目录，然后将 kernel.img 写入 boot 目录，取消挂载。
-# 以 bochrc.txt 为配置文件运行 bochs。
-if [ ${OS} == 0 ]; then
-  # mac 下的命令与 linux 的不同
-  # hdiutil attach -mountpoint ${folder} ${disk}
-  # cp ${img} ${boot_folder}
-  # hdiutil detach ${folder}
-  cp ${img} ${iso_boot}
-  i386-elf-grub-mkrescue -o ${iso} ${iso_folder}
-  bochs -q -f ${bochsrc}
-elif [ ${OS} == 1 ]; then
-  mkdir ${folder}
-  mount ${disk} ${folder}
-  cp ${img} ${boot_folder}
-  unmount ${folder}
-  rm -rf ${folder}
-  bochs -q -f ${bochsrc}
-fi
+cp ${img} ${iso_boot}
+i386-elf-grub-mkrescue -o ${iso} ${iso_folder}
+bochs -q -f ${bochsrc}
