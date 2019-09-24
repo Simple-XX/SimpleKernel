b30c6b3c91e746ef372f4835afcf7d175247817f
MRNIU <a6813140@hotmail.com>
2019-09-24 20:29:18 +0800

    improve existing boot code TODO: improve architecture

--- src/kernel/init.c
+++ src/kernel/init.c
@@ -3,23 +3,19 @@
 // boot .s for MRNIU/SimpleKernel.
 
 #include "stdint.h"
-#include "stdio.h"
 
-
-
-uint8_t kern_stack[0x1000]  __attribute__ ( ( aligned(0x1000) ) );
-
-
-
-uint32_t kern_stack_top = (uint32_t)kern_stack + 0x1000;
-
+// #include "kernel.h"
+//
+// uint8_t kern_stack[0x1000]  __attribute__ ( ( aligned(0x1000) ) );
+//
+//
+//
+// uint32_t kern_stack_top = (uint32_t)kern_stack + 0x1000;
+//
 
 
 // 内核入口函数
-__attribute__( ( section(".init.text") ) ) void kernel_entry(uint64_t magic, uint64_t addr)
+__attribute__( ( section(".init.text") ) ) void kernel_entry(uint32_t magic, uint32_t addr)
 {
-    printk("-----------\n");
-    printk("mag: %X, addr: %X\n", magic, addr);
-    printk("-----------\n");
     kernel_main(magic, addr);
 }
