2037857aff7e434b3814213f613f99a0a6144ce1
MRNIU <a6813140@hotmail.com>
2019-09-24 20:29:18 +0800

    improve existing boot code TODO: improve architecture

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -8,23 +8,17 @@
 
 // 内核入口
 // 指针是 32 位的
-void kernel_main(uint64_t magic, uint64_t addr) {
-    // console_init();     // 控制台初始化
-    // gdt_init();      // GDT 初始化
-    // idt_init();     // IDT 初始化
-    // clock_init();     // 时钟初始化
-    // keyboard_init();     // 键盘初始化
-    //
-    //
-    // printk("%X\t%X\n", magic, addr);
-    //
-    //
-    // multiboot2_init(magic, addr); // 包括 pmm_init 与 vmm_init
-    //
-    // debug_init(magic, addr);
-    // showinfo();
-    // test();
+void kernel_main(uint32_t magic, uint32_t addr) {
+    console_init();     // 控制台初始化
+    gdt_init();      // GDT 初始化
+    idt_init();     // IDT 初始化
+    clock_init();     // 时钟初始化
+    keyboard_init();     // 键盘初始化
+    multiboot2_init(magic, addr); // 包括 pmm_init 与 vmm_init
 
+    debug_init(magic, addr);
+    showinfo();
+    test();
 
     while(1);
 
