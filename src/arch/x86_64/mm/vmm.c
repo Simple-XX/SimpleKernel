fa6438af8d4b2d48824164feb7a94e25e8d6bf4c
MRNIU <a6813140@hotmail.com>
2019-12-09 01:10:59 +0800

    update

--- src/arch/x86_64/mm/vmm.c
+++ src/arch/x86_64/mm/vmm.c
@@ -24,37 +24,25 @@ uint32_t page_directory[1024] __attribute__((aligned(4096)));
 uint32_t first_page_table[1024] __attribute__((aligned(4096)));
 
 void vmm_init() {
-	//set each entry to not present
+
 	for(int i = 0; i < 1024; i++) {
-		// This sets the following flags to the pages:
-		//   Supervisor: Only kernel-mode can access them
-		//   Write Enabled: It can be both read from and written to
-		//   Not Present: The page table is not present
-		page_directory[i] = 0x00000002;
+		page_directory[i] = PAGE_WRITE;
 	}
 	// holds the physical address where we want to start mapping these pages to.
-// in this case, we want to map these pages to the very beginning of memory.
+	// in this case, we want to map these pages to the very beginning of memory.
 
-//we will fill all 1024 entries in the table, mapping 4 megabytes
+	//we will fill all 1024 entries in the table, mapping 4 megabytes
 	for(int i = 0; i < 1024; i++) {
 		// As the address is page aligned, it will always leave 12 bits zeroed.
 		// Those bits are used by the attributes ;)
-		first_page_table[i] = (i * 0x1000) | 3; // attributes: supervisor level, read/write, present.
+		first_page_table[i] = (i * PAGE_SIZE) | PAGE_WRITE | PAGE_PRESENT; // attributes: supervisor level, read/write, present.
 	}
 
 	// attributes: supervisor level, read/write, present
-	page_directory[0] = ((unsigned int)first_page_table) | 3;
+	page_directory[0] = ((uint32_t)first_page_table) | PAGE_WRITE | PAGE_PRESENT;
 
+	// enable_page(page_directory);
 
-	// 设置临时页表
-	__asm__ volatile ( "mov %0, %%cr3" : : "r" (page_directory) );
-
-	uint32_t cr0;
-
-	__asm__ volatile ( "mov %%cr0, %0" : "=r" (cr0) );
-	// 最高位 PG 位置 1，分页开启
-	cr0 |= (1u << 31);
-	__asm__ volatile ( "mov %0, %%cr0" : : "r" (cr0) );
 
 	printk_color(COL_INFO, "[INFO] ");
 	printk("vmm_init\n");
