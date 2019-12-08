d749ba777f4db1b6b202a818cf0a1b09225792c2
MRNIU <a6813140@hotmail.com>
2019-12-08 20:19:05 +0800

    update

--- src/arch/x86_64/mm/include/vmm.h
+++ src/arch/x86_64/mm/include/vmm.h
@@ -79,9 +79,39 @@ typedef uint32_t pte_t;
 // 内核页目录区域
 extern pgd_t pgd_kernel[PGD_SIZE];
 
+typedef
+        struct page {
+	uint32_t present    : 1;  // Page present in memory
+	uint32_t rw         : 1;  // Read-only if clear, readwrite if set
+	uint32_t user       : 1;  // Supervisor level only if clear
+	uint32_t accessed   : 1;  // Has the page been accessed since last refresh?
+	uint32_t dirty      : 1;  // Has the page been written to since last refresh?
+	uint32_t unused     : 7;  // Amalgamation of unused and reserved bits
+	uint32_t frame      : 20;  // Frame address (shifted right 12 bits)
+} page_t;
+
+typedef
+        struct page_table {
+	page_t pages[1024];
+} page_table_t;
+
+typedef
+        struct page_directory {
+	// Array of pointers to pagetables.
+	page_table_t *tables[1024];
+
+	// Array of pointers to the pagetables above, but gives their *physical*
+	// location, for loading into the CR3 register.
+	uint32_t tablesPhysical[1024];
+
+	// The physical address of tablesPhysical. This comes into play
+	// when we get our kernel heap allocated and the directory
+	// may be in a different location in virtual memory.
+	uint32_t physicalAddr;
+} page_directory_t;
+
 // 初始化虚拟内存管理
 void vmm_init(void);
-
 
 #ifdef __cplusplus
 }
