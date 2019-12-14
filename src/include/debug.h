0a777ac8fde60ddcff7c1bacf4e9b2278bf51740
MRNIU <a6813140@hotmail.com>
2019-12-14 22:32:08 +0800

    update

--- src/include/debug.h
+++ src/include/debug.h
@@ -1,22 +1,19 @@
 
 // This file is a part of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
-// Based on http://wiki.0xffffff.org/posts/hurlex-5.html
+//
 // debug.h for MRNIU/SimpleKernel.
-
 
 #ifndef _DEBUG_H_
 #define _DEBUG_H_
 
-#include "stdarg.h"
 #include "stddef.h"
 #include "stdio.h"
 #include "string.h"
-#include "intr/intr.h"
+#include "intr/include/intr.h"
 
+#define DEBUG 1
 
 void debug_init(void);
-void print_registers(pt_regs_t * regs);
+void print_cur_status(void);
 
-
-
-#endif
+#endif /* _DEBUG_H_ */
---
+++ src/include/mem/pmm.h
@@ -0,0 +1,51 @@
+
+// This file is a part of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
+//
+// pmm.h for MRNIU/SimpleKernel.
+
+#ifndef _PMM_H_
+#define _PMM_H_
+
+#include "stdint.h"
+#include "stdio.h"
+#include "stddef.h"
+#include "debug.h"
+#include "multiboot2.h"
+
+#define STACK_SIZE    0x1000  // 4096
+#define PMM_MAX_SIZE  0x20000000// 512MB
+#define PMM_PAGE_SIZE 0x1000      // 页面大小 4KB
+#define PAGE_MAX_SIZE ( PMM_MAX_SIZE / PMM_PAGE_SIZE )
+
+extern ptr_t kernel_init_start[];
+extern ptr_t kernel_init_text_start[];
+extern ptr_t kernel_init_text_end[];
+extern ptr_t kernel_init_data_start[];
+extern ptr_t kernel_init_data_end[];
+extern ptr_t kernel_init_end[];
+
+extern ptr_t kernel_start[];
+extern ptr_t kernel_text_start[];
+extern ptr_t kernel_text_end[];
+extern ptr_t kernel_data_start[];
+extern ptr_t kernel_data_end[];
+extern ptr_t kernel_end[];
+
+// 开启分页机制之后的内核栈
+extern uint8_t kernel_stack[STACK_SIZE];
+// 内核栈的栈顶
+extern ptr_t kernel_stack_top;
+
+extern multiboot_memory_map_entry_t * mmap_entries;
+extern multiboot_mmap_tag_t * mmap_tag;
+
+// 初始化内存管理
+void pmm_init(void);
+
+ptr_t pmm_alloc_page(void);
+
+ptr_t pmm_alloc_pages(uint32_t size);
+
+void pmm_free_page(ptr_t page);
+
+#endif /* _PMM_H_ */
