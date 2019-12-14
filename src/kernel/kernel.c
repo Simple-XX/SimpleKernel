c3d20fd1d39c53d75e708ae4ccb45504eddf76e2
MRNIU <a6813140@hotmail.com>
2019-12-14 22:32:08 +0800

    update

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,29 +1,36 @@
 
 // This file is a part of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
-
+//
 // kernel.c for MRNIU/SimpleKernel.
 
-#include "kernel.h"
-#include "assert.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "include/kernel.h"
 
 extern void tmp();
 
 // 内核入口
 void kernel_main(void);
 void kernel_main(){
-		// 系统初始化
-		debug_init();
-		console_init();       // 控制台初始化
-		gdt_init();       // GDT 初始化
-		idt_init();       // IDT 初始化
+	// 系统初始化
+	debug_init();
+	console_init();               // 控制台初始化
+	gdt_init();               // GDT 初始化
+	idt_init();               // IDT 初始化
 
-		clock_init();       // 时钟初始化
-		keyboard_init();       // 键盘初始化
-		showinfo();
+	clock_init();               // 时钟初始化
+	keyboard_init();               // 键盘初始化
+	showinfo();
 
 
-		while(1);
+	while(1);
 
-		// printk_color(white, "\nEnd.\n");
-		return;
+	// printk_color(white, "\nEnd.\n");
+	return;
 }
+
+#ifdef __cplusplus
+}
+#endif
