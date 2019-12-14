e1dd79d425e6b8ca484238fe0e5e8156df0cd0e4
MRNIU <a6813140@hotmail.com>
2019-12-14 22:05:34 +0800

    update

---
+++ src/arch/x86_64/debug/debug.c
@@ -0,0 +1,18 @@
+
+// This file is a part of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
+//
+// debug.c for MRNIU/SimpleKernel.
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "debug.h"
+
+void debug_init(void) {
+	return;
+}
+
+#ifdef __cplusplus
+}
+#endif
