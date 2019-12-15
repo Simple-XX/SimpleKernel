470c683fae2ab681ee7b636c89cb7418b26a8654
MRNIU <a6813140@hotmail.com>
2019-12-15 12:53:09 +0800

    Working on vmm

--- src/arch/x86_64/intr/intr.c
+++ src/arch/x86_64/intr/intr.c
@@ -243,39 +243,6 @@ void general_protection(pt_regs_t * regs) {
 	die("General Protection.", regs->old_esp, regs->int_no);
 }
 
-void page_fault(pt_regs_t * regs) {
-	// die("Page Fault.", regs->old_esp, regs->int_no);
-	uint64_t cr2;
-#ifdef __x86_64__
-	asm volatile ( "movq %%cr2,%0" : "=r" ( cr2 ) );
-#else
-	asm volatile ( "mov %%cr2,%0" : "=r" ( cr2 ) );
-#endif
-	printk("Page fault at 0x%x, virtual faulting address 0x%x\n", regs->eip, cr2);
-	printk("Error code: %x\n", regs->err_code);
-
-	// bit 0 为 0 指页面不存在内存里
-	if ( !( regs->err_code & 0x1 ) )
-		printk_color(red, "Because the page wasn't present.\n");
-	// bit 1 为 0 表示读错误，为 1 为写错误
-	if (regs->err_code & 0x2)
-		printk_color(red, "Write error.\n");
-	else
-		printk_color(red, "Read error.\n");
-	// bit 2 为 1 表示在用户模式打断的，为 0 是在内核模式打断的
-	if (regs->err_code & 0x4)
-		printk_color(red, "In user mode.\n");
-	else
-		printk_color(red, "In kernel mode.\n");
-	// bit 3 为 1 表示错误是由保留位覆盖造成的
-	if (regs->err_code & 0x8)
-		printk_color(red, "Reserved bits being overwritten.\n");
-	// bit 4 为 1 表示错误发生在取指令的时候
-	if (regs->err_code & 0x10)
-		printk_color(red, "The fault occurred during an instruction fetch.\n");
-	while (1);
-}
-
 void enable_irq(uint32_t irq_no) {
 	uint8_t mask = 0;
 	// printk_color(green, "enable_irq mask: %X", mask);
--- src/include/mem/vmm.h
+++ src/include/mem/vmm.h
@@ -13,48 +13,46 @@ extern "C" {
 #include "intr/include/intr.h"
 #include "pmm.h"
 
-// 最大内存
-#define KMEM_SIZE PMM_MAX_SIZE
-
-// 映射 KMEM_SIZE 的内存所需要的页数
-#define PTE_COUNT         ( KMEM_SIZE / PAGE_MAP_SIZE )
-
 // 内核的偏移地址
-#define PAGE_OFFSET     0xC0000000
+#define PAGE_OFFSET    0xC0000000
+
+// 最大内存, 512MB, 0x20000000
+#define KMEM_SIZE    PMM_MAX_SIZE
 
 // 每个页表可以映射的内存数, 4MB
-#define PAGE_MAP_SIZE     (0x400000)
+#define PAGE_MAP_SIZE    (0x400000)
+
+// 映射 KMEM_SIZE 的内存所需要的页数 0x80, 128
+#define PTE_COUNT    (KMEM_SIZE / PAGE_MAP_SIZE)
+
+// 虚拟分页大小 4096
+#define PAGE_SIZE    0x1000
+
+// 页掩码，用于 4KB 对齐
+#define PAGE_MASK    0xFFFFF000
+
+// 页目录成员数, 128, 0x80
+#define PGD_SIZE    (PAGE_SIZE / sizeof(pte_t))
+
+// 页表成员数, 128, 0x80
+#define PTE_SIZE    (PAGE_SIZE / sizeof(ptr_t))
 
 // P-- 位 0 是存在 (Present) 标志，用于指明表项对地址转换是否有效。
 // P = 1 表示有效； P = 0 表示无效。
-// 在页转换过程中，如果说涉及的页目录或页表的表项无效，则会导致一个异常。
-// 如果 P = 0  那么除表示表项无效外，其余位可供程序自由使用。
-// 例如，操作系统可以使用这些位来保存已存储在磁盘上的页面的序号。
-
 #define PAGE_PRESENT    0x00000001
 
 // R/W-- 位 1 是读 / 写 (Read/Write) 标志。如果等于 1  表示页面可以被读、写或执行。
 // 如果为 0  表示页面只读或可执行。
-// 当处理器运行在超级用户特权级 （级别 0,1 或 2） 时，则 R/W 位不起作用。
-// 页目录项中的 R/W 位对其所映射的所有页面起作用。
-
-#define PAGE_WRITE  0x00000002
+#define PAGE_WRITE    0x00000002
 
 // U/S-- 位 2 是用户 / 超级用户 (User/Supervisor) 标志。
 // 如果为 1  那么运行在任何特权级上的程序都可以访问该页面。
 // 如果为 0  那么页面只能被运行在超级用户特权级 (0,1 或 2)  的程序访问。
-// 页目录项中的 U/S 位对其所映射的所有页面起作用。
 
-#define PAGE_USER   0x00000004
-
-// 虚拟分页大小 4096
-#define PAGE_SIZE   0x1000
-
-// 页掩码，用于 4KB 对齐
-#define PAGE_MASK      0xFFFFF000
+#define PAGE_USER    0x00000004
 
 // 获取一个地址的页目录项，高 10 位
-#define PGD_INDEX(x) ( ( ( x ) >> 22 ) & 0x03FF )
+#define PGD_INDEX(x) (((x) >> 22) & 0x03FF)
 
 // 获取一个地址的页表项，中间 10 位
 #define PTE_INDEX(x) ( ( ( x ) >> 12 ) & 0x03FF )
@@ -67,12 +65,6 @@ typedef ptr_t pgd_t;
 
 // 页表
 typedef ptr_t pte_t;
-
-// 页目录成员数
-#define PGD_SIZE ( PAGE_SIZE / sizeof( pte_t ) )
-
-// 页表成员数
-#define PTE_SIZE ( PAGE_SIZE / sizeof( ptr_t ) )
 
 // 内核页目录区域
 extern pgd_t pgd_kernel[PGD_SIZE];
@@ -110,6 +102,8 @@ typedef
 
 // 初始化虚拟内存管理
 void vmm_init(void);
+
+void switch_pgd(ptr_t pd);
 
 #ifdef __cplusplus
 }
