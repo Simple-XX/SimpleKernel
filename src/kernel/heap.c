6331c80a4d47aa090058afd684409291c0d41fe5
MRNIU <a6813140@hotmail.com>
2019-12-29 23:20:53 +0800

    - Update devlog - Add ``kmalloc` and `kfree` It hasn't been tested yet `

---
+++ src/kernel/heap.c
@@ -0,0 +1,23 @@
+
+// This file is a part of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
+//
+// heap.c for MRNIU/SimpleKernel.
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stdlib.h"
+#include "mem/pmm.h"
+
+void * kmalloc(size_t byte) {
+	return (void *)pmm_alloc(byte);
+}
+void kfree(void * page) {
+	pmm_free_page(*(ptr_t*)page);
+	return;
+}
+
+#ifdef __cplusplus
+}
+#endif
