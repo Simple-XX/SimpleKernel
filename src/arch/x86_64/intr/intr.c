e64717238ccfd2ef50ed94753a25d471a162a716
MRNIU <a6813140@hotmail.com>
2020-01-11 18:56:35 +0800

    - Add codestyle file - Fixup code style - Update devlog - Update paging related structure definitions

--- src/arch/x86_64/intr/intr.c
+++ src/arch/x86_64/intr/intr.c
@@ -10,14 +10,14 @@ extern "C" {
 #include "include/intr.h"
 
 // 中断描述符表
-static idt_entry_t idt_entries[INTERRUPT_MAX] __attribute__ ( ( aligned(16) ) );
+static idt_entry_t idt_entries[INTERRUPT_MAX] __attribute__( (aligned(16) ) );
 
 static idt_ptr_t idt_ptr; // IDTR
 
 // 设置中断描述符
 static void idt_set_gate(uint8_t num, uint32_t base, uint16_t target, uint8_t flags) {
-	idt_entries[num].base_low = ( base & 0xFFFF );
-	idt_entries[num].base_high = ( base >> 16 ) & 0xFFFF;
+	idt_entries[num].base_low = (base & 0xFFFF);
+	idt_entries[num].base_high = (base >> 16) & 0xFFFF;
 	idt_entries[num].selector = target;
 	idt_entries[num].zero = 0;
 	idt_entries[num].flags = flags;
@@ -26,10 +26,10 @@ static void idt_set_gate(uint8_t num, uint32_t base, uint16_t target, uint8_t fl
 }
 
 // 中断处理函数指针数组
-static interrupt_handler_t interrupt_handlers[INTERRUPT_MAX] __attribute__ ( ( aligned(4) ) );
+static interrupt_handler_t interrupt_handlers[INTERRUPT_MAX] __attribute__( (aligned(4) ) );
 
-static const char *intrname(uint32_t intrno) {
-	static const char *const intrnames[] = {
+static const char * intrname(uint32_t intrno) {
+	static const char * const intrnames[] = {
 		"Divide error",
 		"Debug",
 		"Non-Maskable Interrupt",
@@ -51,19 +51,19 @@ static const char *intrname(uint32_t intrno) {
 		"Machine-Check",
 		"SIMD Floating-Point Exception"
 	};
-	if ( intrno < sizeof( intrnames ) / sizeof( const char *const ) ) {
+	if(intrno < sizeof(intrnames) / sizeof(const char * const) ) {
 		return intrnames[intrno];
 	}
 	return "(unknown trap)";
 }
 
 // 调用中断处理函数
-void isr_handler(pt_regs_t *regs) {
-	if (interrupt_handlers[regs->int_no]) {
+void isr_handler(pt_regs_t * regs) {
+	if(interrupt_handlers[regs->int_no]) {
 		interrupt_handlers[regs->int_no](regs);
 	}
 	else {
-		printk( "Unhandled interrupt: %d %s\n", regs->int_no, intrname(regs->int_no) );
+		printk("Unhandled interrupt: %d %s\n", regs->int_no, intrname(regs->int_no) );
 		cpu_hlt();
 	}
 }
@@ -74,9 +74,9 @@ void register_interrupt_handler(uint8_t n, interrupt_handler_t h) {
 }
 
 // IRQ 处理函数
-void irq_handler(pt_regs_t *regs) {
+void irq_handler(pt_regs_t * regs) {
 	clear_interrupt_chip(regs->int_no); // 重设PIC芯片
-	if (interrupt_handlers[regs->int_no]) {
+	if(interrupt_handlers[regs->int_no]) {
 		interrupt_handlers[regs->int_no](regs);
 	}
 }
@@ -101,7 +101,7 @@ static isr_irq_func_t isr_irq_func[INTERRUPT_MAX] = {
 // idt 初始化
 void idt_init(void) {
 	init_interrupt_chip();
-	idt_ptr.limit = sizeof( idt_entry_t ) * INTERRUPT_MAX - 1;
+	idt_ptr.limit = sizeof(idt_entry_t) * INTERRUPT_MAX - 1;
 	idt_ptr.base = (uint32_t)&idt_entries;
 
 	// 0-32:  用于 CPU 的中断处理
@@ -109,7 +109,7 @@ void idt_init(void) {
 	// 0x8E: 10001110: DPL=0s
 	// 0x08: 0000 1000
 
-	for (uint32_t i = 0; i < 48; ++i) {
+	for(uint32_t i = 0 ; i < 48 ; ++i) {
 		idt_set_gate(i, (uint32_t)isr_irq_func[i], 0x08, 0x8E);
 	}
 	// 128 (0x80) 将来用于实现系统调用
@@ -119,7 +119,7 @@ void idt_init(void) {
 	idt_set_gate(INT_BOUND, (uint32_t)isr_irq_func[INT_BOUND], 0x08, 0xEF);
 	idt_set_gate(128, (uint32_t)isr128, 0x08, 0xEF);
 
-	idt_load( (uint32_t)&idt_ptr );
+	idt_load( (uint32_t)&idt_ptr);
 
 	register_interrupt_handler(INT_DIVIDE_ERROR, &divide_error);
 	register_interrupt_handler(INT_DEBUG, &debug);
@@ -142,7 +142,7 @@ void idt_init(void) {
 
 static void die(char * str, uint32_t oesp, uint32_t int_no) {
 	// uint32_t * old_esp = (uint32_t *)oesp;
-	pt_regs_t * old_esp = (pt_regs_t *) oesp;
+	pt_regs_t * old_esp = (pt_regs_t *)oesp;
 	printk_color(red, "%s\t: %d\n\r", str, int_no);
 	printk_color(light_red, "die_Unuseable.\n");
 	// cs::EIP
@@ -154,15 +154,15 @@ static void die(char * str, uint32_t oesp, uint32_t int_no) {
 	// printk_color(red, "EIP:\t%08x:%08X\nEFLAGS:\t%08x\nESP:\t%08x:%08X\n",
 	// old_esp->cs, old_esp->eip, old_esp->eflags, old_esp->ss, old_esp->old_esp);
 	printk_color(red, "gs: %08x\tfs: %08x\tes: %08x\tds: %08x\n",
-	             old_esp->gs, old_esp->fs, old_esp->es, old_esp->ds);
+		old_esp->gs, old_esp->fs, old_esp->es, old_esp->ds);
 	printk_color(red, "edi: %08x\tesi: %08x\tebp: %08x\told_esp: %08x\n",
-	             old_esp->edi, old_esp->esi, old_esp->ebp, old_esp->old_esp);
+		old_esp->edi, old_esp->esi, old_esp->ebp, old_esp->old_esp);
 	printk_color(red, "ebx: %08x\tedx: %08x\tecx: %08x\teax: %08x\n",
-	             old_esp->ebx, old_esp->edx, old_esp->ecx, old_esp->eax);
+		old_esp->ebx, old_esp->edx, old_esp->ecx, old_esp->eax);
 	printk_color(red, "int_no: %08X\terr_code: %08X\teip: %08x\tcs: %08x\n",
-	             old_esp->int_no, old_esp->err_code, old_esp->eip, old_esp->cs);
+		old_esp->int_no, old_esp->err_code, old_esp->eip, old_esp->cs);
 	printk_color(red, "eflags: %08x\tuser_esp: %08x\tss: %08x\n",
-	             old_esp->eflags, old_esp->user_esp, old_esp->user_ss);
+		old_esp->eflags, old_esp->user_esp, old_esp->user_ss);
 	printk_color(red, "addr: %08x, %08X\n", &old_esp->gs, &old_esp->user_ss);
 
 	cpu_hlt();
@@ -178,20 +178,20 @@ void debug(pt_regs_t * regs) {
 	uint32_t * old_esp = (uint32_t *)regs->old_esp;
 
 	// 取任务寄存器值->tr
-	__asm__ volatile ( "str %%ax"
-	                   : "=a" ( tr )
-	                   : "0" ( 0 ) );
+	__asm__ volatile ("str %%ax"
+	                  : "=a" (tr)
+	                  : "0" (0) );
 	printk_color(light_red, "Unuseable.\n");
 
 	printk_color(red, "eax 0x%08X\tebx 0x%08X\tecx 0x%08X\tedx 0x%08X\n",
-	             regs->eax, regs->ebx, regs->ecx, regs->edx);
+		regs->eax, regs->ebx, regs->ecx, regs->edx);
 	printk_color(red, "esi 0x%08X\tedi 0x%08X\tebp 0x%08X\tesp 0x%08X\n",
-	             regs->esi, regs->edi, regs->ebp, (uint32_t) regs->user_esp);
+		regs->esi, regs->edi, regs->ebp, (uint32_t)regs->user_esp);
 	printk_color(red, "ds 0x%08X\tes 0x%08X\tfs 0x%08X\tgs 0x%08X\n",
-	             regs->ds, regs->es, regs->fs, regs->gs);
+		regs->ds, regs->es, regs->fs, regs->gs);
 	printk_color(red, "EIP: 0x%08X\tEFLAGS: 0x%08X\tCS: 0x%08X\n",
-	             //old_esp[0], old_esp[1], old_esp[2]);
-	             old_esp[0], read_eflags(), old_esp[2]);
+		// old_esp[0], old_esp[1], old_esp[2]);
+		old_esp[0], read_eflags(), old_esp[2]);
 	return;
 }
 
@@ -246,11 +246,11 @@ void general_protection(pt_regs_t * regs) {
 void enable_irq(uint32_t irq_no) {
 	uint8_t mask = 0;
 	// printk_color(green, "enable_irq mask: %X", mask);
-	if (irq_no >= IRQ8) {
-		mask = ( ( inb(IO_PIC2C) ) & ( ~( 1 << ( irq_no % 8 ) ) ) );
+	if(irq_no >= IRQ8) {
+		mask = ( (inb(IO_PIC2C) ) & (~(1 << (irq_no % 8) ) ) );
 		outb(IO_PIC2C, mask);
 	} else {
-		mask = ( ( inb(IO_PIC1C) ) & ( ~( 1 << ( irq_no % 8 ) ) ) );
+		mask = ( (inb(IO_PIC1C) ) & (~(1 << (irq_no % 8) ) ) );
 		outb(IO_PIC1C, mask);
 	}
 }
@@ -258,11 +258,11 @@ void enable_irq(uint32_t irq_no) {
 void disable_irq(uint32_t irq_no) {
 	uint8_t mask = 0;
 	// printk_color(green, "disable_irq mask: %X", mask);
-	if (irq_no >= IRQ8) {
-		mask = ( ( inb(IO_PIC2C) ) | ( 1 << ( irq_no % 8 ) ) );
+	if(irq_no >= IRQ8) {
+		mask = ( (inb(IO_PIC2C) ) | (1 << (irq_no % 8) ) );
 		outb(IO_PIC2C, mask);
 	} else {
-		mask = ( ( inb(IO_PIC1C) ) | ( 1 << ( irq_no % 8 ) ) );
+		mask = ( (inb(IO_PIC1C) ) | (1 << (irq_no % 8) ) );
 		outb(IO_PIC1C, mask);
 	}
 }
--- src/arch/x86_64/mm/vmm.c
+++ src/arch/x86_64/mm/vmm.c
@@ -15,10 +15,10 @@ extern "C" {
 #include "cpu.hpp"
 
 // 内核页目录区域
-static pgd_t pgd_kernel[VMM_PGD_COUNT] __attribute__((aligned(VMM_PAGE_SIZE)));
+static pgd_t pgd_kernel[VMM_PGD_COUNT] __attribute__( (aligned(VMM_PAGE_SIZE) ) );
 
 // 内核页表区域
-static pte_t pte_kernel[VMM_PTE_COUNT] __attribute__ ((aligned(VMM_PAGE_SIZE)));
+static pte_t pte_kernel[VMM_PTE_COUNT] __attribute__( (aligned(VMM_PAGE_SIZE) ) );
 
 void vmm_init(void) {
 	// TODO
@@ -47,7 +47,7 @@ void vmm_init(void) {
 	// }
 	//
 
-	for(int i = 0; i<VMM_PAGE_COUNT; i++) {
+	for(int i = 0 ; i < VMM_PAGE_COUNT ; i++) {
 
 	}
 
@@ -57,16 +57,17 @@ void vmm_init(void) {
 	ptr_t pgd_idx = VMM_PGD_INDEX(0);
 
 	// 页目录
-	for (i = pgd_idx, j = 0; i < VMM_KERNEL_PAGE_COUNT + pgd_idx; i++, j++) {
+	for(i = pgd_idx, j = 0 ; i < VMM_KERNEL_PAGE_COUNT + pgd_idx ; i++, j++) {
+
 		// 此处是内核虚拟地址，MMU 需要物理地址，所以减去偏移，下同
-		pgd_kernel[i] = ((ptr_t)pte_kernel[j] - PAGE_OFFSET) | VMM_PAGE_PRESENT | VMM_PAGE_WRITE;
+		pgd_kernel[i] = ( (ptr_t)pte_kernel[j] - PAGE_OFFSET) | VMM_PAGE_PRESENT | VMM_PAGE_WRITE;
 	}
 	printk_debug("22222222222\n");
 	// 映射全部内存
 	uint32_t * pte = (uint32_t *)pte_kernel;
 	// 不映射第 0 页，便于跟踪 NULL 指针
 	// 0x80000, 512MB
-	for (i = 1; i < 0x38000000; i++) {
+	for(i = 1 ; i < 0x38000000 ; i++) {
 		pte[i] = (i << 12) | VMM_PAGE_PRESENT | VMM_PAGE_WRITE;
 	}
 	printk_debug("333333333333\n");
@@ -83,64 +84,68 @@ void vmm_init(void) {
 	return;
 }
 
-void map(pgd_t *pgd_now, ptr_t va, ptr_t pa, uint32_t flags) {
+void test() {
+
+}
+
+void map(pgd_t * pgd_now, ptr_t va, ptr_t pa, uint32_t flags) {
 	uint32_t pgd_idx = VMM_PGD_INDEX(va);
 	uint32_t pte_idx = VMM_PTE_INDEX(va);
 
-	pte_t *pte = (pte_t *)(pgd_now[pgd_idx] & VMM_PAGE_MASK);
-	if (!pte) {
+	pte_t * pte = (pte_t *)(pgd_now[pgd_idx] & VMM_PAGE_MASK);
+	if(!pte) {
 		pte = (pte_t *)pmm_alloc(1);
 		pgd_now[pgd_idx] = (ptr_t)pte | VMM_PAGE_PRESENT | VMM_PAGE_WRITE;
 
 		// 转换到内核线性地址并清 0
-		pte = (pte_t *)((ptr_t)pte + PAGE_OFFSET);
+		pte = (pte_t *)( (ptr_t)pte + PAGE_OFFSET);
 		bzero(pte, VMM_PAGE_SIZE);
 	} else {
 		// 转换到内核线性地址
-		pte = (pte_t *)((ptr_t)pte + PAGE_OFFSET);
+		pte = (pte_t *)( (ptr_t)pte + PAGE_OFFSET);
 	}
 
 	pte[pte_idx] = (pa & VMM_PAGE_MASK) | flags;
 
 	// 通知 CPU 更新页表缓存
-	__asm__ volatile ("invlpg (%0)" : : "a" (va));
+	__asm__ volatile ("invlpg (%0)" : : "a" (va) );
 	// __native_flush_tlb_single(va);
 }
 
-void unmap(pgd_t *pgd_now, ptr_t va) {
+void unmap(pgd_t * pgd_now, ptr_t va) {
 	uint32_t pgd_idx = VMM_PGD_INDEX(va);
 	uint32_t pte_idx = VMM_PTE_INDEX(va);
 
-	pte_t *pte = (pte_t *)(pgd_now[pgd_idx] & VMM_PAGE_MASK);
+	pte_t * pte = (pte_t *)(pgd_now[pgd_idx] & VMM_PAGE_MASK);
 
-	if (!pte) {
+	if(!pte) {
 		return;
 	}
 
 	// 转换到内核线性地址
-	pte = (pte_t *)((ptr_t)pte + PAGE_OFFSET);
+	pte = (pte_t *)( (ptr_t)pte + PAGE_OFFSET);
 
 	pte[pte_idx] = 0;
 
 	// 通知 CPU 更新页表缓存
-	__asm__ volatile ("invlpg (%0)" : : "a" (va));
+	__asm__ volatile ("invlpg (%0)" : : "a" (va) );
 	// __native_flush_tlb_single(va);
 }
 
-uint32_t get_mapping(pgd_t *pgd_now, ptr_t va, ptr_t pa) {
+uint32_t get_mapping(pgd_t * pgd_now, ptr_t va, ptr_t pa) {
 	uint32_t pgd_idx = VMM_PGD_INDEX(va);
 	uint32_t pte_idx = VMM_PTE_INDEX(va);
 
-	pte_t *pte = (pte_t *)(pgd_now[pgd_idx] & VMM_PAGE_MASK);
-	if (!pte) {
+	pte_t * pte = (pte_t *)(pgd_now[pgd_idx] & VMM_PAGE_MASK);
+	if(!pte) {
 		return 0;
 	}
 
 	// 转换到内核线性地址
-	pte = (pte_t *)((ptr_t)pte + PAGE_OFFSET);
+	pte = (pte_t *)( (ptr_t)pte + PAGE_OFFSET);
 
 	// 如果地址有效而且指针不为NULL，则返回地址
-	if (pte[pte_idx] != 0 && pa) {
+	if(pte[pte_idx] != 0 && pa) {
 		pa = pte[pte_idx] & VMM_PAGE_MASK;
 		return 1;
 	}
@@ -149,40 +154,40 @@ uint32_t get_mapping(pgd_t *pgd_now, ptr_t va, ptr_t pa) {
 }
 
 void switch_pgd(ptr_t pd) {
-	__asm__ volatile ("mov %0, %%cr3" : : "r" (pd));
+	__asm__ volatile ("mov %0, %%cr3" : : "r" (pd) );
 }
 
 void page_fault(pt_regs_t * regs) {
 #ifdef __x86_64__
 	uint64_t cr2;
-	asm volatile ( "movq %%cr2,%0" : "=r" ( cr2 ) );
+	asm volatile ("movq %%cr2,%0" : "=r" (cr2) );
 #else
 	uint32_t cr2;
-	asm volatile ( "mov %%cr2,%0" : "=r" ( cr2 ) );
+	asm volatile ("mov %%cr2,%0" : "=r" (cr2) );
 #endif
 	printk("Page fault at 0x%08X, virtual faulting address 0x%08X\n", regs->eip, cr2);
 	printk_err("Error code: 0x%08X\n", regs->err_code);
 
 	// bit 0 为 0 指页面不存在内存里
-	if ( !( regs->err_code & 0x1 ) )
+	if(!(regs->err_code & 0x1) )
 		printk_color(red, "Because the page wasn't present.\n");
 	// bit 1 为 0 表示读错误，为 1 为写错误
-	if (regs->err_code & 0x2)
+	if(regs->err_code & 0x2)
 		printk_err("Write error.\n");
 	else
 		printk_err("Read error.\n");
 	// bit 2 为 1 表示在用户模式打断的，为 0 是在内核模式打断的
-	if (regs->err_code & 0x4)
+	if(regs->err_code & 0x4)
 		printk_err("In user mode.\n");
 	else
 		printk_err("In kernel mode.\n");
 	// bit 3 为 1 表示错误是由保留位覆盖造成的
-	if (regs->err_code & 0x8)
+	if(regs->err_code & 0x8)
 		printk_err("Reserved bits being overwritten.\n");
 	// bit 4 为 1 表示错误发生在取指令的时候
-	if (regs->err_code & 0x10)
+	if(regs->err_code & 0x10)
 		printk_err("The fault occurred during an instruction fetch.\n");
-	while (1);
+	while(1);
 }
 
 #ifdef __cplusplus
