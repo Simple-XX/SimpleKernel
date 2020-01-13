47e5629de1f3b1b435fa079ca3653dc7d97da3b2
MRNIU <a6813140@hotmail.com>
2020-01-13 17:00:21 +0800

    - Update macor in vmm

--- src/arch/x86_64/mm/vmm.c
+++ src/arch/x86_64/mm/vmm.c
@@ -15,77 +15,16 @@ extern "C" {
 #include "cpu.hpp"
 
 // 内核页目录区域
-static pgd_t pgd_kernel[VMM_PGD_COUNT] __attribute__( (aligned(VMM_PAGE_SIZE) ) );
+static pgd_t pgd_kernel[VMM_PAGE_TABLES_PRE_PAGE_DIRECTORY] __attribute__( (aligned(VMM_PAGE_SIZE) ) );
 
 // 内核页表区域
-static pte_t pte_kernel[VMM_PTE_COUNT] __attribute__( (aligned(VMM_PAGE_SIZE) ) );
+static pte_t pte_kernel[VMM_PAGES_PRE_PAGE_TABLE] __attribute__( (aligned(VMM_PAGE_SIZE) ) );
 
 void vmm_init(void) {
-	// TODO
-	// 使用 vmm.h 中的结构体重写该部分
 
-	// 首先需要这么几个东西
-	// 页目录，页表，页表项
-	// 页目录用于保存页表地址
-	// 页表每个项对应一个页表
-	// 页表项保存了相关属性信息，见 page 结构体
-
-
-	// 页表数组指针
-	// pte_t (*pte_kern)[PTE_SIZE] = (pte_t (*)[PTE_SIZE])pte_addr;
-	//
-	// // 构造页目录(MMU需要的是物理地址，此处需要减去偏移)
-	// uint32_t pgd_idx = PGD_INDEX(PAGE_OFFSET);
-	// for (uint32_t i = pgd_idx; i < pgd_idx + PTE_COUNT; ++i) {
-	// 	pgd_kern[i] = ((uint32_t)ka_to_pa(pte_kern[i])) | PAGE_PRESENT | PAGE_WRITE;
-	// }
-	//
-	// // 构造页表映射，内核 0xC0000000～0xF8000000 映射到 物理 0x00000000～0x38000000 (物理内存前896MB)
-	// uint32_t *pte_start = (uint32_t *)(pte_addr + PTE_SIZE * pgd_idx);
-	// for (uint32_t i = 0; i < PTE_SIZE * PTE_COUNT; ++i) {
-	// 	pte_start[i] = (i << 12) | PAGE_PRESENT | PAGE_WRITE;
-	// }
-	//
-
-	for(int i = 0 ; i < VMM_PAGE_COUNT ; i++) {
-
-	}
-
-	ptr_t i, j;
-	// 0xC0000000 这个地址在页目录的索引，0x0300
-	// ptr_t pgd_idx = VMM_PGD_INDEX(PAGE_OFFSET);
-	ptr_t pgd_idx = VMM_PGD_INDEX(0);
-
-	// 页目录
-	for(i = pgd_idx, j = 0 ; i < VMM_KERNEL_PAGE_COUNT + pgd_idx ; i++, j++) {
-
-		// 此处是内核虚拟地址，MMU 需要物理地址，所以减去偏移，下同
-		pgd_kernel[i] = ( (ptr_t)pte_kernel[j] - PAGE_OFFSET) | VMM_PAGE_PRESENT | VMM_PAGE_WRITE;
-	}
-	printk_debug("22222222222\n");
-	// 映射全部内存
-	uint32_t * pte = (uint32_t *)pte_kernel;
-	// 不映射第 0 页，便于跟踪 NULL 指针
-	// 0x80000, 512MB
-	for(i = 1 ; i < 0x38000000 ; i++) {
-		pte[i] = (i << 12) | VMM_PAGE_PRESENT | VMM_PAGE_WRITE;
-	}
-	printk_debug("333333333333\n");
-	ptr_t pgd_kernel_phy_addr = (ptr_t)pgd_kernel - PAGE_OFFSET;
-
-	// 注册页错误中断的处理函数 ( 14 是页故障的中断号 )
-	register_interrupt_handler(INT_PAGE_FAULT, &page_fault);
-
-	printk_debug("666666666666666\n");
-	switch_pgd(pgd_kernel_phy_addr);
-	printk_debug("777777777777777\n");
 	printk_info("vmm_init\n");
 
 	return;
-}
-
-void test() {
-
 }
 
 void map(pgd_t * pgd_now, ptr_t va, ptr_t pa, uint32_t flags) {
@@ -95,14 +34,14 @@ void map(pgd_t * pgd_now, ptr_t va, ptr_t pa, uint32_t flags) {
 	pte_t * pte = (pte_t *)(pgd_now[pgd_idx] & VMM_PAGE_MASK);
 	if(!pte) {
 		pte = (pte_t *)pmm_alloc(1);
-		pgd_now[pgd_idx] = (ptr_t)pte | VMM_PAGE_PRESENT | VMM_PAGE_WRITE;
+		pgd_now[pgd_idx] = (ptr_t)pte | VMM_PAGE_PRESENT | VMM_PAGE_RW;
 
 		// 转换到内核线性地址并清 0
-		pte = (pte_t *)( (ptr_t)pte + PAGE_OFFSET);
+		pte = (pte_t *)( (ptr_t)pte + KERNEL_BASE);
 		bzero(pte, VMM_PAGE_SIZE);
 	} else {
 		// 转换到内核线性地址
-		pte = (pte_t *)( (ptr_t)pte + PAGE_OFFSET);
+		pte = (pte_t *)( (ptr_t)pte + KERNEL_BASE);
 	}
 
 	pte[pte_idx] = (pa & VMM_PAGE_MASK) | flags;
@@ -123,7 +62,7 @@ void unmap(pgd_t * pgd_now, ptr_t va) {
 	}
 
 	// 转换到内核线性地址
-	pte = (pte_t *)( (ptr_t)pte + PAGE_OFFSET);
+	pte = (pte_t *)( (ptr_t)pte + KERNEL_BASE);
 
 	pte[pte_idx] = 0;
 
@@ -142,7 +81,7 @@ uint32_t get_mapping(pgd_t * pgd_now, ptr_t va, ptr_t pa) {
 	}
 
 	// 转换到内核线性地址
-	pte = (pte_t *)( (ptr_t)pte + PAGE_OFFSET);
+	pte = (pte_t *)( (ptr_t)pte + KERNEL_BASE);
 
 	// 如果地址有效而且指针不为NULL，则返回地址
 	if(pte[pte_idx] != 0 && pa) {
--- src/include/mem/vmm.h
+++ src/include/mem/vmm.h
@@ -13,147 +13,78 @@ extern "C" {
 #include "intr/include/intr.h"
 #include "vmm.h"
 
-// 内核的偏移地址
-#define VMM_PAGE_OFFSET    PMM_PAGE_OFFSET
-
-// 页掩码，用于 4KB 对齐
-#define VMM_PAGE_MASK    PMM_PAGE_MASK
-
-// 页大小
-#define VMM_PAGE_SIZE    PMM_PAGE_SIZE
-
-// 页目录项大小
-#define VMM_PGD_SIZE    (sizeof(pte_t) )
-
-// 页表项大小
-#define VMM_PTE_SIZE    (sizeof(ptr_t) )
-
-#ifdef __x86_64__
-// 虚拟地址位
-#define VMM_VA_BITS     64
-// 最大虚拟地址
-#define VMEM_SIZE       (1 << VMM_VA_BITS)
-#else
-// 二级索引
-// 虚拟地址位数
-#define VMM_VA_BITS     32
-// 最大虚拟地址, 4GB
-#define VMEM_SIZE       (1 << VMM_VA_BITS)
-// 页内偏移位数, log2(VMM_PAGE_SIZE)
-#define VMM_PG_OFFSET_BITS      (12)
-// 页数量
-#define VMM_PAGE_COUNT      (1 << VMM_PG_OFFSET_BITS)
-// 每页可以放入多少页表项
+// 页大小，一页能映射多少 Byte 内存 2^12
+#define VMM_PAGE_SIZE       (PMM_PAGE_SIZE)
+// 页掩码
+#define VMM_PAGE_MASK       (PMM_PAGE_MASK)
+// 页表项 sizeof(pte_t) 大小 = 4B 2^2
+#define VMM_PTE_SIZE        (sizeof(pte_t) )
+// 每页能映射多少页表项 = 页大小/页表项大小 2^10
 #define VMM_PTE_PRE_PAGE    (VMM_PAGE_SIZE / VMM_PTE_SIZE)
-// 页表位数, log2(VMM_PTE_PRE_PAGE)
-#define VMM_PTE_BITS    (10)
-// 页表数量
-#define VMM_PTE_COUNT   (1 << VMM_PTE_BITS)
-// 每页可以放入多少页目录项
-#define VMM_PGD_PRE_PAGE    (VMM_PAGE_SIZE / VMM_PGD_SIZE)
-// 页目录位数, log2(VMM_PGD_PRE_PAGE)
-#define VMM_PGD_BITS    (10)
-// 页目录数量
-#define VMM_PGD_COUNT   (1 << VMM_PGD_BITS)
-#endif
+// 页目录项 sizeof(pgd_t) 大小 = 4B 2^2
+#define VMM_PGDE_SIZE       (sizeof(pgd_t) )
+// 每页能映射多少页目录项 = 页大小/页目录项大小 2^10
+#define VMM_PGDE_PRE_PAGE   (VMM_PAGE_SIZE / VMM_PGDE_SIZE)
+// 每个页表能映射多少页 = 页大小/页表项大小 2^10
+#define VMM_PAGES_PRE_PAGE_TABLE            (VMM_PAGE_SIZE / VMM_PTE_SIZE)
+// 每个页目录能映射多少页表 = 页大小/页目录项大小 2^10
+#define VMM_PAGE_TABLES_PRE_PAGE_DIRECTORY  (VMM_PAGE_SIZE / VMM_PGDE_SIZE)
+// 每个页目录能映射多少页 = 页表数量*每个页表能映射多少页 2^20
+#define VMM_PAGES_PRE_PAGE_DIRECTORY        (VMM_PAGE_TABLES_PRE_PAGE_DIRECTORY * VMM_PAGES_PRE_PAGE_TABLE)
+// 页表大小，一页表能映射多少 Byte 内存 = 页表项数量*页表项映射大小 2^22
+#define VMM_PAGE_TABLE_SIZE (VMM_PAGES_PRE_PAGE_TABLE * VMM_PAGE_SIZE)
+// 页目录大小，一页目录能映射多少 Byte 内存 = 页表数量*页表映射大小 2^32
+#define VMM_PAGE_DIRECTORY_SIZE     (VMM_PAGE_TABLES_PRE_PAGE_DIRECTORY * VMM_PAGE_TABLE_SIZE)
+// 虚拟内存位数
+#define VMM_VMEM_BITS       (32U)
+// 虚拟内存大小
+#define VMM_VMEM_SIZE       (1U << (VMM_VMEM_BITS - 1) )
+// 映射全部虚拟内存需要的页数 = 虚拟内存大小/页大小 2^20
+#define VMM_PAGES_TOTAL     (VMM_VMEM_SIZE / VMM_PAGE_SIZE)
+// 映射全部虚拟内存需要的页表数 = 虚拟内存大小/页表大小 2^12
+#define VMM_PAGE_TABLES_TOTAL       (VMM_VMEM_SIZE / VMM_PAGE_TABLE_SIZE)
+// 映射全部虚拟内存需要的页目录数 = 虚拟内存大小/页目录大小 2^0
+#define VMM_PAGE_DIRECTORIES_TOTAL  (VMM_VMEM_SIZE / VMM_PAGE_DIRECTORY_SIZE)
 
-// 虚拟页数量
-// 0x100000000/0x1000 = 0x100000(32bits without PSE and PAE)
-#define VMM_PAGE_COUNT      (VMEM_SIZE / VMM_PAGE_SIZE)
-
-// P-- 位 0 是存在 (Present) 标志，用于指明表项对地址转换是否有效。
 // P = 1 表示有效； P = 0 表示无效。
-#define VMM_PAGE_PRESENT    0x00000001
+#define VMM_PAGE_PRESENT    (0x00000001)
 
-// R/W-- 位 1 是读 / 写 (Read/Write) 标志。如果等于 1  表示页面可以被读、写或执行。
 // 如果为 0  表示页面只读或可执行。
-#define VMM_PAGE_WRITE    0x00000002
+#define VMM_PAGE_RW      (0x00000002)
 
 // U/S-- 位 2 是用户 / 超级用户 (User/Supervisor) 标志。
 // 如果为 1  那么运行在任何特权级上的程序都可以访问该页面。
+#define VMM_PAGE_USER       (0x00000004)
 // 如果为 0  那么页面只能被运行在超级用户特权级 (0,1 或 2)  的程序访问。
-#define VMM_PAGE_USER    0x00000004
+#define VMM_PAGE_KERNEL     (0x00000000)
+
+// 线性地址转换为物理地址
+#define VMM_LA_TO_PA(la)    (la - KERNEL_BASE)
 
 // 获取一个地址的页目录，高 10 位
-#define VMM_PGD_INDEX(x) ( ( (x) >> 22) & 0x03FF)
+#define VMM_PGD_INDEX(x)        ( ( (x) >> 22) & 0x03FF)
 
 // 获取一个地址的页表，中间 10 位
-#define VMM_PTE_INDEX(x) ( ( (x) >> 12) & 0x03FF)
+#define VMM_PTE_INDEX(x)        ( ( (x) >> 12) & 0x03FF)
 
 // 获取一个地址的页內偏移，低 12 位
-#define VMM_OFFSET_INDEX(x) ( (x) & 0x0FFF)
+#define VMM_OFFSET_INDEX(x)     ( (x) & 0x0FFF)
 
 // 映射内核页需要的页数 0x40000
 // #define VMM_KERNEL_PAGE_COUNT   (0x40000000/VMM_PAGE_SIZE)
 #define VMM_KERNEL_PAGE_COUNT   (0x20000000 / VMM_PAGE_SIZE)
 
-// 页全局目录
+// 页全局目录项
 typedef ptr_t pgd_t;
 
-// 页上级目录
+// 页上级目录项
 typedef ptr_t pmd_t;
 
-// 页中间目录
+// 页中间目录项
 typedef ptr_t pud_t;
 
-// 页表
+// 页表项
 typedef ptr_t pte_t;
-
-typedef
-        struct page {
-	// must be 1 to map a 4-KByte page
-	uint32_t present    : 1;
-	// if 0, writes may not be allowed to the 4-KByte page referenced by this entry
-	uint32_t rw         : 1;
-	// if 0, user-mode accesses are not allowed to the 4-KByte page referenced by this entry
-	uint32_t user       : 1;
-	//  indirectly determines the memory type used to access the 4-KByte page referenced by this entry
-	uint32_t pwt        : 1;
-	// indirectly determines the memory type used to access the 4-KByte page referenced by this entry
-	uint32_t pcd        : 1;
-	// indicates whether software has accessed the 4-KByte page referenced by this entry
-	uint32_t accessed   : 1;
-	// indicates whether software has written to the 4-KByte page referenced by this entry
-	uint32_t dirty      : 1;
-	// If the PAT is supported, indirectly determines the memory type used to access
-	// the 4-KByte page referenced by this entry (see Section 4.9.2); otherwise, reserved (must be 0)1
-	uint32_t pat        : 1;
-	// Global; if CR4.PGE = 1, determines whether the translation is global (see Section 4.10); ignored otherwise
-	uint32_t global     : 1;
-	// ignore
-	uint32_t ignore     : 3;
-	// Physical address of the 4-KByte page referenced by this entry
-	uint32_t addr       : 20;
-} page_t;
-
-typedef
-        struct page_table {
-	// must be 1 to reference a page table
-	uint32_t present    : 1;
-	// if 0, writes may not be allowed to the 4-MByte region controlled by this entry
-	uint32_t rw         : 1;
-	//  if 0, user-mode accesses are not allowed to the 4-MByte region controlled by this entry
-	uint32_t user       : 1;
-	//  indirectly determines the memory type used to access the page table referenced by this entry
-	uint32_t pwt        : 1;
-	// indirectly determines the memory type used to access the page table referenced by this entry
-	uint32_t pcd        : 1;
-	// indicates whether this entry has been used for linear-address translation
-	uint32_t accessed   : 1;
-	// ignore
-	uint32_t ignore1    : 1;
-	// If CR4.PSE = 1, must be 0 (otherwise, this entry maps a 4-MByte page; see Table 4-4); otherwise, ignored
-	uint32_t ps         : 1;
-	// ignore
-	uint32_t ignore2    : 4;
-	// Physical address of 4-KByte aligned page table referenced by this entry
-	uint32_t addr       : 20;
-} page_table_t;
-
-typedef
-        struct page_directory {
-	page_table_t page_table[VMM_PTE_COUNT];
-} page_directory_t;
 
 // 初始化虚拟内存管理
 void vmm_init(void);
