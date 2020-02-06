5ebfa6b3f99e65bd4ae0ec038539de2d0461ebf5
MRNIU <a6813140@hotmail.com>
2020-02-06 11:02:58 +0800

    - Update devlog - Working on slab:alloc - Fix bug at vmm.c:map()

--- src/test/test.c
+++ src/test/test.c
@@ -18,7 +18,7 @@ bool test_pmm(void) {
 	allc_addr = pmm_alloc(9000);
 	printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr);
 	printk_test("Free pages count: %d\n", pmm_free_pages_count() );
-	pmm_free_page(allc_addr, 9000);
+	pmm_free(allc_addr, 9000);
 	printk_test("Free!\n");
 	printk_test("Free pages count: %d\n", pmm_free_pages_count() );
 	allc_addr = pmm_alloc(1);
@@ -47,30 +47,30 @@ bool test_sched() {
 
 bool test_heap() {
 	printk_test("Test Heap kmalloc :\n");
-	ptr_t allc_addr1 = (ptr_t)kmalloc(9000);
+	ptr_t allc_addr1 = (ptr_t)kmalloc(1);
 	printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr1);
-	ptr_t allc_addr2 = (ptr_t)kmalloc(1);
-	printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr2);
-	ptr_t allc_addr3 = (ptr_t)kmalloc(1);
-	printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr3);
-	ptr_t allc_addr4 = (ptr_t)kmalloc(1);
-	printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr4);
-	printk_test("Test Heap kfree :\n");
-	kfree( (ptr_t)allc_addr1);
-	kfree( (ptr_t)allc_addr2);
-	kfree( (ptr_t)allc_addr3);
-	kfree( (ptr_t)allc_addr4);
-	ptr_t new_addr = (ptr_t)kmalloc(9000);
-	printk_test("New Alloc Physical Addr: 0x%08X\n", new_addr);
+	// ptr_t allc_addr2 = (ptr_t)kmalloc(1);
+	// printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr2);
+	// ptr_t allc_addr3 = (ptr_t)kmalloc(1);
+	// printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr3);
+	// ptr_t allc_addr4 = (ptr_t)kmalloc(1);
+	// printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr4);
+	// printk_test("Test Heap kfree :\n");
+	// kfree( (ptr_t)allc_addr1);
+	// kfree( (ptr_t)allc_addr2);
+	// kfree( (ptr_t)allc_addr3);
+	// kfree( (ptr_t)allc_addr4);
+	// ptr_t new_addr = (ptr_t)kmalloc(9000);
+	// printk_test("New Alloc Physical Addr: 0x%08X\n", new_addr);
 
 	return true;
 }
 
 bool test(void) {
-	test_pmm();
+	// test_pmm();
 	// test_vmm();
 	// test_libc();
-	// test_heap();
+	test_heap();
 	// test_task();
 	// test_sched();
 	return true;
