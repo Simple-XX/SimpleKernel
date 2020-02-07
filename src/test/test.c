c18b62f45310111a187d1f8f6b2fe6ba0572bb88
MRNIU <a6813140@hotmail.com>
2020-02-07 16:25:16 +0800

    - Update devlog - Fix bug that slab:alloc used be skip init block

--- src/test/test.c
+++ src/test/test.c
@@ -21,19 +21,18 @@ extern "C" {
 
 bool test_pmm(void) {
 	ptr_t allc_addr = 0;
-	printk_test("Test Physical Memory Alloc :\n");
+	printk_test("Test pmm_alloc :\n");
 	allc_addr = pmm_alloc(1);
-	printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr);
+	printk_test("pmm_alloc addr: 0x%08X\n", allc_addr);
 	allc_addr = pmm_alloc(9000);
-	printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr);
+	printk_test("pmm_alloc addr: 0x%08X\n", allc_addr);
 	printk_test("Free pages count: %d\n", pmm_free_pages_count() );
 	pmm_free(allc_addr, 9000);
-	printk_test("Free!\n");
 	printk_test("Free pages count: %d\n", pmm_free_pages_count() );
 	allc_addr = pmm_alloc(1);
-	printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr);
+	printk_test("pmm_alloc addr: 0x%08X\n", allc_addr);
 	allc_addr = pmm_alloc(1);
-	printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr);
+	printk_test("pmm_alloc addr: 0x%08X\n", allc_addr);
 	printk_test("Free pages count: %d\n", pmm_free_pages_count() );
 	return true;
 }
@@ -56,21 +55,21 @@ bool test_sched() {
 
 bool test_heap() {
 	printk_test("Test Heap kmalloc :\n");
-	ptr_t allc_addr1 = (ptr_t)kmalloc(9000);
-	printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr1);
-	ptr_t allc_addr2 = (ptr_t)kmalloc(1);
-	printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr2);
+	ptr_t allc_addr1 = (ptr_t)kmalloc(1);
+	printk_test("kmalloc heap addr: 0x%08X\n", allc_addr1);
+	ptr_t allc_addr2 = (ptr_t)kmalloc(9000);
+	printk_test("kmalloc heap addr: 0x%08X\n", allc_addr2);
 	ptr_t allc_addr3 = (ptr_t)kmalloc(4095);
-	printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr3);
+	printk_test("kmalloc heap addr: 0x%08X\n", allc_addr3);
 	ptr_t allc_addr4 = (ptr_t)kmalloc(12);
-	printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr4);
+	printk_test("kmalloc heap addr: 0x%08X\n", allc_addr4);
 	printk_test("Test Heap kfree :\n");
 	kfree( (ptr_t)allc_addr1);
 	kfree( (ptr_t)allc_addr2);
 	kfree( (ptr_t)allc_addr3);
 	kfree( (ptr_t)allc_addr4);
 	ptr_t new_addr = (ptr_t)kmalloc(9000);
-	printk_test("New Alloc Physical Addr: 0x%08X\n", new_addr);
+	printk_test("New kmalloc heap addr: 0x%08X\n", new_addr);
 	return true;
 }
 
