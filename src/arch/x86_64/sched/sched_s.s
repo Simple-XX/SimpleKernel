f622c5cda464a4bf1ac64f892efbe7b8bbd82f97
MRNIU <a6813140@hotmail.com>
2020-02-22 20:59:03 +0800

    Update - Update devlog - Add SWITCH_TO macro that will be used in the future - Working on bugs

--- src/arch/x86_64/sched/sched_s.s
+++ src/arch/x86_64/sched/sched_s.s
@@ -9,20 +9,17 @@
 switch_to:
     // push $10
     // call print_stack
-
     # 保存 prev
     mov -4(%esp), %eax
     # 保存 eip
     // mov -4(%esp), %edx
     // mov %edx, 0(%eax)
-
     # 保存 esp
     mov %esp, 4(%eax)
     # 保存 ebp
     mov %ebp, 8(%eax)
     # 保存 ebx
     mov %ebx, 12(%eax)
-
     # 保存 ecx
     mov %ecx, 16(%eax)
     # 保存 edx
@@ -31,8 +28,12 @@ switch_to:
     mov %esi, 24(%eax)
     # 保存 edi
     mov %edi, 28(%eax)
+    // push $5
+    // call print_stack
     # 恢复 curr
     mov -8(%esp), %eax
+    // push $5
+    // call print_stack
     # 填充 eip
     // mov 0(%eax), %edx
     // mov %edx, -8(%esp)
@@ -50,4 +51,7 @@ switch_to:
     mov 24(%eax), %esi
     # 填充 edi
     mov 28(%eax), %edi
+    // push $10
+    // call print_stack
+    // hlt
     ret
