80cb60598b20152b4af97585cb19598a5a8a3d7f
MRNIU <a6813140@hotmail.com>
2020-07-01 21:53:27 +0800

    update shell script, unavailable

--- tools/x86_64-elf-binutils.sh
+++ tools/x86_64-elf-binutils.sh
@@ -6,21 +6,21 @@
 #!/usr/bin/env bash
 
 if ! [ -x "$(command -v wget)" ]; then
-  echo 'Error: wget is not installed.'
-  exit 1
+    echo 'Error: wget is not installed.'
+    exit 1
 elif ! [ -x "$(command -v tar)" ]; then
-  echo 'Error: tar is not installed.'
-  exit 1
+    echo 'Error: tar is not installed.'
+    exit 1
 else
-  wget https://ftp.gnu.org/gnu/binutils/binutils-2.33.1.tar.xz
-  tar zxvf binutils-2.33.1.tar.xz
-  cd binutils-2.33.1
-  ./configure \
-    --target=x86_64-elf \
-    --enable-targets=all \
-    --enable-multilib \
-    --enable-64-bit-bfd \
-    --disable-werror
-  make
-  make install
+    wget https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.xz
+    tar zxvf binutils-2.34.tar.xz
+    cd binutils-2.34
+    ./configure \
+        --target=x86_64-elf \
+        --enable-targets=all \
+        --enable-multilib \
+        --enable-64-bit-bfd \
+        --disable-werror
+    make
+    make install
 fi
