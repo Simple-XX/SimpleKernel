d26e06fa940a6d88108490369eae2196124d1e59
MRNIU <a6813140@hotmail.com>
2020-07-01 21:53:27 +0800

    update shell script, unavailable

--- tools/i386-elf-binutils.sh
+++ tools/i386-elf-binutils.sh
@@ -6,20 +6,20 @@
 #!/usr/bin/env bash
 
 if ! [ -x "$(command -v wget)" ]; then
-  echo 'Error: wget is not installed.'
-  exit 1
+    echo 'Error: wget is not installed.'
+    exit 1
 elif ! [ -x "$(command -v tar)" ]; then
-  echo 'Error: tar is not installed.'
-  exit 1
+    echo 'Error: tar is not installed.'
+    exit 1
 else
-  wget https://ftp.gnu.org/gnu/binutils/binutils-2.31.1.tar.xz
-  tar xvJf binutils-2.31.1.tar.xz
-  cd binutils-2.31.1
-  ./configure \
-    --target=i386-elf \
-    --disable-multilib \
-    --disable-nls \
-    --disable-werror
-  make
-  make install
+    wget https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.xz
+    tar xvJf binutils-2.34.tar.xz
+    cd binutils-2.34
+    ./configure \
+        --target=i386-elf \
+        --disable-multilib \
+        --disable-nls \
+        --disable-werror
+    make
+    make install
 fi
--- tools/setup.sh
+++ tools/setup.sh
@@ -5,34 +5,11 @@
 
 #!/usr/bin/env bash
 # shell 执行出错时终止运行
-set -e
+# set -e
 # 输出实际执行内容
 # set -x
 
-TARGET="x86_64-elf"
-# ARCH: i386, x86_64
-ARCH="i386"
-# 虚拟机
-SIMULATOR="bochs"
-# bochs 配置文件
-bochsrc="bochsrc.txt"
-# 内核映像
-kernel='./src/kernel.bin'
-bootloader='./src/bootloader.bin'
-# 软盘
-disk='simplekernel.img'
-# 安装目录
-boot_folder='./boot_folder/boot/'
-# 挂载目录
-folder='./boot_folder'
-# 工具目录
-tool='./tools'
-iso_boot_grub='./iso/boot/grub'
-iso_boot='./iso/boot/'
-iso='./simplekernel.iso'
-iso_folder='./iso/'
-# 判断操作系统类型
-OS=`uname -s`
+source ./env.sh
 
 # 检测环境，如果没有安装需要的软件，则安装
 if ! [ -x "$(command -v ${SIMULATOR})" ]; then
@@ -65,6 +42,18 @@ if ! [ -x "$(command -v ${TARGET}-gcc)" ]; then
     fi
 fi
 
+if [ ${OS} == "Darwin" ]; then
+    if ! [ -x "$(command -v i386-elf-grub-file)" ]; then
+        echo 'Error: i386-elf-grub is not installed.'
+        echo 'Install i386-elf-grub...'
+        brew install i386-elf-grub
+    fi
+elif [ ${OS} == "Linux" ]; then
+    if ! [ -x "$(command -v grub-file)" ]; then
+        shell ${tool}/grub.sh
+    fi
+fi
+
 if ! [ -x "$(command -v i386-elf-grub-file)" ]; then
     echo 'Error: i386-elf-grub is not installed.'
     echo 'Install i386-elf-grub...'
@@ -84,41 +73,3 @@ if ! [ -x "$(command -v xorriso)" ]; then
         shell ${tool}/xorriso.sh
     fi
 fi
-
-# 重新编译
-cd src/
-make remake
-cd ../
-
-if i386-elf-grub-file --is-x86-multiboot2 ${kernel}; then
-    echo Multiboot2 Confirmed!
-elif [ ${ARCH} == "x86_64" ]; then
-    if i386-elf-grub-file --is-x86-multiboot2 ${bootloader}; then
-        echo Multiboot2 Confirmed!
-    else
-        echo The File is Not Multiboot.
-        exit
-    fi
-fi
-
-rm -rf -f ${iso_boot}/*
-cp ${kernel} ${iso_boot}
-mkdir ${iso_boot_grub}
-touch ${iso_boot_grub}/grub.cfg
-if [ ${ARCH} == "x86_64" ]; then
-    cp ${bootloader} ${iso_boot}
-    echo 'set timeout=15
-    set default=0
-    menuentry "SimpleKernel" {
-       multiboot2 /boot/bootloader.bin
-       module /boot/kernel.bin "KERNEL_BIN"
-   }' >${iso_boot_grub}/grub.cfg
-else
-    echo 'set timeout=15
-    set default=0
-    menuentry "SimpleKernel" {
-       multiboot2 /boot/kernel.bin "KERNEL_BIN"
-   }' >${iso_boot_grub}/grub.cfg
-fi
-i386-elf-grub-mkrescue -o ${iso} ${iso_folder}
-${SIMULATOR} -q -f ${bochsrc} -rc ./tools/bochsinit
