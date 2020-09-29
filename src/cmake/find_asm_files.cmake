be23012f8b6f2a0d48816ee0dc0bd1f83148028f
MRNIU <a6813140@hotmail.com>
2020-09-29 23:50:49 +0800

    refactor(cmake): 现在可以使用 cmake 进行构建了 refactor(arch_init): 移动文件

---
+++ src/cmake/find_asm_files.cmake
@@ -0,0 +1,9 @@
+
+# This file is a part of SimpleXX/SimpleKernel (https://github.com/SimpleXX/SimpleKernel).
+# 
+# find_asm_files.cmake for SimpleXX/SimpleKernel.
+
+macro(find_asm_source_files OutValue Directory)
+    file(GLOB ${OutValue} LIST_DIRECTORIES false ${Directory}/*.S)
+    message(STATUS "found asm files: ${${OutValue}}")
+endmacro()
