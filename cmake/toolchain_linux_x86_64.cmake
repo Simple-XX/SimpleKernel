d4c5c38331d735bb82057b1a4c78700662bf646c
MRNIU <a6813140@hotmail.com>
2020-09-30 17:27:25 +0800

    refactor(docs): move pref(build): 准备使用 cmake 完成工具链的配置

--- cmake/toolchain_linux_x86_64.cmake
+++ cmake/toolchain_linux_x86_64.cmake
@@ -8,15 +8,15 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-find_program(LinuxX86_64 gcc REQUIRED)
+find_program(LinuxX86_64 x86_64-elf-gcc REQUIRED)
 if (NOT LinuxX86_64)
-    message(FATAL_ERROR "gcc not found.\n"
+    message(FATAL_ERROR "x86_64-elf-gcc not found.\n"
             "run `brew install x86_64-elf-gcc` to install the toolchain")
 else ()
-    message(STATUS "found gcc ${LinuxX86_64}")
+    message(STATUS "found x86_64-elf-gcc ${LinuxX86_64}")
 endif ()
 
-set(TOOLCHAIN_PREFIX )
+set(TOOLCHAIN_PREFIX)
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
 set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy)
