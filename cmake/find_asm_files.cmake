c6a248ddf737daf64db8eb0d26862b2b747eaaea
MRNIU <a6813140@hotmail.com>
2020-10-02 02:42:56 +0800

    pref(build): 大小写问题，更完善的工具

--- cmake/find_asm_files.cmake
+++ cmake/find_asm_files.cmake
@@ -4,7 +4,6 @@
 # find_asm_files.cmake for SimpleXX/SimpleKernel.
 
 macro(find_asm_source_files OutValue Directory)
-    file(GLOB ${OutValue} LIST_DIRECTORIES false ${Directory}/*.s)
-    file(GLOB ${OutValue} LIST_DIRECTORIES false ${Directory}/*.S)
+    file(GLOB ${OutValue} LIST_DIRECTORIES false ${Directory}/*.[sS])
     message(STATUS "found asm files: ${${OutValue}}")
 endmacro()
