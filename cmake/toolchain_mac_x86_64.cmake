5cdfb9e0d03624acea9a8aac372fb1ce219fa41f
MRNIU <a6813140@hotmail.com>
2020-10-01 20:36:05 +0800

    pref(build): 优化工具链配置

--- cmake/toolchain_mac_x86_64.cmake
+++ cmake/toolchain_mac_x86_64.cmake
@@ -4,19 +4,28 @@
 # toolchain_mac_x86_64.cmake for SimpleXX/SimpleKernel.
 
 set(CMAKE_SYSTEM_NAME Darwin)
-
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-find_program(MacX86_64 x86_64-elf-gcc REQUIRED)
-if (NOT MacX86_64)
+# GCC
+find_program(X86_64 x86_64-elf-gcc REQUIRED)
+if (NOT X86_64)
     message(FATAL_ERROR "x86_64-elf-gcc not found.\n"
-            "run `brew install x86_64-elf-gcc` to install the toolchain")
+            "Run `brew install x86_64-elf-gcc` to install the toolchain.")
 else ()
-    message(STATUS "found x86_64-elf-gcc ${MacX86_64}")
+    message(STATUS "Found x86_64-elf-gcc ${X86_64}.")
 endif ()
 
 set(TOOLCHAIN_PREFIX x86_64-elf-)
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
 set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy)
+
+# Grub
+find_program(Grub grub-file REQUIRED)
+if (NOT Grub)
+    message(FATAL_ERROR "grub-file not found.\n"
+            "Use grub4mac.sh to install grub.")
+else ()
+    message(STATUS "Found grub-file ${Grub}")
+endif ()
