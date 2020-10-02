7430c5611d1f4c4d09473ca6556186d598c1ea20
MRNIU <a6813140@hotmail.com>
2020-10-02 19:06:23 +0800

    fix(kernel): 原因是忘了开中断

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -9,9 +9,11 @@ extern "C" {
 #endif
 
 #include "include/kernel.h"
+#include "cpu.hpp"
 
 // 内核入口
 void kernel_main() {
+    cpu_cli();
     // 系统初始化
     debug_init();
     // 控制台初始化
@@ -25,6 +27,7 @@ void kernel_main() {
     // 键盘初始化
     keyboard_init();
     showinfo();
+    cpu_sti();
     while (1) {
         ;
     }
