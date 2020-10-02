f60f72d01a93210883f623641a4d012015e4e8de
MRNIU <a6813140@hotmail.com>
2020-10-02 17:36:50 +0800

    sync(ALL): 同步新的代码

---
+++ run.sh
@@ -0,0 +1,50 @@
+
+# This file is a part of Simple-XX/SimpleKernel (https://github.com/Simple-XX/SimpleKernel).
+#
+# setup.sh for Simple-XX/SimpleKernel.
+
+#!/bin/bash
+# shell 执行出错时终止运行
+set -e
+# 输出实际执行内容
+# set -x
+
+source ./tools/env.sh
+export PATH="${GRUB_PATH}:$PATH"
+
+# 重新编译
+mkdir -p ./build/
+rm -rf ./build/*
+cd ./build
+cmake -DCMAKE_TOOLCHAIN_FILE=./cmake/${TOOLS} -DPLATFORM=${SIMULATOR} -DARCH=${ARCH} ..
+make
+cd ../
+
+if ${GRUB_PATH}/grub-file --is-x86-multiboot2 ${kernel}; then
+    echo Multiboot2 Confirmed!
+else
+    echo The File is Not Multiboot.
+    exit
+fi
+
+# 检测路径是否合法，发生过 rm -rf -f /* 的惨剧
+if [ "${iso_boot}" == "" ]; then
+    echo iso_boot path error.
+else
+    rm -rf -f ${iso_boot}/*
+fi
+
+cp ${kernel} ${iso_boot}
+mkdir ${iso_boot_grub}
+touch ${iso_boot_grub}/grub.cfg
+
+if [ ${ARCH} == "x86_64" ]; then
+    echo 'set timeout=15
+    set default=0
+    menuentry "SimpleKernel" {
+       multiboot2 /boot/kernel.bin "KERNEL_BIN"
+   }' >${iso_boot_grub}/grub.cfg
+fi
+
+${GRUB_PATH}/grub-mkrescue -o ${iso} ${iso_folder}
+${SIMULATOR} -q -f ${bochsrc} -rc ./tools/bochsinit
--- src/drv/console/include/console.h
+++ src/drv/console/include/console.h
@@ -1,7 +1,8 @@
 
-// This file is a part of MRNIU/SimpleKernel (https://github.com/MRNIU/SimpleKernel).
+// This file is a part of Simple-XX/SimpleKernel
+// (https://github.com/Simple-XX/SimpleKernel).
 //
-// console.h for MRNIU/SimpleKernel.
+// console.h for Simple-XX/SimpleKernel.
 
 #ifndef _CONSOLE_H_
 #define _CONSOLE_H_
@@ -11,24 +12,19 @@ extern "C" {
 #endif
 
 #include "stdint.h"
-#include "stddef.h"
-#include "string.h"
-#include "stdio.h"
-#include "vga/include/vga.hpp"
+#include "vga.hpp"
 
-static uint16_t * console_buffer __attribute__((unused)) = (uint16_t *) VGA_MEM_BASE;
-
-void console_init(void);
-void console_scroll(void);
+void     console_init(void);
+void     console_scroll(void);
 uint16_t console_getcursorpos(void);
-void console_setcursorpos(size_t, size_t);
-void console_writestring(const char*);
-void console_write(const char*, size_t);
-void console_putchar(char);
-void console_escapeconv(char);
-void console_putentryat(char, uint8_t, size_t, size_t);
-void console_setcolor(uint8_t);
-uint8_t console_getcolor(void);
+void     console_setcursorpos(size_t, size_t);
+void     console_writestring(const char *);
+void     console_write(const char *, size_t);
+void     console_putchar(char);
+void     console_escapeconv(char);
+void     console_putentryat(char, uint8_t, size_t, size_t);
+void     console_setcolor(uint8_t);
+uint8_t  console_getcolor(void);
 
 #ifdef __cplusplus
 }
