24acc08bfa14ca99cda876532b1124b34102b5d1
MRNIU <a6813140@hotmail.com>
2020-10-22 17:28:59 +0800

    test(pmm): 添加测试

--- src/test/test.c
+++ src/test/test.c
@@ -10,30 +10,14 @@ extern "C" {
 
 #include "stdio.h"
 #include "stdint.h"
+#include "assert.h"
+#include "test.h"
 #include "debug.h"
 #include "pmm.h"
-#include "clock.h"
-#include "keyboard.h"
-#include "test.h"
-#include "firstfit.h"
 
-bool test_pmm(void) {
-    ptr_t allc_addr = 0;
-    printk_test("Test Physical Memory Alloc :\n");
-    allc_addr = pmm_alloc(1, DMA);
-    printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr);
-    allc_addr = pmm_alloc(9000, DMA);
-    printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr);
-    printk_test("Free pages count: %d\n", pmm_free_pages_count(DMA));
-    pmm_free_page(allc_addr, 9000, DMA);
-    printk_test("Free!\n");
-    printk_test("Free pages count: %d\n", pmm_free_pages_count(DMA));
-    allc_addr = pmm_alloc(1, DMA);
-    printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr);
-    printk_test("Free pages count: %d\n", pmm_free_pages_count(DMA));
-    allc_addr = pmm_alloc(1, DMA);
-    printk_test("Alloc Physical Addr: 0x%08X\n", allc_addr);
-    printk_test("Free pages count: %d\n", pmm_free_pages_count(DMA));
+bool test(void) {
+    test_pmm();
+    // test_libc();
     return true;
 }
 
@@ -41,9 +25,69 @@ bool test_libc(void) {
     return true;
 }
 
-bool test(void) {
-    test_pmm();
-    // test_libc();
+// TODO: 完善测试
+bool test_pmm(void) {
+    ptr_t    allc_addr1   = 0;
+    ptr_t    allc_addr2   = 0;
+    ptr_t    allc_addr3   = 0;
+    ptr_t    allc_addr4   = 0;
+    uint32_t dma_free     = pmm_free_pages_count(DMA);
+    uint32_t normal_free  = pmm_free_pages_count(NORMAL);
+    uint32_t highmem_free = pmm_free_pages_count(HIGHMEM);
+    // 单次分配&回收
+    allc_addr1 = pmm_alloc(1, DMA);
+    pmm_free_page(allc_addr1, 1, DMA);
+    assert(dma_free == pmm_free_pages_count(DMA),
+           "pmm_free_page(allc_addr1, 1, DMA) error\n");
+    allc_addr1 = pmm_alloc(9000, DMA);
+    pmm_free_page(allc_addr1, 9000, DMA);
+    assert(dma_free == pmm_free_pages_count(DMA),
+           "pmm_free_page(allc_addr1, 9000, DMA) error\n");
+
+    allc_addr1 = pmm_alloc(1, NORMAL);
+    pmm_free_page(allc_addr1, 1, NORMAL);
+    assert(normal_free == pmm_free_pages_count(NORMAL),
+           "pmm_free_page(allc_addr1, 1, NORMAL) error\n");
+    allc_addr1 = pmm_alloc(9000, NORMAL);
+    pmm_free_page(allc_addr1, 9000, NORMAL);
+    assert(normal_free == pmm_free_pages_count(NORMAL),
+           "pmm_free_page(allc_addr1, 9000, NORMAL) error\n");
+
+    allc_addr1 = pmm_alloc(1, HIGHMEM);
+    pmm_free_page(allc_addr1, 1, HIGHMEM);
+    assert(highmem_free == pmm_free_pages_count(HIGHMEM),
+           "pmm_free_page(allc_addr1, 1, HIGHMEM) error\n");
+    allc_addr1 = pmm_alloc(9000, HIGHMEM);
+    pmm_free_page(allc_addr1, 9000, HIGHMEM);
+    assert(highmem_free == pmm_free_pages_count(HIGHMEM),
+           "pmm_free_page(allc_addr1, 9000, HIGHMEM) error\n");
+
+    // 连续分配&回收
+    allc_addr1 = pmm_alloc(1, DMA);
+    allc_addr2 = pmm_alloc(2, DMA);
+    allc_addr3 = pmm_alloc(3, DMA);
+    allc_addr4 = pmm_alloc(4, DMA);
+    pmm_free_page(allc_addr1, 1, DMA);
+    pmm_free_page(allc_addr2, 2, DMA);
+    pmm_free_page(allc_addr3, 3, DMA);
+    pmm_free_page(allc_addr4, 4, DMA);
+    assert(dma_free == pmm_free_pages_count(DMA),
+           "pmm_free_page(allc_addr1, 1, DMA) error\n");
+    allc_addr1 = pmm_alloc(9000, DMA);
+    allc_addr2 = pmm_alloc(9000, DMA);
+    allc_addr3 = pmm_alloc(9000, DMA);
+    allc_addr4 = pmm_alloc(9000, DMA);
+    pmm_free_page(allc_addr1, 9000, DMA);
+    pmm_free_page(allc_addr2, 9000, DMA);
+    pmm_free_page(allc_addr3, 9000, DMA);
+    pmm_free_page(allc_addr4, 9000, DMA);
+    assert(dma_free == pmm_free_pages_count(DMA),
+           "pmm_free_page(allc_addr1, 9000, DMA) error\n");
+
+    // 边界测试
+
+    // 极限测试
+    printk_info("pmm test done.\n");
     return true;
 }
 
