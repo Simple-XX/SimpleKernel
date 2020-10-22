e0b7a9670d5eac7bfebd7717e610dfc6dd971156
MRNIU <a6813140@hotmail.com>
2020-10-22 14:33:12 +0800

    pref: 删除不必要的代码

--- src/arch/x86_64/gdt/gdt.c
+++ src/arch/x86_64/gdt/gdt.c
@@ -10,8 +10,6 @@ extern "C" {
 
 #include "stdio.h"
 #include "debug.h"
-#include "cpu.hpp"
-#include "sync.hpp"
 #include "gdt.h"
 
 static gdt_ptr_t gdt_ptr;
@@ -57,35 +55,30 @@ void tss_set_gate(int32_t num, uint16_t ss0, uint32_t esp0) {
 
 // 初始化全局描述符表
 void gdt_init(void) {
-    bool intr_flag = false;
-    local_intr_store(intr_flag);
-    {
-        // 全局描述符表界限  从 0 开始，所以总长要 - 1
-        gdt_ptr.limit = sizeof(gdt_entry_t) * GDT_LENGTH - 1;
-        gdt_ptr.base  = (uint32_t)&gdt_entries;
+    // 全局描述符表界限  从 0 开始，所以总长要 - 1
+    gdt_ptr.limit = sizeof(gdt_entry_t) * GDT_LENGTH - 1;
+    gdt_ptr.base  = (uint32_t)&gdt_entries;
 
-        // 采用 Intel 平坦模型
-        // 0xC0: 粒度为 4096?
-        // Intel 文档要求首个描述符全 0
-        gdt_set_gate(SEG_NULL, 0x0, 0x0, 0x0, 0x0);
-        // 内核指令段
-        gdt_set_gate(SEG_KTEXT, 0x0, 0xFFFFFFFF, KREAD_EXEC, 0xC0);
-        // 内核数据段
-        gdt_set_gate(SEG_KDATA, 0x0, 0xFFFFFFFF, KREAD_WRITE, 0xC0);
-        // 用户模式代码段
-        gdt_set_gate(SEG_UTEXT, 0x0, 0xFFFFFFFF, UREAD_EXEC, 0xC0);
-        // 用户模式数据段
-        gdt_set_gate(SEG_UDATA, 0x0, 0xFFFFFFFF, UREAD_WRITE, 0xC0);
-        tss_set_gate(SEG_TSS, KERNEL_DS, 0);
+    // 采用 Intel 平坦模型
+    // 0xC0: 粒度为 4096?
+    // Intel 文档要求首个描述符全 0
+    gdt_set_gate(SEG_NULL, 0x0, 0x0, 0x0, 0x0);
+    // 内核指令段
+    gdt_set_gate(SEG_KTEXT, 0x0, 0xFFFFFFFF, KREAD_EXEC, 0xC0);
+    // 内核数据段
+    gdt_set_gate(SEG_KDATA, 0x0, 0xFFFFFFFF, KREAD_WRITE, 0xC0);
+    // 用户模式代码段
+    gdt_set_gate(SEG_UTEXT, 0x0, 0xFFFFFFFF, UREAD_EXEC, 0xC0);
+    // 用户模式数据段
+    gdt_set_gate(SEG_UDATA, 0x0, 0xFFFFFFFF, UREAD_WRITE, 0xC0);
+    tss_set_gate(SEG_TSS, KERNEL_DS, 0);
 
-        // 加载全局描述符表地址到 GDTR 寄存器
-        gdt_load((uint32_t)&gdt_ptr);
-        // 加载任务寄存器
-        tss_load();
+    // 加载全局描述符表地址到 GDTR 寄存器
+    gdt_load((uint32_t)&gdt_ptr);
+    // 加载任务寄存器
+    tss_load();
 
-        printk_info("gdt_init\n");
-        local_intr_restore(intr_flag);
-    }
+    printk_info("gdt_init\n");
     return;
 }
 
--- src/drv/clock/clock.c
+++ src/drv/clock/clock.c
@@ -12,27 +12,20 @@ extern "C" {
 #include "stdio.h"
 #include "stddef.h"
 #include "port.hpp"
-#include "cpu.hpp"
-#include "sync.hpp"
 #include "clock.h"
 
 void clock_init(void) {
-    bool intr_flag = false;
-    local_intr_store(intr_flag);
-    {
-        uint32_t divisor = TIMER_FREQ / FREQUENCY;
-        // 0x34
-        outb(TIMER_MODE, TIMER_SEL0 | TIMER_RATEGEN | TIMER_16BIT);
-        // 拆分低字节和高字节
-        uint8_t low  = (uint8_t)(divisor & 0xFF);
-        uint8_t hign = (uint8_t)((divisor >> 8) & 0xFF);
-        // 分别写入低字节和高字节
-        outb(IO_TIMER, low);
-        outb(IO_TIMER, hign);
+    uint32_t divisor = TIMER_FREQ / FREQUENCY;
+    // 0x34
+    outb(TIMER_MODE, TIMER_SEL0 | TIMER_RATEGEN | TIMER_16BIT);
+    // 拆分低字节和高字节
+    uint8_t low  = (uint8_t)(divisor & 0xFF);
+    uint8_t hign = (uint8_t)((divisor >> 8) & 0xFF);
+    // 分别写入低字节和高字节
+    outb(IO_TIMER, low);
+    outb(IO_TIMER, hign);
 
-        printk_info("clock_init\n");
-    }
-    local_intr_restore(intr_flag);
+    printk_info("clock_init\n");
     return;
 }
 
