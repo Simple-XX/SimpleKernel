8cc1fec15df3c9ef3cb92237e51e7ab99336c829
MRNIU <a6813140@hotmail.com>
2020-10-23 14:26:44 +0800

    style: 注释单独成行

--- src/kernel/include/kernel.h
+++ src/kernel/include/kernel.h
@@ -7,10 +7,18 @@
 #ifndef _KERNEL_H_
 #define _KERNEL_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "stdint.h"
 #include "stdio.h"
 
 void kernel_main(void);
 void console_init(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _KERNEL_H_ */
