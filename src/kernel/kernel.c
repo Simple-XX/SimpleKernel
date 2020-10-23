0e7a4148be26ce5e0fd710223f182bd84aefcf12
MRNIU <a6813140@hotmail.com>
2020-10-24 01:55:24 +0800

    style(kernel.c)

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -32,8 +32,9 @@ void kernel_main(ptr_t magic, ptr_t addr) {
     debug_init(magic, addr);
     // 物理内存初始化
     pmm_init();
-    showinfo();
+
     test();
+    showinfo();
 
     cpu_sti();
     while (1) {
