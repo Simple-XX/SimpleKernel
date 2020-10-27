a4d0104745870ef859973de961212a570568f3bf
MRNIU <a6813140@hotmail.com>
2020-10-27 17:46:30 +0800

    pref(cmake): 优化变量名

--- cmake/arch_detector.cmake
+++ cmake/arch_detector.cmake
@@ -3,14 +3,12 @@
 #
 # arch_detector.cmake for Simple-XX/SimpleKernel.
 
-if (ARCH STREQUAL x86)
-    set(SimpleKernelArch x86)
-elseif (ARCH STREQUAL x86_64)
+if (ARCH STREQUAL x86_64)
     set(SimpleKernelArch x86_64)
 else ()
-    message(WARNING "unexpected ARCH ${ARCH}, using default value \"x86\"")
-    set(SimpleKernelArch x86)
-    set(ARCH x86\(default\))
+    message(WARNING "unexpected ARCH ${ARCH}, using default value \"x86_64\"")
+    set(SimpleKernelArch x86_64)
+    set(ARCH x86_64\(default\))
 endif ()
 
 message(STATUS "SimpleKernelArch is ${SimpleKernelArch}")
