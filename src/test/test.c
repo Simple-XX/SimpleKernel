add3ee1247fa78fb985952cba96c50ccbd7d5dc1
MRNIU <a6813140@hotmail.com>
2020-10-27 18:07:30 +0800

    style(copyright)

--- src/test/test.c
+++ src/test/test.c
@@ -1,8 +1,8 @@
 
-// This file is a part of SimpleXX/SimpleKernel
-// (https://github.com/SimpleXX/SimpleKernel).
+// This file is a part of Simple-XX/SimpleKernel
+// (https://github.com/Simple-XX/SimpleKernel).
 //
-// test.c for SimpleXX/SimpleKernel.
+// test.c for Simple-XX/SimpleKernel.
 
 #ifdef __cplusplus
 extern "C" {
