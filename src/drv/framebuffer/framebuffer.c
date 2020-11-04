0488f64f009ead310cb3ea2f20d8b283a76cbfc7
MRNIU <a6813140@hotmail.com>
2020-11-04 22:26:30 +0800

    pref(mailbox): 完善 mailbox 相关程序

--- src/drv/framebuffer/framebuffer.c
+++ src/drv/framebuffer/framebuffer.c
@@ -12,80 +12,172 @@ extern "C" {
 #include "stdio.h"
 #include "framebuffer.h"
 #include "mailbox.h"
+#include "hardware.h"
 
+// 宽
+static uint32_t framebuffer_width;
+// 高
+static uint32_t framebuffer_height;
+// 每行字节数
+static uint32_t framebuffer_pitch;
 // framebuffer 地址
-uint8_t *lfb = NULL;
+static uint8_t *lfb = NULL;
+
+static mailbox_set_physical_display_wh_data_t set_physical_display_wh_data
+    __attribute__((aligned(16)));
+
+static mailbox_set_virtual_buffer_wh_data_t set_virtual_buffer_wh_data
+    __attribute__((aligned(16)));
+
+static mailbox_set_virtual_offset_data_t set_virtual_offset_data
+    __attribute__((aligned(16)));
+
+static mailbox_set_depth_data_t set_depth_data __attribute__((aligned(16)));
+
+static mailbox_set_pixel_order_data_t set_pixel_order_data
+    __attribute__((aligned(16)));
+
+static mailbox_allocate_buffer_data_t allocate_buffer_data
+    __attribute__((aligned(16)));
+
+static mailbox_get_pitch_data_t get_pitch_data __attribute__((aligned(16)));
 
 void framebuffer_init(void) {
-    mbox[0] = 35 * 4;
-    mbox[1] = MAILBOX_REQUEST;
+    mailbox_mail_t mail;
 
-    // set phy wh
-    mbox[2] = MAILBOX_TAG_SET_PHYSICAL_DISPLAY_WH;
-    mbox[3] = 8;
-    mbox[4] = 8;
-    // FrameBufferInfo.width
-    mbox[5] = FRAMEBUFFER_WIDTH;
-    // FrameBufferInfo.height
-    mbox[6] = FRAMEBUFFER_HEIGHT;
+    // 依次填充以下字段
+    // 总大小
+    // 请求标识
+    // tag
+    // 缓冲区大小
+    // 可不填写，这里用于验证
+    // 数据
+    // end 标记
+    set_physical_display_wh_data.size =
+        sizeof(mailbox_set_physical_display_wh_data_t);
+    set_physical_display_wh_data.code = MAILBOX_REQUEST;
+    set_physical_display_wh_data.data.common.tag =
+        MAILBOX_TAG_SET_PHYSICAL_DISPLAY_WH;
+    set_physical_display_wh_data.data.common.buffer_size = 8;
+    set_physical_display_wh_data.data.common.code        = 8;
+    set_physical_display_wh_data.data.height             = FRAMEBUFFER_HEIGHT;
+    set_physical_display_wh_data.data.width              = FRAMEBUFFER_WIDTH;
+    set_physical_display_wh_data.end                     = MAILBOX_TAG_END;
+    // 向 mailbox 发送请求
+    mail.channel = MAILBOX_CHANNEL_ARM2VC;
+    mail.data    = (uint32_t)&set_physical_display_wh_data;
+    mailbox_call(mail);
+    if (set_physical_display_wh_data.data.common.code !=
+        MAILBOX_RESPONSE_SUCCESSFUL + 8) {
+        log_error("framebuffer: set_physical_display_wh.\n");
+    }
 
-    // set virt wh
-    mbox[7] = MAILBOX_TAG_SET_VIRTUAL_BUFFER_WH;
-    mbox[8] = 8;
-    mbox[9] = 8;
-    // FrameBufferInfo.virtual_width
-    mbox[10] = FRAMEBUFFER_WIDTH;
-    // FrameBufferInfo.virtual_height
-    mbox[11] = FRAMEBUFFER_HEIGHT;
+    set_virtual_buffer_wh_data.size =
+        sizeof(mailbox_set_virtual_buffer_wh_data_t);
+    set_virtual_buffer_wh_data.code = MAILBOX_REQUEST;
+    set_virtual_buffer_wh_data.data.common.tag =
+        MAILBOX_TAG_SET_VIRTUAL_BUFFER_WH;
+    set_virtual_buffer_wh_data.data.common.buffer_size = 8;
+    set_virtual_buffer_wh_data.data.common.code        = 8;
+    set_virtual_buffer_wh_data.data.height             = FRAMEBUFFER_HEIGHT;
+    set_virtual_buffer_wh_data.data.width              = FRAMEBUFFER_WIDTH;
+    set_virtual_buffer_wh_data.end                     = MAILBOX_TAG_END;
+    mail.channel                                       = MAILBOX_CHANNEL_ARM2VC;
+    mail.data = (uint32_t)&set_virtual_buffer_wh_data;
+    mailbox_call(mail);
+    if (set_virtual_buffer_wh_data.data.common.code !=
+        MAILBOX_RESPONSE_SUCCESSFUL + 8) {
+        log_error("framebuffer: set_virtual_buffer_wh.\n");
+    }
 
-    // set virt offset
-    mbox[12] = MAILBOX_TAG_SET_VIRTUAL_OFFSET;
-    mbox[13] = 8;
-    mbox[14] = 8;
-    // FrameBufferInfo.x_offset
-    mbox[15] = 0;
-    // FrameBufferInfo.y.offset
-    mbox[16] = 0;
+    set_virtual_offset_data.size = sizeof(mailbox_set_virtual_offset_data_t);
+    set_virtual_offset_data.code = MAILBOX_REQUEST;
+    set_virtual_offset_data.data.common.tag = MAILBOX_TAG_SET_VIRTUAL_OFFSET;
+    set_virtual_offset_data.data.common.buffer_size = 8;
+    set_virtual_offset_data.data.common.code        = 8;
+    set_virtual_offset_data.data.offset_x           = 0;
+    set_virtual_offset_data.data.offset_y           = 0;
+    set_virtual_offset_data.end                     = MAILBOX_TAG_END;
+    mail.channel                                    = MAILBOX_CHANNEL_ARM2VC;
+    mail.data = (uint32_t)&set_virtual_offset_data;
+    mailbox_call(mail);
+    if (set_virtual_offset_data.data.common.code !=
+        MAILBOX_RESPONSE_SUCCESSFUL + 8) {
+        log_error("framebuffer: set_virtual_offset.\n");
+    }
 
-    // set depth
-    mbox[17] = MAILBOX_TAG_SET_DEPTH;
-    mbox[18] = 4;
-    mbox[19] = 4;
-    // FrameBufferInfo.depth
-    mbox[20] = 32;
+    set_depth_data.size                    = sizeof(mailbox_set_depth_data_t);
+    set_depth_data.code                    = MAILBOX_REQUEST;
+    set_depth_data.data.common.tag         = MAILBOX_TAG_SET_DEPTH;
+    set_depth_data.data.common.buffer_size = 4;
+    set_depth_data.data.common.code        = 4;
+    set_depth_data.data.depth              = 32;
+    set_depth_data.end                     = MAILBOX_TAG_END;
+    mail.channel                           = MAILBOX_CHANNEL_ARM2VC;
+    mail.data                              = (uint32_t)&set_depth_data;
+    mailbox_call(mail);
+    if (set_depth_data.data.common.code != MAILBOX_RESPONSE_SUCCESSFUL + 4) {
+        log_error("framebuffer: set_depth.\n");
+    }
 
-    // set pixel order
-    mbox[21] = MAILBOX_TAG_SET_PIXEL_ORDER;
-    mbox[22] = 4;
-    mbox[23] = 4;
-    // RGB, not BGR preferably
-    mbox[24] = FRAMEBUFFER_ISRGB;
+    set_pixel_order_data.size = sizeof(mailbox_set_pixel_order_data_t);
+    set_pixel_order_data.code = MAILBOX_REQUEST;
+    set_pixel_order_data.data.common.tag         = MAILBOX_TAG_SET_PIXEL_ORDER;
+    set_pixel_order_data.data.common.buffer_size = 4;
+    set_pixel_order_data.data.common.code        = 4;
+    set_pixel_order_data.data.order              = FRAMEBUFFER_RGB;
+    set_pixel_order_data.end                     = MAILBOX_TAG_END;
+    mail.channel                                 = MAILBOX_CHANNEL_ARM2VC;
+    mail.data = (uint32_t)&set_pixel_order_data;
+    mailbox_call(mail);
+    if (set_pixel_order_data.data.common.code !=
+        MAILBOX_RESPONSE_SUCCESSFUL + 4) {
+        log_error("framebuffer: set_pixel_order.\n");
+    }
 
-    // get framebuffer, gets alignment on request
-    mbox[25] = MAILBOX_TAG_ALLOCATE_BUFFER;
-    mbox[26] = 8;
-    mbox[27] = 8;
-    // FrameBufferInfo.pointer
-    mbox[28] = 4096;
-    // FrameBufferInfo.size
-    mbox[29] = 0;
+    allocate_buffer_data.size = sizeof(mailbox_allocate_buffer_data_t);
+    allocate_buffer_data.code = MAILBOX_REQUEST;
+    allocate_buffer_data.data.common.tag         = MAILBOX_TAG_ALLOCATE_BUFFER;
+    allocate_buffer_data.data.common.buffer_size = 8;
+    allocate_buffer_data.data.common.code        = 8;
+    allocate_buffer_data.data.addr_align         = 4096;
+    allocate_buffer_data.data.size               = 0;
+    allocate_buffer_data.end                     = MAILBOX_TAG_END;
+    mail.channel                                 = MAILBOX_CHANNEL_ARM2VC;
+    mail.data = (uint32_t)&allocate_buffer_data;
+    mailbox_call(mail);
+    if (allocate_buffer_data.data.common.code !=
+        MAILBOX_RESPONSE_SUCCESSFUL + 8) {
+        log_error("framebuffer: allocate_buffer.\n");
+    }
 
-    // get pitch
-    mbox[30] = MAILBOX_TAG_GET_PITCH;
-    mbox[31] = 4;
-    mbox[32] = 4;
-    // FrameBufferInfo.pitch
-    mbox[33] = 0;
+    get_pitch_data.size                    = sizeof(mailbox_get_pitch_data_t);
+    get_pitch_data.code                    = MAILBOX_REQUEST;
+    get_pitch_data.data.common.tag         = MAILBOX_TAG_GET_PITCH;
+    get_pitch_data.data.common.buffer_size = 4;
+    get_pitch_data.data.common.code        = 4;
+    get_pitch_data.data.pitch              = 0;
+    get_pitch_data.end                     = MAILBOX_TAG_END;
+    mail.channel                           = MAILBOX_CHANNEL_ARM2VC;
+    mail.data                              = (uint32_t)&get_pitch_data;
+    mailbox_call(mail);
+    if (get_pitch_data.data.common.code != MAILBOX_RESPONSE_SUCCESSFUL + 4) {
+        log_error("framebuffer: get_pitch.\n");
+        log_error("MAILBOX_RESPONSE_SUCCESSFUL: 0x%X\n",
+                  MAILBOX_RESPONSE_SUCCESSFUL + 8);
+        log_error("get_pitch_data.code: 0x%X\n", get_pitch_data.code);
+        log_error("get_pitch_data.data.common.code: 0x%X\n",
+                  get_pitch_data.data.common.code);
+    }
 
-    mbox[34] = MAILBOX_TAG_END;
-
-    // this might not return exactly what we asked for, could be
-    // the closest supported resolution instead
-    if (mbox_call(MAILBOX_CHANNEL_PROP_ARM2VC) && mbox[20] == 32 &&
-        mbox[28] != 0) {
+    if (set_depth_data.data.depth == 32 &&
+        (void *)allocate_buffer_data.data.addr_align != NULL) {
+        framebuffer_width  = set_physical_display_wh_data.data.width;
+        framebuffer_height = set_physical_display_wh_data.data.height;
+        framebuffer_pitch  = get_pitch_data.data.pitch;
         // 将 GPU 地址转换为 ARM 地址
-        mbox[28] &= 0x3FFFFFFF;
-        lfb = (uint8_t *)mbox[28];
+        allocate_buffer_data.data.addr_align &= 0x3FFFFFFF;
+        lfb = (uint8_t *)allocate_buffer_data.data.addr_align;
     }
     else {
         log_error("Unable to set screen resolution to 1024x768x32\n");
@@ -100,7 +192,7 @@ void framebuffer_set_pixel(uint32_t x, uint32_t y, uint32_t color) {
     uint32_t g = (color >> 8) & 0xFF;
     uint32_t b = color & 0xFF;
     // 计算位置
-    ptr += (FRAMEBUFFER_PITCH * y) + (x << 2);
+    ptr += (framebuffer_pitch * y) + (x << 2);
     *((uint32_t *)ptr) = (b << 16) | (g << 24) | r;
     return;
 }
