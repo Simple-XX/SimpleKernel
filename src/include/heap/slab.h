ae1b601d344a98c9ac7b288501ef91432d3a363a
MRNIU <a6813140@hotmail.com>
2020-12-27 22:25:40 +0800

    feat(heap): 堆管理相关代码
    Signed-off-by: MRNIU <a6813140@hotmail.com>

---
+++ src/include/heap/slab.h
@@ -0,0 +1,23 @@
+
+// This file is a part of SimpleXX/SimpleKernel
+// (https://github.com/SimpleXX/SimpleKernel).
+//
+// slab.h for SimpleXX/SimpleKernel.
+
+#ifndef _SLAB_H_
+#define _SLAB_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "heap.h"
+
+// 用于管理堆物理地址
+extern heap_manage_t slab_manage;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SLAB_H_ */
