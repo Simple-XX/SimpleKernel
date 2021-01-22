4f3afd3c1f781f06e524e163cfe319d71d6c813b
MRNIU <a6813140@hotmail.com>
2021-01-22 11:07:39 +0800

    pref(clock)
    Signed-off-by: MRNIU <a6813140@hotmail.com>

--- src/kernel/include/kernel.h
+++ src/kernel/include/kernel.h
@@ -13,18 +13,12 @@
 #include "intr.h"
 #include "clock.h"
 
-void console_init(void);
-void gdt_init(void);
-void idt_init(void);
-void clock_init(void);
-void debug_init(void);
-
-extern addr_t kernel_start[];
-extern addr_t kernel_text_start[];
-extern addr_t kernel_text_end[];
-extern addr_t kernel_data_start[];
-extern addr_t kernel_data_end[];
-extern addr_t kernel_end[];
+extern "C" void *kernel_start[];
+extern "C" void *kernel_text_start[];
+extern "C" void *kernel_text_end[];
+extern "C" void *kernel_data_start[];
+extern "C" void *kernel_data_end[];
+extern "C" void *kernel_end[];
 
 extern "C" void kernel_main(void);
 
