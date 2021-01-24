ddbfd1c639d4c83bee2a2726a54590d42fc9a755
MRNIU <a6813140@hotmail.com>
2021-01-25 01:16:58 +0800

    revert(raspi2): remove raspi2 code
    Signed-off-by: MRNIU <a6813140@hotmail.com>

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -8,12 +8,6 @@
 #include "cxxabi.h"
 #include "color.h"
 #include "io.h"
-#include "uart.h"
-
-#if defined(RASPI2)
-#include "uart.h"
-#include "framebuffer.h"
-#endif
 
 KERNEL::KERNEL(void) {
     return;
@@ -24,29 +18,18 @@ KERNEL::~KERNEL(void) {
 }
 
 void KERNEL::show_info(void) {
-#if defined(__i386__) || defined(__x86_64__)
     io.printf(LIGHT_GREEN, "kernel in memory start: 0x%08X, end 0x%08X\n",
               kernel_start, kernel_end);
     io.printf(LIGHT_GREEN, "kernel in memory size: %d KB, %d pages\n",
               (kernel_end - kernel_start) / 1024,
               (kernel_end - kernel_start + 4095) / 1024 / 4);
     io.printf(LIGHT_GREEN, "Simple Kernel.\n");
-#endif
 
-#if defined(__arm__) || defined(__aarch__)
-    io.log("kernel in memory start: 0x%08X, end 0x%08X\n", kernel_start,
-           kernel_end);
-    io.log("kernel in memory size: %d KB, %d pages\n",
-           (kernel_end - kernel_start) / 1024,
-           (kernel_end - kernel_start + 4095) / 1024 / 4);
-    io.log("Simple Kernel.\n");
-#endif
     return;
 }
 
 int32_t KERNEL::init(void) {
     cpp_init();
-    uart.init();
     show_info();
     return 0;
 }
