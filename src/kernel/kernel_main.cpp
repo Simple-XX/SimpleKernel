394ab2a1dfb6cfa6c57d24ebf9501cab20fc5405
MRNIU <a6813140@hotmail.com>
2021-01-24 18:28:23 +0800

    feat(raspi2)
    Signed-off-by: MRNIU <a6813140@hotmail.com>

---
+++ src/kernel/kernel_main.cpp
@@ -0,0 +1,13 @@
+
+// This file is a part of Simple-XX/SimpleKernel
+// (https://github.com/Simple-XX/SimpleKernel).
+//
+// kernel_main.cpp for Simple-XX/SimpleKernel.
+
+#include "kernel.h"
+
+void kernel_main(void) {
+    KERNEL kernel;
+    kernel.init();
+    return;
+}
