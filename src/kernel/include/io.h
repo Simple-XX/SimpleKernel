4443b8034b46442663801fbf4f7175a7bf85f5ee
MRNIU <a6813140@hotmail.com>
2021-01-26 11:04:32 +0800

    refactor
    Signed-off-by: MRNIU <a6813140@hotmail.com>

--- src/kernel/include/io.h
+++ src/kernel/include/io.h
@@ -8,14 +8,21 @@
 #define _IO_H_
 
 #include "stdint.h"
+#include "stdarg.h"
+#include "string.h"
 #include "stddef.h"
 #include "color.h"
+#include "port.h"
 
 #if defined(__i386__) || defined(__x86_64__)
 #include "vga.h"
-#include "console.h"
 #endif
 
+#include "console.h"
+
+extern "C" int32_t vsprintf(char *buf, const char *fmt, va_list args);
+
+template <class video_t>
 class IO {
 private:
     // //屏幕缓冲区
@@ -32,19 +39,19 @@ private:
     // uint8_t fcolor;
     // // console background color
     // uint8_t bcolor;
-    // // 当前行
-    // uint8_t curr_row;
-    // // 当前列
-    // uint8_t curr_col;
+    // 当前行
+    size_t curr_row;
+    // 当前列
+    size_t curr_col;
     // // console 属性
     // uint8_t kattr;
     // // 显存地址
     // static char *vidmem;
-    CONSOLE &console;
+    CONSOLE<video_t> &console;
 
 protected:
 public:
-    IO(CONSOLE &console);
+    IO(CONSOLE<video_t> &console);
     ~IO(void);
     int32_t init(void);
     // 端口读字节
@@ -85,6 +92,156 @@ public:
     int32_t write_string(const char *s);
     // 串口输出
     int32_t log(const char *format, ...);
+    // 格式输出
+    int32_t printf(const char *fmt, ...);
+    int32_t printf(color_t color, const char *fmt, ...);
 };
+
+template <class video_t>
+IO<video_t>::IO(CONSOLE<video_t> &console) : console(console) {
+    return;
+}
+
+template <class video_t>
+IO<video_t>::~IO(void) {
+    return;
+}
+
+template <class video_t>
+int32_t IO<video_t>::init(void) {
+    write_string("io init\n");
+    return 0;
+}
+
+template <class video_t>
+uint8_t IO<video_t>::inb(const uint32_t port) {
+    return PORT::inb(port);
+}
+
+template <class video_t>
+uint16_t IO<video_t>::inw(const uint32_t port) {
+    return PORT::inw(port);
+}
+
+template <class video_t>
+uint32_t IO<video_t>::ind(const uint32_t port) {
+    return PORT::ind(port);
+}
+
+template <class video_t>
+void IO<video_t>::outb(const uint32_t port, const uint8_t data) {
+    PORT::outb(port, data);
+    return;
+}
+
+template <class video_t>
+void IO<video_t>::outw(const uint32_t port, const uint16_t data) {
+    PORT::outw(port, data);
+    return;
+}
+
+template <class video_t>
+void IO<video_t>::outd(const uint32_t port, const uint32_t data) {
+    PORT::outd(port, data);
+    return;
+}
+
+template <class video_t>
+size_t IO<video_t>::get_rows(void) {
+    return 0;
+}
+
+template <class video_t>
+size_t IO<video_t>::get_clos(void) {
+    return 0;
+}
+
+template <class video_t>
+void IO<video_t>::set_rows(const size_t rows) {
+    console.set_rows(rows);
+    return;
+}
+
+template <class video_t>
+void IO<video_t>::set_cols(const size_t cols) {
+    console.set_cols(cols);
+    return;
+}
+
+template <class video_t>
+size_t IO<video_t>::get_row(void) {
+    return 0;
+}
+
+template <class video_t>
+size_t IO<video_t>::get_col(void) {
+    return 0;
+}
+
+template <class video_t>
+size_t IO<video_t>::set_row(const size_t row) {
+    curr_row = row;
+    console.set_row(row);
+    return 0;
+}
+
+template <class video_t>
+size_t IO<video_t>::set_col(const size_t col) {
+    curr_col = col;
+    console.set_col(col);
+    return 0;
+}
+
+template <class video_t>
+color_t IO<video_t>::get_color(void) {
+    return console.get_color();
+}
+
+template <class video_t>
+void IO<video_t>::set_color(const color_t color) {
+    console.set_color(color);
+    return;
+}
+
+template <class video_t>
+void IO<video_t>::put_char(char c) {
+    console.put_char(c);
+    return;
+}
+
+template <class video_t>
+int32_t IO<video_t>::write_string(const char *s) {
+    console.write_string(s);
+    return 0;
+}
+
+template <class video_t>
+int32_t IO<video_t>::printf(const char *fmt, ...) {
+    va_list args;
+    int32_t i;
+    char    buf[256];
+    va_start(args, fmt);
+    i = vsprintf(buf, fmt, args);
+    va_end(args);
+    write_string(buf);
+    bzero(buf, 256);
+    return i;
+}
+
+template <class video_t>
+int32_t IO<video_t>::printf(color_t color, const char *fmt, ...) {
+    color_t curr_color = get_color();
+    set_color(color);
+    va_list args;
+    int32_t i;
+    char    buf[256];
+    va_start(args, fmt);
+    i = vsprintf(buf, fmt, args);
+    va_end(args);
+    write_string(buf);
+    bzero(buf, 256);
+    set_color(curr_color);
+    return i;
+}
 
 #endif /* _IO_H_ */
--- src/kernel/include/kernel.h
+++ src/kernel/include/kernel.h
@@ -8,12 +8,17 @@
 #define _KERNEL_H_
 
 #include "stdint.h"
-#include "io.h"
+#include "stdarg.h"
+#include "string.h"
+#include "cxxabi.h"
+#include "color.h"
 
 #if defined(__i386__) || defined(__x86_64__)
 #include "vga.h"
-#include "console.h"
 #endif
+
+#include "io.h"
+#include "console.h"
 
 extern "C" uint8_t kernel_start[];
 extern "C" uint8_t kernel_text_start[];
@@ -22,25 +27,51 @@ extern "C" uint8_t kernel_data_start[];
 extern "C" uint8_t kernel_data_end[];
 extern "C" uint8_t kernel_end[];
 
-extern "C" void kernel_main(void);
-
+template <class video_t>
 class KERNEL {
 private:
-#if defined(__i386__) || defined(__x86_64__)
-    VGA     vga;
-    CONSOLE console;
-#endif
-    IO io;
+    video_t          video;
+    CONSOLE<video_t> console;
 
 protected:
 public:
     KERNEL(void);
     ~KERNEL(void);
-    int  init(void);
-    void show_info(void);
-    // 格式输出
-    int32_t printf(const char *format, ...);
-    int32_t printf(color_t color, const char *format, ...);
+    IO<video_t> io;
+    int         init(void);
+    void        show_info(void);
 };
+
+template <class video_t>
+KERNEL<video_t>::KERNEL(void)
+    : video(video_t()), console(CONSOLE(video)), io(IO(console)) {
+    return;
+}
+
+template <class video_t>
+KERNEL<video_t>::~KERNEL(void) {
+    return;
+}
+
+template <class video_t>
+void KERNEL<video_t>::show_info(void) {
+    io.printf(LIGHT_GREEN, "kernel in memory start: 0x%08X, end 0x%08X\n",
+              kernel_start, kernel_end);
+    io.printf(LIGHT_GREEN, "kernel in memory size: %d KB, %d pages\n",
+              (kernel_end - kernel_start) / 1024,
+              (kernel_end - kernel_start + 4095) / 1024 / 4);
+    io.printf(LIGHT_GREEN, "Simple Kernel.\n");
+    return;
+}
+
+template <class video_t>
+int32_t KERNEL<video_t>::init(void) {
+    cpp_init();
+    video.init();
+    console.init();
+    io.init();
+    show_info();
+    return 0;
+}
 
 #endif /* _KERNEL_H_ */
