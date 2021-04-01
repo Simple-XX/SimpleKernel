4314827108eb929b05cc5700be1bb085d1956bc4
MRNIU <a6813140@hotmail.com>
2021-04-01 22:15:07 +0800

    pref: remove addr_t
    Signed-off-by: MRNIU <a6813140@hotmail.com>

--- src/kernel/include/kernel.h
+++ src/kernel/include/kernel.h
@@ -18,16 +18,16 @@
 
 class KERNEL {
 private:
-    addr_t  magic;
-    addr_t  addr;
-    PMM     pmm;
-    void    arch_init(void) const;
-    int32_t test_pmm(void);
+    uint32_t magic;
+    void *   addr;
+    PMM      pmm;
+    void     arch_init(void) const;
+    int32_t  test_pmm(void);
 
 protected:
 public:
     KERNEL(void);
-    KERNEL(addr_t magic, addr_t addr);
+    KERNEL(uint32_t magic, void *addr);
     ~KERNEL(void);
     int32_t init(void);
     int32_t test(void);
