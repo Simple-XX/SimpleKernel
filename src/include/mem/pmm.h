68db9e4ce33329fdc1d1c2813ca7a7866c96146c
MRNIU <a6813140@hotmail.com>
2021-04-10 17:15:33 +0800

    feat(PMM): zone
    Signed-off-by: MRNIU <a6813140@hotmail.com>

--- src/include/mem/pmm.h
+++ src/include/mem/pmm.h
@@ -14,18 +14,38 @@
 #include "common.h"
 #include "firstfit.h"
 
+// DMA 区域大小 1MB
+static constexpr const size_t DMA_SIZE = 0x100000;
+// NORMAL 区域大小
+static constexpr const size_t NORMAL_SIZE = COMMON::KERNEL_SIZE;
+// HIGH 区域大小 剩余所有
+static constexpr const size_t HIGH_SIZE =
+    COMMON::PMM_MAX_SIZE - DMA_SIZE - NORMAL_SIZE;
+
+static constexpr const size_t DMA_PAGES    = DMA_SIZE / COMMON::PAGE_SIZE;
+static constexpr const size_t NORMAL_PAGES = NORMAL_SIZE / COMMON::PAGE_SIZE;
+static constexpr const size_t HIGH_PAGES   = HIGH_SIZE / COMMON::PAGE_SIZE;
+
 class PMM {
 private:
     static IO io;
     // 可用内存的物理页数组
     static COMMON::physical_pages_t phy_pages[COMMON::PMM_PAGE_MAX_SIZE];
-    // 管理算法的名称
-    static const char *name;
-    static FIRSTFIT    manage;
+    // DMA 区域
+    static size_t   dma_pages;
+    static FIRSTFIT dma;
+    // NORMAL 区域
+    static size_t   normal_pages;
+    static FIRSTFIT normal;
+    // HIGH 区域
+    static size_t   high_pages;
+    static FIRSTFIT high;
+    // 物理内存总数
+    static size_t pages;
     // 从 GRUB 读取物理内存信息
     void get_ram_info(e820map_t *e820map);
-    // 物理内存管理初始化 参数为实际可用物理页数量
-    void mamage_init(uint32_t pages);
+    // 物理内存管理初始化
+    void mamage_init(void);
 
 protected:
 public:
@@ -36,9 +56,9 @@ public:
     // 请求指定 zone 的物理页
     void *alloc_page(uint32_t _pages, COMMON::zone_t _zone);
     // 释放内存页
-    void free_page(void *_addr, uint32_t _pages);
+    void free_page(void *_addr, uint32_t _pages, COMMON::zone_t _zone);
     // 获取空闲内存页数量
-    uint32_t free_pages_count(void);
+    uint32_t free_pages_count(COMMON::zone_t _zone);
 };
 
 #endif /* _PMM_H_ */
