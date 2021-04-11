28e6a059bffcd728b9859d631ece01b6dc5a9e8c
MRNIU <a6813140@hotmail.com>
2021-04-12 05:45:00 +0800

    merge(*)
    Signed-off-by: MRNIU <a6813140@hotmail.com>

--- src/arch/i386/gdt/include/gdt.h
+++ src/arch/i386/gdt/include/gdt.h
@@ -1,7 +1,7 @@
 
 // This file is a part of Simple-XX/SimpleKernel
 // (https://github.com/Simple-XX/SimpleKernel).
-// Based on http://wiki.0xffffff.org/posts/hurlex-6.html
+//
 // gdt.h for Simple-XX/SimpleKernel.
 
 #ifndef _GDT_H_
@@ -9,37 +9,108 @@
 
 #include "stdint.h"
 
+// See 64-ia-32-architectures-software-developer-vol-3a-manual Chapter3
+
 namespace GDT {
     // 全局描述符表长度
     static constexpr const uint32_t GDT_LENGTH = 6;
     // 各个内存段所在全局描述符表下标
-    static constexpr const uint32_t SEG_NULL  = 0;
-    static constexpr const uint32_t SEG_KTEXT = 1;
-    static constexpr const uint32_t SEG_KDATA = 2;
-    static constexpr const uint32_t SEG_UTEXT = 3;
-    static constexpr const uint32_t SEG_UDATA = 4;
-    static constexpr const uint32_t SEG_TSS   = 5;
+    static constexpr const uint32_t GDT_NULL        = 0;
+    static constexpr const uint32_t GDT_KERNEL_CODE = 1;
+    static constexpr const uint32_t GDT_KERNEL_DATA = 2;
+    static constexpr const uint32_t GDT_USER_CODE   = 3;
+    static constexpr const uint32_t GDT_USER_DATA   = 4;
+    static constexpr const uint32_t GDT_TSS         = 5;
     // 内核代码段 0x08
-    static constexpr const uint32_t GD_KTEXT = SEG_KTEXT << 3;
+    static constexpr const uint32_t SEG_KERNEL_CODE = GDT_KERNEL_CODE << 3;
     // 内核数据段
-    static constexpr const uint32_t GD_KDATA = SEG_KDATA << 3;
+    static constexpr const uint32_t SEG_KERNEL_DATA = GDT_KERNEL_DATA << 3;
     // 用户代码段
-    static constexpr const uint32_t GD_UTEXT = SEG_UTEXT << 3;
+    static constexpr const uint32_t SEG_USER_CODE = GDT_USER_CODE << 3;
     // 用户数据段
-    static constexpr const uint32_t GD_UDATA = SEG_UDATA << 3;
+    static constexpr const uint32_t SEG_USER_DATA = GDT_USER_DATA << 3;
     // 任务段
-    static constexpr const uint32_t GD_TSS = SEG_TSS << 3;
+    static constexpr const uint32_t SEG_TSS = GDT_TSS << 3;
+
+    // type 类型
+    // Code- and Data-Segment Types, S=1
+    static constexpr const uint32_t TYPE_DATA_READ_ONLY             = 0x00;
+    static constexpr const uint32_t TYPE_DATA_READ_ONLY_ACCESSED    = 0x01;
+    static constexpr const uint32_t TYPE_DATA_READ_WRITE            = 0x02;
+    static constexpr const uint32_t TYPE_DATA_READ_WRITE_ACCESSED   = 0x03;
+    static constexpr const uint32_t TYPE_DATA_READ_ONLY_EXPAND_DOWN = 0x04;
+    static constexpr const uint32_t TYPE_DATA_READ_ONLY_EXPAND_DOWN_ACCESSED =
+        0x05;
+    static constexpr const uint32_t TYPE_DATA_READ_WRITE_EXPAND_DOWN = 0x06;
+    static constexpr const uint32_t TYPE_DATA_READ_WRITE_EXPAND_DOWN_ACCESSED =
+        0x07;
+
+    static constexpr const uint32_t TYPE_CODE_EXECUTE_ONLY            = 0x08;
+    static constexpr const uint32_t TYPE_CODE_EXECUTE_ONLY_ACCESSED   = 0x09;
+    static constexpr const uint32_t TYPE_CODE_EXECUTE_READ            = 0x0A;
+    static constexpr const uint32_t TYPE_CODE_EXECUTE_READ_ACCESSED   = 0x0B;
+    static constexpr const uint32_t TYPE_CODE_EXECUTE_ONLY_CONFORMING = 0x0C;
+    static constexpr const uint32_t TYPE_CODE_EXECUTE_ONLY_CONFORMING_ACCESSED =
+        0x0D;
+    static constexpr const uint32_t TYPE_CODE_EXECUTE_READ_CONFORMING = 0x0E;
+    static constexpr const uint32_t TYPE_CODE_EXECUTE_READ_CONFORMING_ACCESSED =
+        0x0F;
+
+    // System-Segment and Gate-Descriptor Types, 32bit, when S=0
+    static constexpr const uint32_t TYPE_SYSTEM_16_RESERVESD1     = 0x00;
+    static constexpr const uint32_t TYPE_SYSTEM_16_TSS_AVAILABLE  = 0x01;
+    static constexpr const uint32_t TYPE_SYSTEM_LDT               = 0x02;
+    static constexpr const uint32_t TYPE_SYSTEM_16_TSS_BUSY       = 0x03;
+    static constexpr const uint32_t TYPE_SYSTEM_16_CALL_GATE      = 0x04;
+    static constexpr const uint32_t TYPE_SYSTEM_TASK_GATE         = 0x05;
+    static constexpr const uint32_t TYPE_SYSTEM_16_INTERRUPT_GATE = 0x06;
+    static constexpr const uint32_t TYPE_SYSTEM_16_TRAP_GATE      = 0x07;
+    static constexpr const uint32_t TYPE_SYSTEM_32_RESERVESD2     = 0x08;
+    static constexpr const uint32_t TYPE_SYSTEM_32_TSS_AVAILABLE  = 0x09;
+    static constexpr const uint32_t TYPE_SYSTEM_32_RESERVESD3     = 0x0A;
+    static constexpr const uint32_t TYPE_SYSTEM_32_TSS_BUSY       = 0x0B;
+    static constexpr const uint32_t TYPE_SYSTEM_32_CALL_GATE      = 0x0C;
+    static constexpr const uint32_t TYPE_SYSTEM_RESERVESD4        = 0x0D;
+    static constexpr const uint32_t TYPE_SYSTEM_32_INTERRUPT_GATE = 0x0E;
+    static constexpr const uint32_t TYPE_SYSTEM_32_TRAP_GATE      = 0x0F;
+
+    // S 位
+    static constexpr const uint32_t S_SYSTEM    = 0x00;
+    static constexpr const uint32_t S_CODE_DATA = 0x01;
+
     // 段描述符 DPL
     // 内核级
-    static constexpr const uint32_t DPL_KERNEL = 0;
+    static constexpr const uint32_t DPL0 = 0x00;
+    static constexpr const uint32_t DPL1 = 0x01;
+    static constexpr const uint32_t DPL2 = 0x02;
     // 用户级
-    static constexpr const uint32_t DPL_USER = 3;
+    static constexpr const uint32_t DPL3 = 0x03;
 
     // 各个段的全局描述符表的选择子
-    static constexpr const uint32_t KERNEL_CS = GD_KTEXT | DPL_KERNEL;
-    static constexpr const uint32_t KERNEL_DS = GD_KDATA | DPL_KERNEL;
-    static constexpr const uint32_t USER_CS   = GD_UTEXT | DPL_USER;
-    static constexpr const uint32_t USER_DS   = GD_UDATA | DPL_USER;
+    static constexpr const uint32_t KERNEL_CS = SEG_KERNEL_CODE | DPL0;
+    static constexpr const uint32_t KERNEL_DS = SEG_KERNEL_DATA | DPL0;
+    static constexpr const uint32_t USER_CS   = SEG_USER_CODE | DPL3;
+    static constexpr const uint32_t USER_DS   = SEG_USER_DATA | DPL3;
+
+    // P 位
+    static constexpr const uint32_t SEGMENT_NOT_PRESENT = 0x00;
+    static constexpr const uint32_t SEGMENT_PRESENT     = 0x01;
+    // AVL
+    static constexpr const uint32_t AVL_NOT_AVAILABLE = 0x00;
+    static constexpr const uint32_t AVL_AVAILABLE     = 0x01;
+    // L 位
+    static constexpr const uint32_t L_32BIT = 0x00;
+    static constexpr const uint32_t L_64BIT = 0x01;
+    // D/B
+    static constexpr const uint32_t DB_EXECUTABLE_CODE_SEGMENT_16     = 0x00;
+    static constexpr const uint32_t DB_EXECUTABLE_CODE_SEGMENT_32     = 0x01;
+    static constexpr const uint32_t DB_STACK_SEGMENT_STACK_POINTER_16 = 0x00;
+    static constexpr const uint32_t DB_STACK_SEGMENT_STACK_POINTER_32 = 0x01;
+    static constexpr const uint32_t DB_EXPAND_DOWN_DATA_SEGMENT_64KB  = 0x00;
+    static constexpr const uint32_t DB_EXPAND_DOWN_DATA_SEGMENT_4GB   = 0x01;
+    // G
+    static constexpr const uint32_t G_BYTE = 0x00;
+    static constexpr const uint32_t G_4KB  = 0x01;
 
     // 访问权限
     static constexpr const uint32_t KREAD_EXEC  = 0x9A;
@@ -47,21 +118,41 @@ namespace GDT {
     static constexpr const uint32_t UREAD_EXEC  = 0xFA;
     static constexpr const uint32_t UREAD_WRITE = 0xF2;
 
+    static constexpr const uint32_t BASE  = 0x00;
+    static constexpr const uint32_t LIMIT = 0xFFFFFFFF;
+
     // 全局描述符类型
     typedef struct gdt_entry {
-        // 段界限   15～0
-        uint16_t limit_low;
-        // 段基地址 15～0
-        uint16_t base_low;
-        // 段基地址 23～16
-        uint8_t base_middle;
-        // 段存在位、描述符特权级、描述符类型、描述符子类别
-        uint8_t access;
-        // 其他标志、段界限 19～16 (unsigned limit_high:
-        // 4;unsigned flags: 4;)
-        uint8_t granularity;
-        // 段基地址 31～24
-        uint8_t base_high;
+        // 段界限 15:00
+        uint64_t limit1 : 16;
+        // 基址 15:00
+        uint64_t base_addr1 : 16;
+        // 基址 23:16
+        uint64_t base_addr2 : 8;
+        // 类型
+        uint64_t type : 4;
+        // Descriptor type (0 = system; 1 = code or data)
+        uint64_t s : 1;
+        // Specifies the privilege level of the segment
+        uint64_t dpl : 2;
+        // Indicates whether the segment is present in memory (set) or not
+        // present (clear).
+        uint64_t p : 1;
+        // 段界限 19:16
+        uint64_t limit2 : 4;
+        // Available for use by system software
+        uint64_t avl : 1;
+        // 64-bit code segment (IA-32e mode only)
+        uint64_t l : 1;
+        // Default operation size(0 = 16 - bit segment; 1 = 32 - bit segment)
+        uint64_t db : 1;
+        // Determines the scaling of the segment limit field. When the
+        // granularity flag is clear, the segment limit is interpreted in
+        // byte units; when flag is set, the segment limit is interpreted in
+        // 4-KByte units.
+        uint64_t g : 1;
+        // 基址 31:24
+        uint64_t base_addr3 : 8;
     } __attribute__((packed)) gdt_entry_t;
 
     // GDTR
