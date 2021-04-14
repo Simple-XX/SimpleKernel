86d4a909785a465c4ac37ee223ba672eb7360244
MRNIU <a6813140@hotmail.com>
2021-04-15 00:21:05 +0800

    feat(PMM): riscv pmm done
    Signed-off-by: MRNIU <a6813140@hotmail.com>

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -19,7 +19,7 @@ KERNEL::KERNEL(void) : pmm(PMM()) {
     // 物理内存管理初始化
     pmm.init();
     // 测试物理内存
-    // test_pmm();
+    test_pmm();
     return;
 }
 
@@ -36,29 +36,37 @@ int32_t KERNEL::test_pmm(void) {
     uint8_t *addr4      = nullptr;
     uint32_t free_count = pmm.free_pages_count(COMMON::NORMAL);
     addr1               = (uint8_t *)pmm.alloc_page(0x9F, COMMON::NORMAL);
-    assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 0x9F);
-    *(uint32_t *)addr1 = cd;
-    assert((*(uint32_t *)addr1 == cd));
-    pmm.free_page(addr1, 0x9F, COMMON::NORMAL);
-    assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
+    if (addr1 != nullptr) {
+        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 0x9F);
+        *(uint32_t *)addr1 = cd;
+        assert((*(uint32_t *)addr1 == cd));
+        pmm.free_page(addr1, 0x9F, COMMON::NORMAL);
+        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
+    }
     addr2 = (uint8_t *)pmm.alloc_page(1, COMMON::NORMAL);
-    assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 1);
-    *(int *)addr2 = cd;
-    assert((*(uint32_t *)addr2 == cd));
-    pmm.free_page(addr2, 1, COMMON::NORMAL);
-    assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
+    if (addr2 != nullptr) {
+        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 1);
+        *(int *)addr2 = cd;
+        assert((*(uint32_t *)addr2 == cd));
+        pmm.free_page(addr2, 1, COMMON::NORMAL);
+        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
+    }
     addr3 = (uint8_t *)pmm.alloc_page(1024, COMMON::NORMAL);
-    assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 1024);
-    *(int *)addr3 = cd;
-    assert((*(uint32_t *)addr3 == cd));
-    pmm.free_page(addr3, 1024, COMMON::NORMAL);
-    assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
+    if (addr3 != nullptr) {
+        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 1024);
+        *(int *)addr3 = cd;
+        assert((*(uint32_t *)addr3 == cd));
+        pmm.free_page(addr3, 1024, COMMON::NORMAL);
+        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
+    }
     addr4 = (uint8_t *)pmm.alloc_page(4096, COMMON::NORMAL);
-    assert((*(uint32_t *)addr4 == cd));
-    assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 4096);
-    *(int *)addr4 = cd;
-    pmm.free_page(addr4, 4096, COMMON::NORMAL);
-    assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
+    if (addr4 != nullptr) {
+        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 4096);
+        *(int *)addr4 = cd;
+        assert((*(uint32_t *)addr4 == cd));
+        pmm.free_page(addr4, 4096, COMMON::NORMAL);
+        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
+    }
     io.printf("pmm test done.\n");
     return 0;
 }
