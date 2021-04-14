5674ed26e00e87073a2158a35b31b980e199f67d
MRNIU <a6813140@hotmail.com>
2021-04-14 21:00:31 +0800

    fix(stdarg, vsprintf): FIX#3
    Signed-off-by: MRNIU <a6813140@hotmail.com>

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -23,13 +23,12 @@ KERNEL::~KERNEL(void) {
 }
 
 void KERNEL::show_info(void) {
-    // BUG: raspi2 下不能正常输出链接脚本中的地址
-    // io.info("kernel in memory start: 0x%08X, end 0x%08X\n",
-    //         COMMON::kernel_start, COMMON::kernel_end);
-    // io.info("kernel in memory size: %d KB, %d pages\n",
-    //         (COMMON::kernel_end - COMMON::kernel_start) / 1024,
-    //         (COMMON::kernel_end - COMMON::kernel_start + 4095) / 1024 / 4);
-    io.printf("kernel in memory start: 0x%d\n", 0xcd);
+    io.info("kernel in memory start: 0x%08X, end 0x%08X\n",
+            COMMON::kernel_start, COMMON::kernel_end);
+    io.info("kernel in memory size: %d KB, %d pages\n",
+            (COMMON::kernel_end - COMMON::kernel_start) / 1024,
+            (COMMON::kernel_end - COMMON::kernel_start + 4095) / 1024 / 4);
+    io.printf("kernel in memory start: 0x%d\n", 0xEF);
     io.info("Simple Kernel.\n");
     return;
 }
