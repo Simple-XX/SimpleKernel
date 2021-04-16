75b7bdc857f81d8311834c34b3a1b73bbff1520f
MRNIU <a6813140@hotmail.com>
2021-04-16 14:24:03 +0800

    pref(vmm): working on riscv vmm
    Signed-off-by: MRNIU <a6813140@hotmail.com>

--- src/arch/riscv64/cpu.hpp
+++ src/arch/riscv64/cpu.hpp
@@ -10,10 +10,288 @@
 #include "stdint.h"
 #include "stdbool.h"
 
-// TODO: CPUID 相关操作，补全寄存器操作，数据地址等
-
 namespace CPU {
-    ;
+
+    // which hart (core) is this?
+    static inline uint64_t READ_MHARTID(void) {
+        uint64_t x;
+        __asm__ volatile("csrr %0, mhartid" : "=r"(x));
+        return x;
+    }
+
+    // Machine Status Register, mstatus
+    // previous mode.
+    static constexpr const uint64_t MSTATUS_MPP_MASK = 3 << 11;
+    static constexpr const uint64_t MSTATUS_MPP_M    = 3 << 11;
+    static constexpr const uint64_t MSTATUS_MPP_S    = 1 << 11;
+    static constexpr const uint64_t MSTATUS_MPP_U    = 0 << 11;
+    // machine-mode interrupt enable.
+    static constexpr const uint64_t MSTATUS_MIE = 1 << 3;
+
+    static inline uint64_t READ_MSTATUS(void) {
+        uint64_t x;
+        __asm__ volatile("csrr %0, mstatus" : "=r"(x));
+        return x;
+    }
+
+    static inline void WRITE_MSTATUS(uint64_t x) {
+        __asm__ volatile("csrw mstatus, %0" : : "r"(x));
+        return;
+    }
+
+    // machine exception program counter, holds the
+    // instruction address to which a return from
+    // exception will go.
+    static inline void WRITE_MEPC(uint64_t x) {
+        __asm__ volatile("csrw mepc, %0" : : "r"(x));
+        return;
+    }
+
+    // Supervisor Status Register, sstatus
+    // User Interrupt Enable
+    static constexpr const uint64_t SSTATUS_UIE = 1 << 0;
+    // Supervisor Interrupt Enable
+    static constexpr const uint64_t SSTATUS_SIE = 1 << 1;
+    // User Previous Interrupt Enable
+    static constexpr const uint64_t SSTATUS_UPIE = 1 << 4;
+    // Supervisor Previous Interrupt Enable
+    static constexpr const uint64_t SSTATUS_SPIE = 1 << 5;
+    // Previous mode, 1=Supervisor, 0=User
+    static constexpr const uint64_t SSTATUS_SPP = 1 << 8;
+
+    static inline uint64_t READ_SSTATUS(void) {
+        uint64_t x;
+        __asm__ volatile("csrr %0, sstatus" : "=r"(x));
+        return x;
+    }
+
+    static inline void WRITE_SSTATUS(uint64_t x) {
+        __asm__ volatile("csrw sstatus, %0" : : "r"(x));
+    }
+
+    // Supervisor Interrupt Pending
+    static inline uint64_t READ_SIP(void) {
+        uint64_t x;
+        __asm__ volatile("csrr %0, sip" : "=r"(x));
+        return x;
+    }
+
+    static inline void WRITE_SIP(uint64_t x) {
+        __asm__ volatile("csrw sip, %0" : : "r"(x));
+        return;
+    }
+
+    // Supervisor Interrupt Enable
+    // software
+    static constexpr const uint64_t SIE_SSIE = 1 << 1;
+    // timer
+    static constexpr const uint64_t SIE_STIE = 1 << 5;
+    // external
+    static constexpr const uint64_t SIE_SEIE = 1 << 9;
+
+    static inline uint64_t READ_SIE(void) {
+        uint64_t x;
+        __asm__ volatile("csrr %0, sie" : "=r"(x));
+        return x;
+    }
+
+    static inline void WRITE_SIE(uint64_t x) {
+        __asm__ volatile("csrw sie, %0" : : "r"(x));
+        return;
+    }
+
+    // Machine-mode Interrupt Enable
+    // software
+    static constexpr const uint64_t MIE_MSIE = 1 << 3;
+    // timer
+    static constexpr const uint64_t MIE_MTIE = 1 << 7;
+    // external
+    static constexpr const uint64_t MIE_MEIE = 1 << 11;
+
+    static inline uint64_t READ_MIE(void) {
+        uint64_t x;
+        __asm__ volatile("csrr %0, mie" : "=r"(x));
+        return x;
+    }
+
+    static inline void WRITE_MIE(uint64_t x) {
+        __asm__ volatile("csrw mie, %0" : : "r"(x));
+        return;
+    }
+
+    // machine exception program counter, holds the
+    // instruction address to which a return from
+    // exception will go.
+    static inline uint64_t READ_SEPC(void) {
+        uint64_t x;
+        __asm__ volatile("csrr %0, sepc" : "=r"(x));
+        return x;
+    }
+
+    static inline void WRITE_SEPC(uint64_t x) {
+        __asm__ volatile("csrw sepc, %0" : : "r"(x));
+        return;
+    }
+
+    // Machine Exception Delegation
+    static inline uint64_t READ_MEDELEG(void) {
+        uint64_t x;
+        __asm__ volatile("csrr %0, medeleg" : "=r"(x));
+        return x;
+    }
+
+    static inline void WRITE_MEDELEG(uint64_t x) {
+        __asm__ volatile("csrw medeleg, %0" : : "r"(x));
+        return;
+    }
+
+    // Machine Interrupt Delegation
+    static inline uint64_t READ_MIDELEG(void) {
+        uint64_t x;
+        __asm__ volatile("csrr %0, mideleg" : "=r"(x));
+        return x;
+    }
+
+    static inline void WRITE_MIDELEG(uint64_t x) {
+        __asm__ volatile("csrw mideleg, %0" : : "r"(x));
+        return;
+    }
+
+    // Supervisor Trap-Vector Base Address
+    // low two bits are mode.
+    static inline uint64_t READ_STVEC(void) {
+        uint64_t x;
+        __asm__ volatile("csrr %0, stvec" : "=r"(x));
+        return x;
+    }
+
+    static inline void WRITE_STVEC(uint64_t x) {
+        __asm__ volatile("csrw stvec, %0" : : "r"(x));
+        return;
+    }
+
+    // Machine-mode interrupt vector
+    static inline void WRITE_MTVEC(uint64_t x) {
+        __asm__ volatile("csrw mtvec, %0" : : "r"(x));
+        return;
+    }
+
+    // use riscv's sv39 page table scheme.
+    static constexpr const uint64_t SATP_SV39 = (uint64_t)8 << 60;
+
+    static constexpr void *MAKE_SATP(void *pagetable) {
+        return (void *)(SATP_SV39 | (((uint64_t)pagetable) >> 12));
+    }
+
+    // supervisor address translation and protection;
+    // holds the address of the page table.
+    static inline void WRITE_SATP(void *x) {
+        __asm__ volatile("csrw satp, %0" : : "r"(x));
+        return;
+    }
+
+    static inline void *READ_SATP(void) {
+        void *x;
+        __asm__ volatile("csrr %0, satp" : "=r"(x));
+        return x;
+    }
+
+    // Supervisor Scratch register, for early trap handler in trampoline.S.
+    static inline void WRITE_SSCRATCH(uint64_t x) {
+        __asm__ volatile("csrw sscratch, %0" : : "r"(x));
+        return;
+    }
+
+    static inline void WRITE_MSCRATCH(uint64_t x) {
+        __asm__ volatile("csrw mscratch, %0" : : "r"(x));
+        return;
+    }
+
+    // Supervisor Trap Cause
+    static inline uint64_t READ_SCAUSE(void) {
+        uint64_t x;
+        __asm__ volatile("csrr %0, scause" : "=r"(x));
+        return x;
+    }
+
+    // Supervisor Trap Value
+    static inline uint64_t READ_STVAL(void) {
+        uint64_t x;
+        __asm__ volatile("csrr %0, stval" : "=r"(x));
+        return x;
+    }
+
+    // Machine-mode Counter-Enable
+    static inline uint64_t READ_MCOUNTEREN(void) {
+        uint64_t x;
+        __asm__ volatile("csrr %0, mcounteren" : "=r"(x));
+        return x;
+    }
+
+    static inline void WRITE_MCOUNTEREN(uint64_t x) {
+        __asm__ volatile("csrw mcounteren, %0" : : "r"(x));
+        return;
+    }
+
+    // supervisor-mode cycle counter
+    static inline uint64_t READ_TIME(void) {
+        uint64_t x;
+        // __asm__ volatile("csrr %0, time" : "=r" (x) );
+        // this instruction will trap in SBI
+        __asm__ volatile("rdtime %0" : "=r"(x));
+        return x;
+    }
+
+    // enable device interrupts
+    static inline void ENABLE_INTR(void) {
+        WRITE_SSTATUS(READ_SSTATUS() | SSTATUS_SIE);
+        return;
+    }
+
+    // disable device interrupts
+    static inline void DISABLE_INTR(void) {
+        WRITE_SSTATUS(READ_SSTATUS() & ~SSTATUS_SIE);
+        return;
+    }
+
+    // are device interrupts enabled?
+    static inline int SSTATUS_INTR_status(void) {
+        uint64_t x = READ_SSTATUS();
+        return (x & SSTATUS_SIE) != 0;
+    }
+
+    static inline uint64_t READ_SP(void) {
+        uint64_t x;
+        __asm__ volatile("mv %0, sp" : "=r"(x));
+        return x;
+    }
+
+    // read and write tp, the thread pointer, which holds
+    // this core's hartid (core number), the index into cpus[].
+    static inline uint64_t READ_TP(void) {
+        uint64_t x;
+        __asm__ volatile("mv %0, tp" : "=r"(x));
+        return x;
+    }
+
+    static inline void WRITE_TP(uint64_t x) {
+        __asm__ volatile("mv tp, %0" : : "r"(x));
+        return;
+    }
+
+    static inline uint64_t READ_RA(void) {
+        uint64_t x;
+        __asm__ volatile("mv %0, ra" : "=r"(x));
+        return x;
+    }
+
+    // flush the TLB.
+    static inline void SFENCE_VMA(void) {
+        // the zero, zero means flush all TLB entries.
+        __asm__ volatile("sfence.vma");
+        return;
+    }
+
 };
 
 #endif /* _CPU_HPP_ */
--- src/arch/riscv64/vmm/vmm.cpp
+++ src/arch/riscv64/vmm/vmm.cpp
@@ -9,13 +9,47 @@
 #include "cpu.hpp"
 #include "vmm.h"
 
+class page_table_entry_t {
+public:
+    uint64_t v : 1;
+    uint64_t r : 1;
+    uint64_t w : 1;
+    uint64_t x : 1;
+    uint64_t u : 1;
+    uint64_t g : 1;
+    uint64_t a : 1;
+    uint64_t d : 1;
+    uint64_t rsw : 2;
+    uint64_t ppn0 : 9;
+    uint64_t ppn1 : 9;
+    uint64_t ppn2 : 26;
+    uint64_t reserved : 10;
+};
+
+// 获取一个地址的 63−39，9 位
+// 63−39 位的值必须等于第 38
+// 位的值，否则会认为该虚拟地址不合法，在访问时会产生异常。
+#define VMM_VPN_RESERVED(x) (((x) >> 39) & 0x3FFFFFF)
+
+// 获取一个地址的 VPN2，9 位
+#define VMM_VPN2(x) (((x) >> 30) & 0x01FF)
+
+// 获取一个地址的 VPN1，9 位
+#define VMM_VPN1(x) (((x) >> 21) & 0x01FF)
+
+// 获取一个地址的 VPN0，9
+#define VMM_VPN0(x) (((x) >> 12) & 0x01FF)
+
+// 获取一个地址的页內偏移，低 12 位
+#define VMM_OFFSET_INDEX(x) ((x)&0x0FFF)
+
 IO           VMM::io;
 PMM          VMM::pmm;
 page_dir_t   VMM::pgd_kernel[VMM_PAGE_TABLES_TOTAL];
 page_table_t VMM::pte_kernel[VMM_KERNEL_PAGES];
 
 VMM::VMM(void) {
-    // curr_dir = (page_dir_t)CPU::read_cr3();
+    curr_dir = (page_dir_t)CPU::READ_SATP();
     return;
 }
 
@@ -31,15 +65,15 @@ void VMM::init(void) {
 #undef DEBUG
 #endif
     for (uint64_t i = 0; i < VMM_KERNEL_PAGES; i++) {
-        pte_kernel[i] = (page_table_t)((i << 12) | VMM_PAGE_PRESENT |
-                                       VMM_PAGE_RW | VMM_PAGE_KERNEL);
+        pte_kernel[i] = (page_table_t)((i << 12) | VMM_PAGE_VALID |
+                                       VMM_PAGE_READABLE | VMM_PAGE_WRITABLE);
     }
-    for (uint64_t i = VMM_PGD_INDEX(COMMON::KERNEL_BASE);
-         i < VMM_KERNEL_PAGE_TABLES; i++) {
+    for (uint64_t i = VMM_VPN2(COMMON::KERNEL_BASE); i < VMM_KERNEL_PAGE_TABLES;
+         i++) {
         pgd_kernel[i] = reinterpret_cast<page_dir_t>(
             reinterpret_cast<uint64_t>(
                 &pte_kernel[VMM_PAGES_PRE_PAGE_TABLE * i]) |
-            VMM_PAGE_PRESENT | VMM_PAGE_RW | VMM_PAGE_KERNEL);
+            VMM_PAGE_VALID | VMM_PAGE_READABLE | VMM_PAGE_WRITABLE);
     }
     // 虚拟地址 0x00 设为 nullptr
     pte_kernel[0] = nullptr;
@@ -53,31 +87,32 @@ void VMM::init(void) {
 #undef DEBUG
 #endif
     set_pgd((page_dir_t)pgd_kernel);
-    // CPU::CR0_SET_PG();
+    CPU::SFENCE_VMA();
     io.printf("vmm_init\n");
     return;
 }
 
 page_dir_t VMM::get_pgd(void) const {
-    return curr_dir;
+    return (page_dir_t)CPU::MAKE_SATP(curr_dir);
 }
 
 void VMM::set_pgd(const page_dir_t pgd) {
     curr_dir = pgd;
-    // CPU::CR3_SET_PGD((void *)curr_dir);
+    CPU::WRITE_SATP(CPU::MAKE_SATP(curr_dir));
     return;
 }
 
 void VMM::mmap(const page_dir_t pgd, const void *va, const void *pa,
                const uint32_t flag) {
-    uint64_t     pgd_idx = VMM_PGD_INDEX(reinterpret_cast<uint64_t>(va));
+    uint64_t     pgd_idx = VMM_VPN2(reinterpret_cast<uint64_t>(va));
     uint64_t     pte_idx = VMM_PTE_INDEX(reinterpret_cast<uint64_t>(va));
     page_table_t pt =
         (page_table_t)((uint64_t)pgd[pgd_idx] & COMMON::PAGE_MASK);
     if (pt == nullptr) {
-        pt           = (page_table_t)pmm.alloc_page(1, COMMON::NORMAL);
-        pgd[pgd_idx] = (page_dir_entry_t)(reinterpret_cast<uint64_t>(pt) |
-                                          VMM_PAGE_PRESENT | VMM_PAGE_RW);
+        pt = (page_table_t)pmm.alloc_page(1, COMMON::NORMAL);
+        pgd[pgd_idx] =
+            (page_dir_entry_t)(reinterpret_cast<uint64_t>(pt) | VMM_PAGE_VALID |
+                               VMM_PAGE_READABLE | VMM_PAGE_WRITABLE);
     }
     pt = (page_table_t)VMM_PA_LA(pt);
     if (pa == nullptr) {
@@ -91,12 +126,11 @@ void VMM::mmap(const page_dir_t pgd, const void *va, const void *pa,
             pa);
 #undef DEBUG
 #endif
-    // CPU::INVLPG(va);
     return;
 }
 
 void VMM::unmmap(const page_dir_t pgd, const void *va) {
-    uint64_t     pgd_idx = VMM_PGD_INDEX(reinterpret_cast<uint64_t>(va));
+    uint64_t     pgd_idx = VMM_VPN2(reinterpret_cast<uint64_t>(va));
     uint64_t     pte_idx = VMM_PTE_INDEX(reinterpret_cast<uint64_t>(va));
     page_table_t pt =
         (page_table_t)((uint64_t)pgd[pgd_idx] & COMMON::PAGE_MASK);
@@ -107,12 +141,11 @@ void VMM::unmmap(const page_dir_t pgd, const void *va) {
     pt          = (page_table_t)VMM_PA_LA(pt);
     pt[pte_idx] = nullptr;
     // TODO: 如果一页表都被 unmap，释放占用的物理内存
-    // CPU::INVLPG(va);
     return;
 }
 
 uint32_t VMM::get_mmap(const page_dir_t pgd, const void *va, const void *pa) {
-    uint64_t     pgd_idx = VMM_PGD_INDEX(reinterpret_cast<uint64_t>(va));
+    uint64_t     pgd_idx = VMM_VPN2(reinterpret_cast<uint64_t>(va));
     uint64_t     pte_idx = VMM_PTE_INDEX(reinterpret_cast<uint64_t>(va));
     page_table_t pt =
         (page_table_t)((uint64_t)pgd[pgd_idx] & COMMON::PAGE_MASK);
