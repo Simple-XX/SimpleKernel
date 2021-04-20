4879d0d6f3569f277fcf49221f7112657e3521b0
MRNIU <a6813140@hotmail.com>
2021-04-20 17:33:28 +0800

    feat(LIBCXX): List
    Signed-off-by: MRNIU <a6813140@hotmail.com>

--- src/libcxx/include/UninitializedFunctions.hpp
+++ src/libcxx/include/UninitializedFunctions.hpp
@@ -12,102 +12,110 @@
 #include "Iterator.hpp"
 #include "TypeTraits.h"
 
-/***************************************************************************/
-template <class InputIterator, class ForwardIterator>
-ForwardIterator _uninitialized_copy_aux(InputIterator first, InputIterator last,
-                                        ForwardIterator result, _true_type);
+namespace STL {
 
-template <class InputIterator, class ForwardIterator>
-ForwardIterator _uninitialized_copy_aux(InputIterator first, InputIterator last,
-                                        ForwardIterator result, _false_type);
+    /***************************************************************************/
+    template <class InputIterator, class ForwardIterator>
+    ForwardIterator _uninitialized_copy_aux(InputIterator   first,
+                                            InputIterator   last,
+                                            ForwardIterator result, _true_type);
 
-template <class InputIterator, class ForwardIterator>
-ForwardIterator uninitialized_copy(InputIterator first, InputIterator last,
-                                   ForwardIterator result) {
-    typedef typename _type_traits<
-        typename iterator_traits<InputIterator>::value_type>::is_POD_type
-        isPODType;
-    return _uninitialized_copy_aux(first, last, result, isPODType());
-}
+    template <class InputIterator, class ForwardIterator>
+    ForwardIterator
+    _uninitialized_copy_aux(InputIterator first, InputIterator last,
+                            ForwardIterator result, _false_type);
 
-template <class InputIterator, class ForwardIterator>
-ForwardIterator _uninitialized_copy_aux(InputIterator first, InputIterator last,
-                                        ForwardIterator result, _true_type) {
-    memcpy(result, (void *)const_cast<char *>(first),
-           (last - first) * sizeof(*first));
-    return result + (last - first);
-}
-
-template <class InputIterator, class ForwardIterator>
-ForwardIterator _uninitialized_copy_aux(InputIterator first, InputIterator last,
-                                        ForwardIterator result, _false_type) {
-    int i = 0;
-    for (; first != last; ++first, ++i) {
-        construct((result + i), *first);
+    template <class InputIterator, class ForwardIterator>
+    ForwardIterator uninitialized_copy(InputIterator first, InputIterator last,
+                                       ForwardIterator result) {
+        typedef typename _type_traits<
+            typename iterator_traits<InputIterator>::value_type>::is_POD_type
+            isPODType;
+        return _uninitialized_copy_aux(first, last, result, isPODType());
     }
-    return (result + i);
-}
 
-/***************************************************************************/
-template <class ForwardIterator, class T>
-void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last,
-                             const T &value, _true_type);
-
-template <class ForwardIterator, class T>
-void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last,
-                             const T &value, _false_type);
-
-template <class ForwardIterator, class T>
-void uninitialized_fill(ForwardIterator first, ForwardIterator last,
-                        const T &value) {
-    typedef typename _type_traits<T>::is_POD_type isPODType;
-    _uninitialized_fill_aux(first, last, value, isPODType());
-}
-
-template <class ForwardIterator, class T>
-void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last,
-                             const T &value, _true_type) {
-    fill(first, last, value);
-}
-
-template <class ForwardIterator, class T>
-void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last,
-                             const T &value, _false_type) {
-    for (; first != last; ++first) {
-        construct(first, value);
+    template <class InputIterator, class ForwardIterator>
+    ForwardIterator
+    _uninitialized_copy_aux(InputIterator first, InputIterator last,
+                            ForwardIterator result, _true_type) {
+        memcpy(result, (void *)const_cast<char *>(first),
+               (last - first) * sizeof(*first));
+        return result + (last - first);
     }
-}
 
-/***************************************************************************/
-template <class ForwardIterator, class Size, class T>
-ForwardIterator _uninitialized_fill_n_aux(ForwardIterator first, Size n,
-                                          const T &x, _true_type);
-
-template <class ForwardIterator, class Size, class T>
-ForwardIterator _uninitialized_fill_n_aux(ForwardIterator first, Size n,
-                                          const T &x, _false_type);
-
-template <class ForwardIterator, class Size, class T>
-inline ForwardIterator uninitialized_fill_n(ForwardIterator first, Size n,
-                                            const T &x) {
-    typedef typename _type_traits<T>::is_POD_type isPODType;
-    return _uninitialized_fill_n_aux(first, n, x, isPODType());
-}
-
-template <class ForwardIterator, class Size, class T>
-ForwardIterator _uninitialized_fill_n_aux(ForwardIterator first, Size n,
-                                          const T &x, _true_type) {
-    return fill_n(first, n, x);
-}
-
-template <class ForwardIterator, class Size, class T>
-ForwardIterator _uninitialized_fill_n_aux(ForwardIterator first, Size n,
-                                          const T &x, _false_type) {
-    int i = 0;
-    for (; i != n; ++i) {
-        construct((T *)(first + i), x);
+    template <class InputIterator, class ForwardIterator>
+    ForwardIterator
+    _uninitialized_copy_aux(InputIterator first, InputIterator last,
+                            ForwardIterator result, _false_type) {
+        int i = 0;
+        for (; first != last; ++first, ++i) {
+            construct((result + i), *first);
+        }
+        return (result + i);
     }
-    return (first + i);
+
+    /***************************************************************************/
+    template <class ForwardIterator, class T>
+    void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last,
+                                 const T &value, _true_type);
+
+    template <class ForwardIterator, class T>
+    void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last,
+                                 const T &value, _false_type);
+
+    template <class ForwardIterator, class T>
+    void uninitialized_fill(ForwardIterator first, ForwardIterator last,
+                            const T &value) {
+        typedef typename _type_traits<T>::is_POD_type isPODType;
+        _uninitialized_fill_aux(first, last, value, isPODType());
+    }
+
+    template <class ForwardIterator, class T>
+    void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last,
+                                 const T &value, _true_type) {
+        fill(first, last, value);
+    }
+
+    template <class ForwardIterator, class T>
+    void _uninitialized_fill_aux(ForwardIterator first, ForwardIterator last,
+                                 const T &value, _false_type) {
+        for (; first != last; ++first) {
+            construct(first, value);
+        }
+    }
+
+    /***************************************************************************/
+    template <class ForwardIterator, class Size, class T>
+    ForwardIterator _uninitialized_fill_n_aux(ForwardIterator first, Size n,
+                                              const T &x, _true_type);
+
+    template <class ForwardIterator, class Size, class T>
+    ForwardIterator _uninitialized_fill_n_aux(ForwardIterator first, Size n,
+                                              const T &x, _false_type);
+
+    template <class ForwardIterator, class Size, class T>
+    inline ForwardIterator uninitialized_fill_n(ForwardIterator first, Size n,
+                                                const T &x) {
+        typedef typename _type_traits<T>::is_POD_type isPODType;
+        return _uninitialized_fill_n_aux(first, n, x, isPODType());
+    }
+
+    template <class ForwardIterator, class Size, class T>
+    ForwardIterator _uninitialized_fill_n_aux(ForwardIterator first, Size n,
+                                              const T &x, _true_type) {
+        return fill_n(first, n, x);
+    }
+
+    template <class ForwardIterator, class Size, class T>
+    ForwardIterator _uninitialized_fill_n_aux(ForwardIterator first, Size n,
+                                              const T &x, _false_type) {
+        int i = 0;
+        for (; i != n; ++i) {
+            construct((T *)(first + i), x);
+        }
+        return (first + i);
+    }
+
 }
 
 #endif /* _UNINITIALIZED_FUNCTIONS_HPP_ */
