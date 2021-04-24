bea5837f52ecd9295b4f34d09af570a9c8579b6f
MRNIU <a6813140@hotmail.com>
2021-04-24 14:24:44 +0800

    pref(VFS): update
    Signed-off-by: MRNIU <a6813140@hotmail.com>

--- src/fs/ramfs/ramfs.cpp
+++ src/fs/ramfs/ramfs.cpp
@@ -5,3 +5,23 @@
 // ramfs.cpp for Simple-XX/SimpleKernel.
 
 #include "ramfs.h"
+
+RAMFS::RAMFS(void) {
+    return;
+}
+
+RAMFS::~RAMFS(void) {
+    return;
+}
+
+int RAMFS::open(void) {
+    return 0;
+}
+
+int RAMFS::close(void) {
+    return 0;
+}
+
+int RAMFS::seek(void) {
+    return 0;
+}
