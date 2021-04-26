a0910e766ce5de6c76e59e74228c56fcf835fe8d
MRNIU <a6813140@hotmail.com>
2021-04-26 12:54:25 +0800

    pref(VFS): update
    Signed-off-by: MRNIU <a6813140@hotmail.com>

--- src/include/vfs.h
+++ src/include/vfs.h
@@ -9,34 +9,8 @@
 
 #include "stdint.h"
 #include "string"
-#include "vector"
+#include "list.hpp"
 #include "time.h"
-
-// 超级块
-// fs 挂载时从存储介质读入
-class SUPERBLOCK {
-private:
-    // block与inode的总量
-    uint32_t block_total;
-    uint32_t inode_total;
-    // 未使用与已使用的inode/block数量
-    uint32_t block_free;
-    uint32_t inode_free;
-    // block与inode的大小（block为1K,2K,4K；inode为128byte）
-    uint32_t block_size;
-    uint32_t inocde_size;
-    // 文件系统的挂在时间、最近一次写入数据的时间、最近一次检验磁盘的时间等文件系统的相关信息
-    // 一个validbit数值，若此文件系统已被挂载，则validbit为0，否则为1
-    bool valid;
-    // 文件系统描述--File system Description
-    // 块对应表--block bitmap 记录磁盘中使用与未使用的block号码
-    // inode对应表--inode bitmap 记录使用与未使用的inode号码
-
-protected:
-public:
-    SUPERBLOCK(void);
-    virtual ~SUPERBLOCK(void) = 0;
-};
 
 typedef uint32_t device_id_t;
 typedef uint32_t user_id_t;
@@ -45,10 +19,12 @@ typedef uint32_t mode_t;
 typedef uint32_t flag_t;
 
 // inode 索引节点
-class INODE {
+class inode_t {
 private:
-    // 以字节为单位表示的文件大小。
-    size_t bytes;
+protected:
+public:
+    // 文件大小 以字节为单位表示的
+    size_t size;
     // 设备 ID，标识容纳该文件的设备。
     device_id_t device_id;
     // 文件所有者的 User ID。
@@ -68,72 +44,81 @@ private:
     // 1个链接数，表示有多少个硬链接指向此inode。
     uint32_t hard_links;
     // 到文件系统存储位置的指针。通常是1K字节或者2K字节的存储容量为基本单位。
-    uint32_t point;
-
-protected:
-public:
-    INODE(void);
-    virtual ~INODE(void);
-} __attribute__((aligned(128)));
-
-// 目录
-class DENTRY {
-private:
-    uint32_t flag;
-
-protected:
-public:
-    DENTRY(void);
-    virtual ~DENTRY(void);
-    virtual int open();
-    virtual int close();
-    virtual int seek();
+    uint32_t pointer;
+    inode_t(void);
+    virtual ~inode_t(void);
 };
 
-// 文件
-class FILE {
+// 超级块
+// fs 挂载时从存储介质读入
+class superblock_t {
 private:
-    uint32_t flag;
-
 protected:
 public:
-    FILE(void);
-    virtual ~FILE(void);
-    virtual int open();
-    virtual int close();
-    virtual int seek();
+    // inode 与 block 的总量
+    uint32_t inode_total;
+    uint32_t block_total;
+    // 未使用的 inode 与 block 数量
+    uint32_t inode_free;
+    uint32_t block_free;
+    // block与inode的大小（block为1K,2K,4K；inode为128byte）
+    uint32_t block_size;
+    uint32_t inode_size;
+    // 文件系统的挂在时间、最近一次写入数据的时间、最近一次检验磁盘的时间等文件系统的相关信息
+    // 文件系统描述--File system Description
+    // 块对应表--block bitmap 记录磁盘中使用与未使用的block号码
+    // inode对应表--inode bitmap 记录使用与未使用的inode号码
+    superblock_t(void);
+    virtual ~superblock_t(void) = 0;
+    // 读取
+    virtual int read(void) = 0;
+};
+
+// 目录
+class dentry_t {
+private:
+protected:
+public:
+    uint32_t flag;
+    // 对应的 inode
+    inode_t *inode;
+    // 父目录
+    dentry_t *parent;
+    // 子目录
+    STL::list<dentry_t *> child;
+    // 文件名
+    STL::string name;
+    dentry_t(void);
+    virtual ~dentry_t(void);
 };
 
 // 实际的文件系统
 class FS {
 private:
-    // 超级块
-    STL::vector<SUPERBLOCK *> supers;
-
 protected:
+    // 超级块
+    STL::list<superblock_t *> supers;
+    // inode 链表
+    STL::list<inode_t *> inodes;
+    // 分配 inode
+    virtual inode_t *alloc_inode(void) = 0;
+
 public:
-    FS(void);
     // 文件系统名
     STL::string name;
-    virtual ~FS(void) = 0;
-    // 挂载
-    virtual int mount(void) = 0;
-    // 卸载
-    virtual int unmount(void) = 0;
-    // 读取超级块
-    virtual int read_super(void) = 0;
-    virtual int open(void)       = 0;
-    virtual int close(void)      = 0;
-    virtual int seek(void)       = 0;
+    FS(void);
+    virtual ~FS(void)                                              = 0;
+    virtual int mkdir(const STL::string _path, const mode_t _mode) = 0;
 };
 
 // 所有文件系统由 vfs 统一管理
-// 文件系统的注册
 // 操作文件时由 vfs 使用实际文件系统进行操作
 class VFS {
 private:
     // 管理的文件系统
-    STL::vector<FS *> fs;
+    STL::list<FS *> fs;
+    // 根目录
+    inode_t root;
 
 protected:
 public:
@@ -141,9 +126,11 @@ public:
     ~VFS(void);
     // 初始化根安装点
     int32_t init(void);
-    //文件系统相关
     // 添加一个文件系统
     int32_t register_filesystem(FS *_fs);
+    // 删除一个文件系统
+    int32_t unregister_filesystem(FS *_fs);
+    //文件系统相关
     // 挂载
     int mount(const char *source, const char *target,
               const char *filesystemtype, unsigned long mountflags,
@@ -161,7 +148,7 @@ public:
     int chdir(void);
     int fchdir(void);
     int getcwd(void);
-    int mkdir(void);
+    int mkdir(STL::string _path, mode_t _mode);
     int rmdir(void);
     int getdents(void);
     int getdents64(void);
@@ -215,6 +202,16 @@ public:
     int sendfile(void);
     int sendfile64(void);
     int readahead(void);
+};
+
+// 文件
+class file_t {
+private:
+protected:
+public:
+    uint32_t flag;
+    file_t(void);
+    virtual ~file_t(void);
 };
 
 static VFS vfs;
