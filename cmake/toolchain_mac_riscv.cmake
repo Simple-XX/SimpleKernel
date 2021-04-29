1d253c6726a4b7544305460de50791b5225cdfe3
Zone.N <Zone.Niuzh@hotmail.com>
2021-04-29 18:57:24 +0800

    merge: MRNIU
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- cmake/toolchain_mac_riscv.cmake
+++ cmake/toolchain_mac_riscv.cmake
@@ -1,11 +1,11 @@
 
 # This file is a part of Simple-XX/SimpleKernel (https://github.com/Simple-XX/SimpleKernel).
 # 
-# toolchain_mac_arm.cmake for Simple-XX/SimpleKernel.
+# toolchain_mac_riscv.cmake for Simple-XX/SimpleKernel.
 
 set(CMAKE_SYSTEM_NAME Darwin)
 set(CMAKE_SYSTEM_VERSION 1)
-set(CMAKE_SYSTEM_PROCESSOR ARM)
+set(CMAKE_SYSTEM_PROCESSOR RISCV)
 
 find_program(BREW brew)
 if (NOT BREW)
@@ -16,24 +16,32 @@ else ()
 endif ()
 
 # GCC
-find_program(GCC arm-none-eabi-gcc)
-if (NOT GCC)
-    message(FATAL_ERROR "arm-none-eabi-gcc not found.\n"
-            "Run `brew install arm-none-eabi-gcc` to install the toolchain")
+find_program(RISCV riscv64-unknown-elf-gcc)
+if (NOT RISCV)
+    message(FATAL_ERROR "riscv64-unknown-elf-gcc not found.\n"
+            "Run `brew tap riscv/riscv` and `brew install riscv-tools` to install the toolchain. Then add the bin path to you PATH.")
 else ()
-    message(STATUS "Found arm-none-eabi-gcc ${GCC}")
+    message(STATUS "Found riscv64-unknown-elf-gcc ${RISCV}.")
 endif ()
 
-set(TOOLCHAIN_PREFIX arm-none-eabi-)
+set(TOOLCHAIN_PREFIX riscv64-unknown-elf-)
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
 set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy)
 
 # qemu
-find_program(QEMU qemu-system-aarch64)
+find_program(QEMU qemu-system-riscv64)
 if (NOT QEMU)
     message(FATAL_ERROR "qemu not found.\n"
             "Please install qemu first.")
 else ()
     message(STATUS "Found qemu ${QEMU}")
 endif ()
+
+# opensbi
+# cd tools
+# git clone https://github.com/riscv/opensbi.git
+# cd opensbi
+# export CROSS_COMPILE=riscv64-unknown-elf-
+# make PLATFORM=generic clean
+# make PLATFORM=generic FW_JUMP_ADDR=0x80200000
