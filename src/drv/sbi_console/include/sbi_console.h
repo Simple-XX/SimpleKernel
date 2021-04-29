34d649cccd97115a1b566df5fe7e6f85f7a92e49
Zone.N <Zone.Niuzh@hotmail.com>
2021-04-29 18:57:24 +0800

    merge: MRNIU
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

---
+++ src/drv/sbi_console/include/sbi_console.h
@@ -0,0 +1,35 @@
+
+// This file is a part of Simple-XX/SimpleKernel
+// (https://github.com/Simple-XX/SimpleKernel).
+//
+// sbi_console.h for Simple-XX/SimpleKernel.
+
+#ifndef _SBI_CONSOLE_H_
+#define _SBI_CONSOLE_H_
+
+#include "opensbi.h"
+#include "stdint.h"
+#include "color.h"
+
+class SBI_CONSOLE {
+private:
+    OPENSBI opensbi;
+
+protected:
+public:
+    SBI_CONSOLE(void);
+    ~SBI_CONSOLE(void);
+    void put_char(const char _c) const;
+    // 写字符串
+    void write_string(const char *_s) const;
+    // 写字符串
+    void write(const char *_s, size_t _len) const;
+    // 读字符 TODO
+    uint8_t get_char(void) const;
+    // 设置颜色 TODO
+    void set_color(const COLOR::color_t _color) const;
+    // 获取颜色 TODO
+    COLOR::color_t get_color(void) const;
+};
+
+#endif /* _SBI_CONSOLE_H_ */
