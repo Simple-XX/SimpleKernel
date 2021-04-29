516fc28a3b72eb93829ab3fcd8e812162da449cf
Zone.N <Zone.Niuzh@hotmail.com>
2021-04-29 19:13:50 +0800

    merge: MRNIU
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

---
+++ src/arch/i386/apic/ioapic.cpp
@@ -0,0 +1,21 @@
+
+// This file is a part of Simple-XX/SimpleKernel
+// (https://github.com/Simple-XX/SimpleKernel).
+//
+// ioapic.cpp for Simple-XX/SimpleKernel.
+
+#include "intr.h"
+#include "cpu.hpp"
+#include "io.h"
+#include "apic.h"
+
+// TODO
+
+IOAPIC::IOAPIC(void) {
+
+    return;
+}
+
+IOAPIC::~IOAPIC(void) {
+    return;
+}
