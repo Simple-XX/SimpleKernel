8c37f7865ae1e9e966ee563e9fcf75fd9a1a0b39
Zone.N <Zone.Niuzh@hotmail.com>
2021-05-01 11:57:47 +0800

    pref(PMM, MULTIBOOT2): add get_e820()
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/arch/i386/pmm/pmm.cpp
+++ src/arch/i386/pmm/pmm.cpp
@@ -11,9 +11,6 @@
 #include "stdio.h"
 #include "pmm.h"
 
-extern MULTIBOOT2::multiboot_memory_map_entry_t *mmap_entries;
-extern MULTIBOOT2::multiboot_mmap_tag_t *        mmap_tag;
-
 // TODO: 优化空间
 // TODO: 换一种更灵活的方法
 COMMON::physical_pages_t PMM::phy_pages[COMMON::PMM_PAGE_MAX_SIZE];
@@ -23,31 +20,6 @@ size_t                   PMM::high_pages = 0;
 FIRSTFIT                 PMM::high(&phy_pages[COMMON::KERNEL_PAGES]);
 size_t                   PMM::pages                    = 0;
 FIRSTFIT *               PMM::zone[COMMON::ZONE_COUNT] = {&normal, &high};
-
-// TODO: 太难看了也
-// 这里的 addr 与 len 4k 对齐
-static void get_ram_info(e820map_t *e820map) {
-    for (; (uint8_t *)MULTIBOOT2::mmap_entries <
-           (uint8_t *)MULTIBOOT2::mmap_tag + MULTIBOOT2::mmap_tag->size;
-         MULTIBOOT2::mmap_entries =
-             (MULTIBOOT2::multiboot_memory_map_entry_t
-                  *)((uint32_t)MULTIBOOT2::mmap_entries +
-                     ((struct MULTIBOOT2::multiboot_tag_mmap *)
-                          MULTIBOOT2::mmap_tag)
-                         ->entry_size)) {
-        // 如果是可用内存
-        if (MULTIBOOT2::mmap_entries->type ==
-            MULTIBOOT2::MULTIBOOT_MEMORY_AVAILABLE) {
-            e820map->map[e820map->nr_map].addr =
-                reinterpret_cast<uint8_t *>(MULTIBOOT2::mmap_entries->addr);
-            e820map->map[e820map->nr_map].length =
-                MULTIBOOT2::mmap_entries->len;
-            e820map->map[e820map->nr_map].type = MULTIBOOT2::mmap_entries->type;
-            e820map->nr_map++;
-        }
-    }
-    return;
-}
 
 PMM::PMM(void) {
     return;
@@ -67,7 +39,8 @@ int32_t PMM::init(void) {
 #endif
     e820map_t e820map;
     bzero(&e820map, sizeof(e820map_t));
-    get_ram_info(&e820map);
+    // TODO: 处理不能使用的内存，在虚拟内存映射时需要考虑
+    MULTIBOOT2::get_e820(e820map);
     // 计算可用的内存
     // 这里需要保证 addr 是按照 PMM_PAGE_MASK 对齐的
     for (size_t i = 0; i < e820map.nr_map; i++) {
