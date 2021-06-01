81ae60919216fecbf675a2cbfb4e6d484bf53f09
Zone.N <Zone.Niuzh@hotmail.com>
2021-06-01 18:44:10 +0800

    pref(boot): move multiboot2.h to boot
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- cmake/header_files.cmake
+++ cmake/header_files.cmake
@@ -3,10 +3,10 @@
 # Based on https://github.com/SynestiaOS/SynestiaOS
 # header_files.cmake for Simple-XX/SimpleKernel.
 
-function(target_include_kernel_header_files Target)
-    target_include_directories(${Target} PRIVATE ${SimpleKernel_SOURCE_CODE_DIR}/kernel/include)
-endfunction()
-
 function(target_include_arch_header_files Target)
     target_include_directories(${Target} PRIVATE ${SimpleKernel_SOURCE_CODE_DIR}/arch/${SimpleKernelArch}/)
+endfunction()
+
+function(target_include_kernel_header_files Target)
+    target_include_directories(${Target} PRIVATE ${SimpleKernel_SOURCE_CODE_DIR}/kernel/include)
 endfunction()
