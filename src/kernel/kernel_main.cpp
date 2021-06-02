d9da8b255e7fa4b311c708010188253b7f681bb3
Zone.N <Zone.Niuzh@hotmail.com>
2021-06-02 09:57:00 +0800

    pref(MULTIBOOT): move to boot.S
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/kernel/kernel_main.cpp
+++ src/kernel/kernel_main.cpp
@@ -12,17 +12,8 @@
 #include "pmm.h"
 #include "kernel.h"
 
-#if defined(__i386__) || defined(__x86_64__)
-#include "multiboot2.h"
-#endif
-
 // 内核入口
-void kernel_main(uint32_t size __attribute__((unused)),
-                 void *   addr __attribute__((unused))) {
-#if defined(__i386__) || defined(__x86_64__)
-    // __x86_64__ 时用于传递 grub 信息
-    MULTIBOOT2::multiboot2_init(size, addr);
-#endif
+void kernel_main(uint32_t, void *) {
     cpp_init();
     // 物理内存初始化
     pmm.init();
@@ -34,50 +25,6 @@ void kernel_main(uint32_t size __attribute__((unused)),
     }
     assert(0);
     return;
-}
-
-int32_t test_pmm(void) {
-    // TODO: 分配的地址应该在相应区域内
-    uint32_t cd         = 0xCD;
-    uint8_t *addr1      = nullptr;
-    uint8_t *addr2      = nullptr;
-    uint8_t *addr3      = nullptr;
-    uint8_t *addr4      = nullptr;
-    uint32_t free_count = pmm.free_pages_count(COMMON::NORMAL);
-    addr1               = (uint8_t *)pmm.alloc_page(0x9F, COMMON::NORMAL);
-    if (addr1 != nullptr) {
-        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 0x9F);
-        *(uint32_t *)addr1 = cd;
-        assert((*(uint32_t *)addr1 == cd));
-        pmm.free_page(addr1, 0x9F, COMMON::NORMAL);
-        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
-    }
-    addr2 = (uint8_t *)pmm.alloc_page(1, COMMON::NORMAL);
-    if (addr2 != nullptr) {
-        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 1);
-        *(int *)addr2 = cd;
-        assert((*(uint32_t *)addr2 == cd));
-        pmm.free_page(addr2, 1, COMMON::NORMAL);
-        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
-    }
-    addr3 = (uint8_t *)pmm.alloc_page(1024, COMMON::NORMAL);
-    if (addr3 != nullptr) {
-        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 1024);
-        *(int *)addr3 = cd;
-        assert((*(uint32_t *)addr3 == cd));
-        pmm.free_page(addr3, 1024, COMMON::NORMAL);
-        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
-    }
-    addr4 = (uint8_t *)pmm.alloc_page(4096, COMMON::NORMAL);
-    if (addr4 != nullptr) {
-        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 4096);
-        *(int *)addr4 = cd;
-        assert((*(uint32_t *)addr4 == cd));
-        pmm.free_page(addr4, 4096, COMMON::NORMAL);
-        assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
-    }
-    printf("pmm test done.\n");
-    return 0;
 }
 
 void show_info(void) {
