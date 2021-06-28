b93aa90d5f848f7be78c47bb4b55e36332f18b02
Zone.N <Zone.Niuzh@hotmail.com>
2021-06-29 00:52:12 +0800

    comment(COMMON): update
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/include/common.h
+++ src/include/common.h
@@ -38,33 +38,31 @@ namespace COMMON {
 
 #if defined(__i386__) || defined(__x86_64__)
     // 物理内存大小 2GB
+    // TODO: 由引导程序传递
     static constexpr const uint32_t PMM_MAX_SIZE = 0x80000000;
     // 内核的偏移地址
     static constexpr const uint32_t KERNEL_BASE = 0x0;
     // 内核占用大小，与 KERNEL_START_ADDR，KERNEL_END_ADDR 无关
+    // 这部分内存是内核保留的
     // 64MB
     static constexpr const uint32_t KERNEL_SIZE = 0x4000000;
-
     // 页大小 4KB
     static constexpr const size_t PAGE_SIZE = 4 * KB;
 #elif defined(__riscv)
     // 物理内存大小 128MB
+    // TODO: 由引导程序传递
     static constexpr const uint64_t PMM_MAX_SIZE = 0x8000000;
     // 内核的偏移地址
     static constexpr const uint64_t KERNEL_BASE = 0x80020000;
     // 内核占用大小，与 KERNEL_START_ADDR，KERNEL_END_ADDR 无关
+    // 这部分内存是内核保留的
     // 8MB
     static constexpr const uint64_t KERNEL_SIZE = 0x800000;
     // 页大小 4KB
     static constexpr const size_t PAGE_SIZE = 4 * KB;
 #endif
 
-    // 页掩码，用于 4KB 对齐
-    static constexpr const uint64_t PAGE_MASK =
-        0xFFFFFFFFFFFFFFFF - PAGE_SIZE + 1;
-    // 物理页数量 131072,
-    // 0x20000，除去外设映射，实际可用物理页数量为 159 + 130800 =
-    // 130959 (这是物理内存为 512MB 的情况)
+    // 物理页数量
     static constexpr const uint64_t PMM_PAGE_MAX_SIZE =
         PMM_MAX_SIZE / PAGE_SIZE;
 
@@ -80,7 +78,7 @@ namespace COMMON {
         int32_t ref;
     };
 
-    // zone 机制
+    // zone 索引
     // NOTE: 这里的 ZONE 与 Linux 中的划分不同，只是方便控制分配内存的位置
     enum zone_t : uint8_t {
         // 与 KERNEL_SIZE 对应
@@ -88,7 +86,7 @@ namespace COMMON {
         // 高于 KERNEL_SIZE 的地址
         HIGH,
     };
-
+    // zone 数量
     static constexpr const size_t ZONE_COUNT = 2;
 
     // 对齐 向上取整
