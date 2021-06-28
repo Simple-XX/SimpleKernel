56225dd2dcae9f67c4c96332d72bfbaa37b9c665
Zone.N <Zone.Niuzh@hotmail.com>
2021-06-28 23:05:01 +0800

    pref(virtio): replace constexpr with struct
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/drv/virtio/include/virtio.h
+++ src/drv/virtio/include/virtio.h
@@ -23,43 +23,46 @@ protected:
     static constexpr const uint64_t VERSION     = 0x02;
     // virtio mmio 控制寄存器
     // virtio-v1.1#4.2.2
-    // a Little Endian equivalent of the “virt” string: 0x74726976
-    static constexpr const uint64_t MMIO_REG_MAGIC_VALUE = 0x0;
-    // Device version number
-    // 0x2, Legacy devices(see 4.2.4 Legacy interface) used 0x1.
-    static constexpr const uint64_t MMIO_REG_VERSION = 0x4;
-    // Virtio Subsystem Device ID
-    static constexpr const uint64_t MMIO_REG_DEVICE_ID = 0x8;
-    // 0x554D4551
-    static constexpr const uint64_t MMIO_REG_VENDOR_ID           = 0xC;
-    static constexpr const uint64_t MMIO_REG_DEVICE_FEATURES     = 0x10;
-    static constexpr const uint64_t MMIO_REG_DEVICE_FEATURES_SEL = 0x14;
-    static constexpr const uint64_t MMIO_REG_DRIVER_FEATURES     = 0x20;
-    static constexpr const uint64_t MMIO_REG_DRIVER_FEATURES_SEL = 0x24;
-    // select queue, write-only
-    static constexpr const uint64_t MMIO_REG_QUEUE_SEL = 0x30;
-    // max size of current queue, read-only
-    static constexpr const uint64_t MMIO_REG_QUEUE_NUM_MAX = 0x34;
-    // size of current queue, write-only
-    static constexpr const uint64_t MMIO_REG_QUEUE_NUM = 0x38;
-    // ready bit
-    static constexpr const uint64_t MMIO_REG_QUEUE_READY = 0x44;
-    // write-only
-    static constexpr const uint64_t MMIO_REG_QUEUE_NOTIFY = 0x50;
-    // read-only
-    static constexpr const uint64_t MMIO_REG_INTERRUPT_STATUS = 0x60;
-    // write-only
-    static constexpr const uint64_t MMIO_REG_INTERRUPT_ACK = 0x64;
-    // read/write
-    static constexpr const uint64_t MMIO_REG_STATUS            = 0x70;
-    static constexpr const uint64_t MMIO_REG_QUEUE_DESC_LOW    = 0x80;
-    static constexpr const uint64_t MMIO_REG_QUEUE_DESC_HIGH   = 0x84;
-    static constexpr const uint64_t MMIO_REG_QUEUE_DRIVER_LOW  = 0x90;
-    static constexpr const uint64_t MMIO_REG_QUEUE_DRIVER_HIGH = 0x94;
-    static constexpr const uint64_t MMIO_REG_QUEUE_DEVICE_LOW  = 0xA0;
-    static constexpr const uint64_t MMIO_REG_QUEUE_DEVICE_HIGH = 0xA4;
-    static constexpr const uint64_t MMIO_REG_CONFIG_GEN        = 0xFC;
-    static constexpr const uint64_t MMIO_REG_CONFIG            = 0x100;
+    struct virtio_regs_t {
+        // a Little Endian equivalent of the “virt” string: 0x74726976
+        uint32_t magic;
+        // Device version number
+        // 0x2, Legacy devices(see 4.2.4 Legacy interface) used 0x1.
+        uint32_t version;
+        // Virtio Subsystem Device ID
+        uint32_t device_id;
+        uint32_t Vendor_id;
+        uint32_t device_features;
+        uint32_t device_features_sel;
+        uint32_t _reserved0[2];
+        uint32_t driver_features;
+        uint32_t driver_features_sel;
+        uint32_t _reserved1[2];
+        uint32_t queue_sel;
+        uint32_t queue_num_max;
+        uint32_t queue_num;
+        uint32_t _reserved2[2];
+        uint32_t queue_ready;
+        uint32_t _reserved3[2];
+        uint32_t queue_notify;
+        uint32_t _reserved4[3];
+        uint32_t interrupt_status;
+        uint32_t interrupt_ack;
+        uint32_t _reserved5[2];
+        uint32_t status;
+        uint32_t _reserved6[3];
+        uint32_t queue_desc_low;
+        uint32_t queue_desc_high;
+        uint32_t _reserved7[2];
+        uint32_t queue_driver_low;
+        uint32_t queue_driver_high;
+        uint32_t _reserved8[2];
+        uint32_t queue_device_low;
+        uint32_t queue_device_high;
+        uint32_t _reserved9[21];
+        uint32_t config_generation;
+        uint32_t config[0];
+    } __attribute__((packed));
 
     // Device Status Field
     // virtio-v1.1#2.1
@@ -188,12 +191,12 @@ protected:
         {NAME2STR(VIRTIO_F_VERSION_1), VIRTIO_F_VERSION_1, false},
     };
 
-    // virtio 基地址
-    void *          base_addr;
+    // virtio mmio 寄存器基地址
+    virtio_regs_t * regs;
     virtio_queue_t *queue;
-    uint32_t        read(uint32_t _off);
-    uint64_t        read64(uint32_t _off);
-    void            write(uint32_t _off, uint32_t _val);
+    uint32_t        read(void *_off);
+    uint64_t        read64(void *_off);
+    void            write(void *_off, uint32_t _val);
     // 设置 features
     bool set_features(const mystl::vector<feature_t> &_features);
     // 设置队列
@@ -227,6 +230,7 @@ public:
         IOMMU_DEVICE,
         MEMORY_DEVICE,
     } virt_device_type_t;
+
     // virtio 设备类型名名称
     const char *const virtio_device_name[25] = {
         "reserved (invalid)",
@@ -260,38 +264,6 @@ public:
     // _type: 设备类型
     VIRTIO(void *_addr, virt_device_type_t _type);
     ~VIRTIO(void);
-
-    /**
-     * @brief 初始化磁盘，配置一些参数，详细流程可以查看
-     *        virtio文档5.2节
-     *        https://docs.oasis-open.org/virtio/virtio/v1.1/virtio-v1.1.pdf
-     *
-     */
-    void VirtioInit();
-
-    /**
-     * @brief 读取磁盘的相应扇区到b->data
-     */
-    void VirtioRead(char *buf, int sectorno);
-
-    /**
-     *  @brief 将b->data写入到磁盘对应扇区
-     */
-    void VirtioWrite(char *buf, int sectorno);
-
-    /**
-     * @brief virtio中断处理函数
-     *
-     */
-    void VirtioIntr();
-
-    /**
-     * @brief 读写磁盘
-     *
-     * @param buf   读写数据缓存区
-     * @param write 0代表读，1代表写
-     */
-    void VirtioRw(char *buf, uint64_t sector, int write);
 };
 
 #endif /* _VIRTIO_H_ */
--- src/drv/virtio/virtio.cpp
+++ src/drv/virtio/virtio.cpp
@@ -8,6 +8,7 @@
 #include "string.h"
 #include "stddef.h"
 #include "stdio.h"
+#include "io.h"
 #include "common.h"
 #include "assert.h"
 #include "vmm.h"
@@ -90,27 +91,27 @@ void VIRTIO::virtio_queue_t::free_desc(uint32_t desc) {
     return;
 }
 
-uint32_t VIRTIO::read(uint32_t _off) {
-    return *((uint32_t *)((ptrdiff_t)base_addr + _off));
+uint32_t VIRTIO::read(void *_off) {
+    return io.read32(_off);
 }
 
-uint64_t VIRTIO::read64(uint32_t _off) {
-    return *((uint64_t *)((ptrdiff_t)base_addr + _off));
+uint64_t VIRTIO::read64(void *_off) {
+    return io.read64(_off);
 }
 
-void VIRTIO::write(uint32_t _off, uint32_t _val) {
-    *((uint32_t *)((ptrdiff_t)base_addr + _off)) = _val;
+void VIRTIO::write(void *_off, uint32_t _val) {
+    io.write32(_off, _val);
     return;
 }
 
 bool VIRTIO::set_features(const mystl::vector<feature_t> &_features) {
     // 首先获取硬件信息
     // 0~31 位
-    write(MMIO_REG_DEVICE_FEATURES_SEL, 0);
-    uint32_t device_feature_low = read(MMIO_REG_DEVICE_FEATURES);
+    write(&regs->device_features_sel, 0);
+    uint32_t device_feature_low = read(&regs->device_features);
     // 32~63 位
-    write(MMIO_REG_DEVICE_FEATURES_SEL, 1);
-    uint32_t device_feature_high = read(MMIO_REG_DEVICE_FEATURES);
+    write(&regs->device_features_sel, 1);
+    uint32_t device_feature_high = read(&regs->device_features);
 
     // TODO: 如果有设备不支持的 feature，需要有错误反馈
     for (auto i : _features) {
@@ -121,10 +122,10 @@ bool VIRTIO::set_features(const mystl::vector<feature_t> &_features) {
             if ((i.status == true)) {
                 if ((device_feature_high & (1 << (i.bit - 32))) == true) {
                     // 选择写高位
-                    write(MMIO_REG_DRIVER_FEATURES_SEL, 1);
+                    write(&regs->device_features_sel, 1);
                     // 写入
-                    write(MMIO_REG_DRIVER_FEATURES,
-                          read(MMIO_REG_DRIVER_FEATURES) | (1 << (i.bit - 32)));
+                    write(&regs->driver_features,
+                          read(&regs->driver_features) | (1 << (i.bit - 32)));
                 }
                 else {
                     err("Not support: %s\n", i.name.c_str());
@@ -136,10 +137,10 @@ bool VIRTIO::set_features(const mystl::vector<feature_t> &_features) {
             if ((i.status == true)) {
                 if (((device_feature_low & (1 << i.bit)) == true)) {
                     // 选择写低位
-                    write(MMIO_REG_DRIVER_FEATURES_SEL, 0);
+                    write(&regs->driver_features_sel, 0);
                     // 写入
-                    write(MMIO_REG_DRIVER_FEATURES,
-                          read(MMIO_REG_DRIVER_FEATURES) | (1 << i.bit));
+                    write(&regs->driver_features,
+                          read(&regs->driver_features) | (1 << i.bit));
                 }
                 else {
                     err("Not support: %s\n", i.name.c_str());
@@ -151,40 +152,40 @@ bool VIRTIO::set_features(const mystl::vector<feature_t> &_features) {
 }
 
 void VIRTIO::add_to_device(uint32_t _queue_sel) {
-    write(MMIO_REG_QUEUE_SEL, _queue_sel);
-    write(MMIO_REG_QUEUE_NUM, queue->virtq->len);
-    write(MMIO_REG_QUEUE_DESC_LOW,
+    write(&regs->queue_sel, _queue_sel);
+    write(&regs->queue_num, queue->virtq->len);
+    write(&regs->queue_desc_low,
           queue->virtq->phys +
               ((uint8_t *)queue->virtq->desc - (uint8_t *)queue->virtq));
-    write(MMIO_REG_QUEUE_DESC_HIGH, 0);
-    write(MMIO_REG_QUEUE_DRIVER_LOW,
+    write(&regs->queue_desc_high, 0);
+    write(&regs->queue_driver_low,
           queue->virtq->phys +
               ((uint8_t *)queue->virtq->avail - (uint8_t *)queue->virtq));
-    write(MMIO_REG_QUEUE_DRIVER_HIGH, 0);
-    write(MMIO_REG_QUEUE_DEVICE_LOW,
+    write(&regs->queue_driver_high, 0);
+    write(&regs->queue_device_low,
           queue->virtq->phys +
               ((uint8_t *)queue->virtq->used - (uint8_t *)queue->virtq));
-    write(MMIO_REG_QUEUE_DEVICE_HIGH, 0);
-    write(MMIO_REG_QUEUE_READY, 1);
+    write(&regs->queue_device_high, 0);
+    write(&regs->queue_ready, 1);
     return;
 }
 
-VIRTIO::VIRTIO(void *_addr, virt_device_type_t _type) : base_addr(_addr) {
+VIRTIO::VIRTIO(void *_addr, virt_device_type_t _type)
+    : regs((virtio_regs_t *)_addr) {
     // 检查相关值
-    assert(read(MMIO_REG_MAGIC_VALUE) == MAGIC_VALUE);
-    assert(read(MMIO_REG_VERSION) == VERSION);
+    assert(read(&regs->magic) == MAGIC_VALUE);
+    assert(read(&regs->version) == VERSION);
     // 检查类型是否符合
-    assert(read(MMIO_REG_DEVICE_ID) == _type);
+    assert(read(&regs->device_id) == _type);
     // 初始化
     // virtio-v1.1#3.1.1
     // 重置设备
-    write(MMIO_REG_STATUS, 0);
+    write(&regs->status, 0);
     // 置位 ACKNOWLEDGE
-    VIRTIO::write(MMIO_REG_STATUS,
-                  read(MMIO_REG_STATUS) | DEVICE_STATUS_ACKNOWLEDGE);
+    VIRTIO::write(&regs->status,
+                  read(&regs->status) | DEVICE_STATUS_ACKNOWLEDGE);
     // 置位 DRIVER
-    VIRTIO::write(MMIO_REG_STATUS,
-                  read(MMIO_REG_STATUS) | DEVICE_STATUS_DRIVER);
+    VIRTIO::write(&regs->status, read(&regs->status) | DEVICE_STATUS_DRIVER);
     // 接下来设置设备相关 feature，交给特定设备进行
     // 跳转到 virtio_blk.cpp 的构造函数
     return;
