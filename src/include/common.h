7ab1c955b01b0b89d0aad62f859a5f278ae296bf
Zone.N <Zone.Niuzh@hotmail.com>
2021-06-29 00:33:12 +0800

    feat(COMMON): add BYTE and so on
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/include/common.h
+++ src/include/common.h
@@ -40,13 +40,23 @@ namespace COMMON {
         kernel_data_end;
     static constexpr const void *KERNEL_END_ADDR = kernel_end;
 
-    // 对齐 向上取整
-    inline void *ALIGN(const void *_addr, uint64_t _align) {
-        uint8_t *tmp = (uint8_t *)_addr;
-        return (void *)((uint64_t)(tmp + _align - 1) & (~(_align - 1)));
-    }
+    static constexpr const size_t BYTE = 0x1;
+    static constexpr const size_t KB   = 0x400;
+    static constexpr const size_t MB   = 0x100000;
 
-    inline uint64_t ALIGN(uint64_t _x, uint64_t _align) {
+    // 页大小 4KB
+    static constexpr const size_t PAGE_SIZE = 4 * KB;
+
+    // 对齐 向上取整
+    // 针对指针
+    template <class T>
+    T ALIGN(const T _addr, const size_t _align) {
+        uint8_t *tmp = (uint8_t *)_addr;
+        return (T)((ptrdiff_t)(tmp + _align - 1) & (~(_align - 1)));
+    }
+    // 针对整数
+    template <>
+    uint64_t ALIGN(const uint64_t _x, const size_t _align) {
         return ((_x + _align - 1) & (~(_align - 1)));
     }
 
