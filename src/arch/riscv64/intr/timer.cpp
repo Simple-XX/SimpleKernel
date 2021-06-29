44d25cc7cf27435eed562509a92df338282a51e1
Zone.N <Zone.Niuzh@hotmail.com>
2021-06-29 22:28:06 +0800

    fear(plic): add plic to riscv
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/arch/riscv64/intr/timer.cpp
+++ src/arch/riscv64/intr/timer.cpp
@@ -11,27 +11,26 @@
 #include "intr.h"
 
 namespace TIMER {
-    static uint64_t tick = 0;
     // timer interrupt interval
     static constexpr const uint64_t INTERVAL = 390000000 / 200;
 
     void set_next(void) {
+        // 调用 opensbi 提供的接口设置时钟
         opensbi.set_timer(CPU::READ_TIME() + INTERVAL);
     }
 
     void timer_intr(void) {
+        // 每次执行中断时设置下一次中断的时间
         set_next();
-        tick++;
-        if ((tick % 10) == 0) {
-            printf("timer tick: %d\n", tick);
-        }
     }
 
     void init(void) {
         // enable supervisor-mode timer interrupts.
         CPU::WRITE_SIE(CPU::READ_SIE() | CPU::SIE_STIE);
-        INTR::register_interrupt_handler(INTR::INTR_S_TIMER, timer_intr);
+        // 注册中断函数
+        CLINT::register_interrupt_handler(CLINT::INTR_S_TIMER, timer_intr);
+        // 设置初次中断
         opensbi.set_timer(CPU::READ_TIME());
-        printf("timerinit\n");
+        printf("timer init\n");
     }
 };
