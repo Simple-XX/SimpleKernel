d17ba70860e112f996ed6a68e680c1a4ead992f3
Zone.N <Zone.Niuzh@hotmail.com>
2021-07-08 00:59:02 +0800

    pref(pmm): working on riscv pmm
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/arch/riscv64/pmm/pmm.cpp
+++ src/arch/riscv64/pmm/pmm.cpp
@@ -5,20 +5,13 @@
 // pmm.cpp for Simple-XX/SimpleKernel.
 
 #include "string.h"
-#include "memlayout.h"
 #include "stdio.h"
+#include "common.h"
+#include "firstfit.h"
 #include "pmm.h"
 
-// TODO: 优化空间
-// TODO: 换一种更灵活的方法
-COMMON::physical_pages_t PMM::phy_pages[COMMON::PMM_PAGE_MAX_SIZE];
-size_t                   PMM::normal_pages = 0;
-FIRSTFIT                 PMM::normal(phy_pages);
-// FIRSTFIT  PMM::normal((COMMON::physical_pages_t *)0x23);
-size_t    PMM::high_pages = 0;
-FIRSTFIT  PMM::high(&phy_pages[COMMON::KERNEL_PAGES]);
-size_t    PMM::pages                    = 0;
-FIRSTFIT *PMM::zone[COMMON::ZONE_COUNT] = {&normal, &high};
+size_t     PMM::total_pages = 0;
+ALLOCATOR *PMM::allocator   = nullptr;
 
 PMM::PMM(void) {
     return;
@@ -28,71 +21,50 @@ PMM::~PMM(void) {
     return;
 }
 
-// TODO: 将各个平台的类似代码进行整合
-int32_t PMM::init(void) {
-    for (uint8_t *addr = (uint8_t *)COMMON::ALIGN(COMMON::KERNEL_END_ADDR,
-                                                  COMMON::PAGE_SIZE);
-         addr < (uint8_t *)MEMLAYOUT::DRAM_END; addr += COMMON::PAGE_SIZE) {
-        // 跳过 0x00 开始的一页，便于判断 nullptr
-        if (addr == nullptr) {
-            continue;
-        }
-        // 初始化可用内存段的物理页数组
-        // 地址对应的物理页数组下标
-        phy_pages[pages].addr = addr;
-        // 内核已使用部分
-        if (addr >=
-                COMMON::ALIGN(COMMON::KERNEL_START_ADDR, COMMON::PAGE_SIZE) &&
-            addr < COMMON::ALIGN(COMMON::KERNEL_END_ADDR, COMMON::PAGE_SIZE)) {
-            phy_pages[pages].ref = 1;
-        }
-        else {
-            phy_pages[pages].ref = 0;
-        }
-// #define DEBUG
-#ifdef DEBUG
-        printf("phy_pages[%d].addr = addr: 0x%X\n", pages,
-               phy_pages[pages].addr);
-#undef DEBUG
-#endif
-        pages++;
-    }
-    // 计算各个分区大小
-    normal_pages = COMMON::KERNEL_PAGES;
-    high_pages   = pages - normal_pages;
-    mamage_init();
+bool PMM::init(void) {
+    // TODO: 这三个要动态获取
+    start       = COMMON::KERNEL_START_ADDR;
+    length      = COMMON::PMM_SIZE;
+    total_pages = length / COMMON::PAGE_SIZE;
+    // 分配器使用 fitst fit 分配器
+    static FIRSTFIT first_fit_allocator(start, length);
+    allocator = (ALLOCATOR *)&first_fit_allocator;
     printf("pmm_init\n");
+    return true;
+}
+
+const void *PMM::alloc_page(void) {
     return 0;
 }
 
-void PMM::mamage_init(void) {
-    normal.init(normal_pages, "NORMAL");
-    high.init(high_pages, "HIGH");
+const void *PMM::alloc_pages(size_t _len) {
+    (void)_len;
+    return 0;
+}
+
+bool PMM::alloc_pages(void *_addr, size_t _len) {
+    (void)_addr;
+    (void)_len;
+    return true;
+}
+
+void PMM::free_page(const void *_addr) {
+    (void)_addr;
     return;
 }
 
-void *PMM::alloc_page(uint32_t _pages, COMMON::zone_t _zone) {
-    void *addr = nullptr;
-    if (_pages > 0) {
-        addr = zone[_zone]->alloc(_pages);
-    }
-    if (addr == nullptr) {
-        printf("No enough mem: 0x%X pages.\n", _pages);
-    }
-    return addr;
-}
-
-void PMM::free_page(void *_addr, uint32_t _pages, COMMON::zone_t _zone) {
-    if (_addr == nullptr) {
-        printf("addr is null.\n");
-        return;
-    }
-    zone[_zone]->free(_addr, _pages);
+void PMM::free_pages(const void *_addr, size_t _len) {
+    (void)_addr;
+    (void)_len;
     return;
 }
 
-uint32_t PMM::free_pages_count(COMMON::zone_t _zone) {
-    return zone[_zone]->free_pages_count();
+uint64_t PMM::get_used_pages_count(void) const {
+    return 0;
+}
+
+uint64_t PMM::get_free_pages_count(void) const {
+    return 0;
 }
 
 PMM pmm;
