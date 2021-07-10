c787a8ce6a14e171f09e6063d2e08cf0b377de2a
Zone.N <Zone.Niuzh@hotmail.com>
2021-07-10 22:47:25 +0800

    feat(firstfit, pmm): replace linklist with bitmap
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/kernel/test.cpp
+++ src/kernel/test.cpp
@@ -12,21 +12,23 @@
 #include "kernel.h"
 
 int32_t test_pmm(void) {
-
     size_t free_pages = pmm.get_free_pages_count();
-    printf("pmm.get_free_pages_count(): 0x%X\n", pmm.get_free_pages_count());
-    assert(free_pages == COMMON::PMM_SIZE / COMMON::PAGE_SIZE);
+    auto   kernel_pages =
+        ((uint8_t *)COMMON::ALIGN(COMMON::KERNEL_END_ADDR, COMMON::PAGE_SIZE) -
+         (uint8_t *)COMMON::ALIGN(COMMON::KERNEL_START_ADDR,
+                                  COMMON::PAGE_SIZE)) /
+        COMMON::PAGE_SIZE;
+    assert(free_pages == (COMMON::PMM_SIZE / COMMON::PAGE_SIZE) - kernel_pages);
     size_t used_pages = pmm.get_used_pages_count();
-    printf("pmm.get_used_pages_count(): 0x%X\n", pmm.get_used_pages_count());
-    assert(used_pages == 0);
+    assert(used_pages == kernel_pages);
     auto addr1 = pmm.alloc_pages(2);
-    assert(pmm.get_used_pages_count() == 2);
+    assert(pmm.get_used_pages_count() == 2 + kernel_pages);
     auto addr2 = pmm.alloc_pages(3);
-    assert(pmm.get_used_pages_count() == 5);
+    assert(pmm.get_used_pages_count() == 5 + kernel_pages);
     auto addr3 = pmm.alloc_pages(100);
-    assert(pmm.get_used_pages_count() == 105);
+    assert(pmm.get_used_pages_count() == 105 + kernel_pages);
     auto addr4 = pmm.alloc_pages(100);
-    assert(pmm.get_used_pages_count() == 205);
+    assert(pmm.get_used_pages_count() == 205 + kernel_pages);
     auto addr5 = pmm.alloc_pages(0xFFFFFFFF);
     assert(addr5 == nullptr);
     pmm.free_pages(addr1, 2);
@@ -34,46 +36,6 @@ int32_t test_pmm(void) {
     pmm.free_pages(addr3, 100);
     pmm.free_pages(addr4, 100);
     assert(pmm.get_free_pages_count() == free_pages);
-
-    // TODO: 分配的地址应该在相应区域内
-    // uint32_t cd    = 0xCD;
-    // uint8_t *addr1 = nullptr;
-    // uint8_t *addr2      = nullptr;
-    // uint8_t *addr3      = nullptr;
-    // uint8_t *addr4      = nullptr;
-    // uint32_t free_count = pmm.free_pages_count(COMMON::NORMAL);
-    // addr1 = (uint8_t *)pmm.alloc_page(0x9F, COMMON::NORMAL);
-    // if (addr1 != nullptr) {
-    //     assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 0x9F);
-    //     *(uint32_t *)addr1 = cd;
-    //     assert((*(uint32_t *)addr1 == cd));
-    //     pmm.free_page(addr1, 0x9F, COMMON::NORMAL);
-    //     assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
-    // }
-    // addr2 = (uint8_t *)pmm.alloc_page(1, COMMON::NORMAL);
-    // if (addr2 != nullptr) {
-    //     assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 1);
-    //     *(int *)addr2 = cd;
-    //     assert((*(uint32_t *)addr2 == cd));
-    //     pmm.free_page(addr2, 1, COMMON::NORMAL);
-    //     assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
-    // }
-    // addr3 = (uint8_t *)pmm.alloc_page(1024, COMMON::NORMAL);
-    // if (addr3 != nullptr) {
-    //     assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 1024);
-    //     *(int *)addr3 = cd;
-    //     assert((*(uint32_t *)addr3 == cd));
-    //     pmm.free_page(addr3, 1024, COMMON::NORMAL);
-    //     assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
-    // }
-    // addr4 = (uint8_t *)pmm.alloc_page(4096, COMMON::NORMAL);
-    // if (addr4 != nullptr) {
-    //     assert(pmm.free_pages_count(COMMON::NORMAL) == free_count - 4096);
-    //     *(int *)addr4 = cd;
-    //     assert((*(uint32_t *)addr4 == cd));
-    //     pmm.free_page(addr4, 4096, COMMON::NORMAL);
-    //     assert(pmm.free_pages_count(COMMON::NORMAL) == free_count);
-    // }
-    // printf("pmm test done.\n");
+    printf("pmm test done.\n");
     return 0;
 }
