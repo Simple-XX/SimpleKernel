4a0da8c257392e0608cf8acaf446efeae8b9e3a4
Zone.N <Zone.Niuzh@hotmail.com>
2021-07-11 01:06:32 +0800

    pref(VMM): change to static
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/kernel/test.cpp
+++ src/kernel/test.cpp
@@ -44,40 +44,41 @@ int32_t test_pmm(void) {
 int32_t test_vmm(void) {
     ptrdiff_t addr = (ptrdiff_t) nullptr;
     // 首先确认内核空间被映射了
-    assert(vmm.get_pgd() != nullptr);
+    assert(VMM::get_pgd() != nullptr);
     // 0x00 留空
-    assert(vmm.get_mmap(vmm.get_pgd(), 0x00, nullptr) == 0);
-    assert(vmm.get_mmap(vmm.get_pgd(), (void *)0x03, nullptr) == 0);
-    assert(vmm.get_mmap(vmm.get_pgd(), (void *)0xCD, nullptr) == 0);
-    assert(vmm.get_mmap(vmm.get_pgd(), (void *)0xFFF, &addr) == 0);
+    assert(VMM::get_mmap(VMM::get_pgd(), 0x00, nullptr) == 0);
+    assert(VMM::get_mmap(VMM::get_pgd(), (void *)0x03, nullptr) == 0);
+    assert(VMM::get_mmap(VMM::get_pgd(), (void *)0xCD, nullptr) == 0);
+    assert(VMM::get_mmap(VMM::get_pgd(), (void *)0xFFF, &addr) == 0);
     assert(addr == (ptrdiff_t) nullptr);
-    assert(vmm.get_mmap(vmm.get_pgd(),
-                        (void *)((ptrdiff_t)COMMON::KERNEL_START_ADDR + 0x1000),
-                        &addr) == 1);
+    assert(
+        VMM::get_mmap(VMM::get_pgd(),
+                      (void *)((ptrdiff_t)COMMON::KERNEL_START_ADDR + 0x1000),
+                      &addr) == 1);
     assert(addr == (ptrdiff_t)COMMON::KERNEL_START_ADDR + 0x1000);
     addr = (ptrdiff_t) nullptr;
-    assert(vmm.get_mmap(vmm.get_pgd(),
-                        (void *)((ptrdiff_t)COMMON::KERNEL_START_ADDR +
-                                 VMM_KERNEL_SIZE - 1),
-                        &addr) == 1);
+    assert(VMM::get_mmap(VMM::get_pgd(),
+                         (void *)((ptrdiff_t)COMMON::KERNEL_START_ADDR +
+                                  VMM_KERNEL_SIZE - 1),
+                         &addr) == 1);
     assert(addr == (ptrdiff_t)(((ptrdiff_t)COMMON::KERNEL_START_ADDR +
                                 VMM_KERNEL_SIZE - 1) &
                                COMMON::PAGE_MASK));
     addr = (ptrdiff_t) nullptr;
     assert(
-        vmm.get_mmap(vmm.get_pgd(),
-                     (void *)((ptrdiff_t)COMMON::ALIGN(
-                                  COMMON::KERNEL_START_ADDR, 4 * COMMON::KB) +
-                              VMM_KERNEL_SIZE),
-                     &addr) == 0);
+        VMM::get_mmap(VMM::get_pgd(),
+                      (void *)((ptrdiff_t)COMMON::ALIGN(
+                                   COMMON::KERNEL_START_ADDR, 4 * COMMON::KB) +
+                               VMM_KERNEL_SIZE),
+                      &addr) == 0);
     assert(addr == (ptrdiff_t) nullptr);
     addr = (ptrdiff_t) nullptr;
     assert(
-        vmm.get_mmap(vmm.get_pgd(),
-                     (void *)((ptrdiff_t)COMMON::ALIGN(
-                                  COMMON::KERNEL_START_ADDR, 4 * COMMON::KB) +
-                              VMM_KERNEL_SIZE + 0x1024),
-                     nullptr) == 0);
+        VMM::get_mmap(VMM::get_pgd(),
+                      (void *)((ptrdiff_t)COMMON::ALIGN(
+                                   COMMON::KERNEL_START_ADDR, 4 * COMMON::KB) +
+                               VMM_KERNEL_SIZE + 0x1024),
+                      nullptr) == 0);
     // 测试映射与取消映射
     addr = (ptrdiff_t) nullptr;
     // 准备映射的虚拟地址 3GB 处
@@ -85,17 +86,17 @@ int32_t test_vmm(void) {
     // 准备映射的物理地址 0.75GB 处
     ptrdiff_t pa = 0x30000000;
     // 确定一块未映射的内存
-    assert(vmm.get_mmap(vmm.get_pgd(), (void *)va, nullptr) == 0);
+    assert(VMM::get_mmap(VMM::get_pgd(), (void *)va, nullptr) == 0);
     // 映射
-    vmm.mmap(vmm.get_pgd(), (void *)va, (void *)pa,
-             VMM_PAGE_READABLE | VMM_PAGE_WRITABLE);
-    assert(vmm.get_mmap(vmm.get_pgd(), (void *)va, &addr) == 1);
+    VMM::mmap(VMM::get_pgd(), (void *)va, (void *)pa,
+              VMM_PAGE_READABLE | VMM_PAGE_WRITABLE);
+    assert(VMM::get_mmap(VMM::get_pgd(), (void *)va, &addr) == 1);
     assert(addr == pa);
     // 写测试
     *(ptrdiff_t *)va = 0xCD;
     //取消映射
-    vmm.unmmap(vmm.get_pgd(), (void *)va);
-    assert(vmm.get_mmap(vmm.get_pgd(), (void *)va, &addr) == 0);
+    VMM::unmmap(VMM::get_pgd(), (void *)va);
+    assert(VMM::get_mmap(VMM::get_pgd(), (void *)va, &addr) == 0);
     assert(addr == (ptrdiff_t) nullptr);
     printf("vmm test done.\n");
     return 0;
