26dc4eb1bb8b539f8262afc72ed914669bdc9ccd
Zone.N <Zone.Niuzh@hotmail.com>
2021-07-14 16:50:13 +0800

    bug(slab): mistake at move
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/include/heap/slab.h
+++ src/include/heap/slab.h
@@ -11,51 +11,58 @@
 #include "stddef.h"
 #include "allocator.h"
 
+// 针对常用的 size 分别建立含有三个链表对象(full, part, free)的数组
+// 需要分配时在对应 size 数组中寻找一个合适的 chunk
+// 查找方法 首先查 part，再查 free，再没有就申请新的空间，再在 free 中查
+
 // SLAB 分配器
 class SLAB : ALLOCATOR {
 private:
     // 两级结构
     // 第二级保存相同大小的内存块的使用情况
-    struct slab_t {
-        // slab_t 结构的物理地址
-        // TODO: 可以移动到最后，这样直接 `addr=&.addr` 就好了
+    struct chunk_t {
+        // 头节点标识
+        static constexpr const uint64_t HEAD = 0xCDCD;
+        // chunk_t 结构的物理地址
         void *addr;
         // 长度，不包括自身大小 单位为 byte
         size_t len;
         // 链表指针
-        slab_t *prev;
-        slab_t *next;
-        slab_t(void);
-        ~slab_t(void);
+        chunk_t *prev;
+        chunk_t *next;
+        chunk_t(void);
+        ~chunk_t(void);
         // 插入新节点
-        void push_back(slab_t *_new_node);
+        void push_back(chunk_t *_new_node);
         // 链表长度
         size_t size(void);
-        bool   operator!=(const slab_t &_node);
+        bool   operator==(const chunk_t &_node);
+        bool   operator!=(const chunk_t &_node);
     };
 
     // 第一级保存不同长度的内存块
     class slab_cache_t {
     private:
         // 在 _which 链表中查找长度符合的
-        slab_t *find(const slab_t &_which, size_t _len);
+        chunk_t *find(chunk_t &_which, size_t _len);
 
     protected:
     public:
         // 长度，单位为 byte
         size_t len;
+        // 这三个作为头节点使用，不会实际使用
         // 全部使用的链表
-        slab_t full;
+        chunk_t full;
         // 部分使用的链表
-        slab_t part;
+        chunk_t part;
         // 未使用的链表
-        slab_t free;
+        chunk_t free;
         // 查找长度符合的
-        slab_t *find(size_t _len);
-        // 移动到另一个链表
-        void move(slab_t *_list);
-        // 申请物理内存，加入 free 链表
-        void alloc_pmm(size_t _len);
+        chunk_t *find(size_t _len);
+        // 对节点进行移动
+        void move(chunk_t *_node);
+        // 申请物理内存，加入 free 链表，成功返回 true
+        bool alloc_pmm(size_t _len);
         // 释放物理内存 单位为页
         void free_pmm(size_t _len);
     };
@@ -63,8 +70,7 @@ private:
     // 管理不同长度的内存
     // 根据下标计算
     enum LEN {
-        LEN32 = 0,
-        LEN64,
+        LEN64 = 0,
         LEN128,
         LEN256,
         LEN512,
@@ -74,13 +80,15 @@ private:
         LEN8192,
         LEN16384,
         LEN32768,
-        LEN65536 = 11,
+        LEN65536 = 10,
     };
-    // 32 << _idx
-    // 最小为 32 bytes
-    // 支持 32～(32<<(CACHAE_LEN-1)) bytes
-    // slab_cache[0] 即为内存为 32 字节的 slab_t 结构链表
-    static constexpr const size_t CACHAE_LEN = 12;
+    // 64 << _idx
+    // 最小为 64 bytes
+    static constexpr const size_t MIN = 64;
+    // 支持 64～(64<<(CACHAE_LEN-1)) bytes
+    static constexpr const size_t SHIFT = 6;
+    // slab_cache[0] 即为内存为 64 字节的 chunk_t 结构链表
+    static constexpr const size_t CACHAE_LEN = 11;
     slab_cache_t                  slab_cache[CACHAE_LEN];
     // 根据 _len 获取对应的 slab_cache 下标
     size_t get_idx(size_t _len);
