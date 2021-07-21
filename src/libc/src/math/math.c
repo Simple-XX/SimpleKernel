b0627225dd91351d001b90302d6fb0d2eee142df
Zone.N <Zone.Niuzh@hotmail.com>
2021-07-21 15:56:48 +0800

    pref(math): update udivmoddi4()
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/libc/src/math/math.c
+++ src/libc/src/math/math.c
@@ -33,35 +33,72 @@ long long divmoddi4(long long num, long long den, long long *rem_p) {
     return v;
 }
 
-unsigned long long udivmoddi4(unsigned long long num, unsigned long long den,
-                              unsigned long long *rem_p) {
-    unsigned long long quot = 0, qbit = 1;
+unsigned long long udivmoddi4(unsigned long long n, unsigned long long d,
+                              unsigned long long *rp) {
+    unsigned long long q = 0, r = n, y = d;
+    unsigned int       lz1, lz2, i, k;
 
-    if (den == 0) {
-        return 1 / ((unsigned)den); /* Intentional divide by zero, without
-                                       triggering a compiler warning which
-                                          would abort the build */
-    }
+    /*
+     * Implements align divisor shift dividend method. This algorithm
+     * aligns the divisor under the dividend and then perform number of
+     * test-subtract iterations which shift the dividend left. Number of
+     * iterations is k + 1 where k is the number of bit positions the
+     * divisor must be shifted left  to align it under the dividend.
+     * quotient bits can be saved in the rightmost positions of the
+     * dividend as it shifts left on each test-subtract iteration.
+     */
 
-    /* Left-justify denominator and count shift */
-    while ((long long)den >= 0) {
-        den <<= 1;
-        qbit <<= 1;
-    }
+    if (y <= r) {
+        lz1 = __builtin_clzll(d);
+        lz2 = __builtin_clzll(n);
 
-    while (qbit) {
-        if (den <= num) {
-            num -= den;
-            quot += qbit;
+        k = lz1 - lz2;
+        y = (y << k);
+
+        /*
+         * Dividend can exceed 2 ^ (width - 1) - 1 but still be less
+         * than the aligned divisor. Normal iteration can drops the
+         * high order bit of the dividend. Therefore, first
+         * test-subtract iteration is a special case, saving its
+         * quotient bit in a separate location and not shifting
+         * the dividend.
+         */
+
+        if (r >= y) {
+            r = r - y;
+            q = (1ULL << k);
         }
-        den >>= 1;
-        qbit >>= 1;
+
+        if (k > 0) {
+            { y = y >> 1; }
+
+            /*
+             * k additional iterations where k regular test
+             * subtract shift dividend iterations are done.
+             */
+            i = k;
+            do {
+                if (r >= y)
+                    r = ((r - y) << 1) + 1;
+                else
+                    r = (r << 1);
+                i = i - 1;
+            } while (i != 0);
+
+            /*
+             * First quotient bit is combined with the quotient
+             * bits resulting from the k regular iterations.
+             */
+            q = q + r;
+            r = r >> k;
+            q = q - (r << k);
+        }
     }
 
-    if (rem_p)
-        *rem_p = num;
-
-    return quot;
+    if (rp) {
+        *rp = r;
+    }
+    return q;
 }
 
 unsigned long long udivdi3(unsigned long long num, unsigned long long den) {
@@ -76,5 +113,4 @@ unsigned long long umoddi3(unsigned long long num, unsigned long long den) {
 
 #ifdef __cplusplus
 }
-
 #endif
