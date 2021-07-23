44cc9ab421c001303c09f817f294d7edc3b19a03
Zone.N <Zone.Niuzh@hotmail.com>
2021-07-24 02:38:36 +0800

    refactor(intr): change to static class
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/arch/i386/intr/include/intr.h
+++ src/arch/i386/intr/include/intr.h
@@ -10,10 +10,165 @@
 #include "stdint.h"
 
 // TODO: 升级为 APIC
-namespace INTR {
+class INTR {
+public:
+    struct error_code_t {
+        uint32_t ext : 1;
+        uint32_t idt : 1;
+        uint32_t ti : 1;
+        uint32_t sec_idx : 28;
+    };
+
+    struct page_fault_error_code_t {
+        uint32_t p : 1;
+        uint32_t wr : 1;
+        uint32_t us : 1;
+        uint32_t rsvd : 1;
+        uint32_t id : 1;
+        uint32_t pk : 1;
+        uint32_t reserved1 : 9;
+        uint32_t sgx : 1;
+        uint32_t reserved2 : 16;
+    };
+
+    // 64-ia-32-architectures-software-developer-vol-3a-manual#6.12.1
+    struct intr_context_t {
+        // segment registers
+        // 16 bits
+        uint32_t ss;
+        // 16 bits
+        uint32_t gs;
+        // 16 bits
+        uint32_t fs;
+        // 16 bits
+        uint32_t es;
+        // 16 bits
+        uint32_t ds;
+
+        // pusha，popa 跟这个相反
+        // Temporary = ESP;
+        // push(EAX);
+        // push(ECX);
+        // push(EDX);
+        // push(EBX);
+        // push(Temporary);
+        // push(EBP);
+        // push(ESI);
+        // push(EDI);
+        // 这几个寄存器由 pusha 压入
+        uint32_t edi;
+        uint32_t esi;
+        uint32_t ebp;
+        uint32_t esp;
+        uint32_t ebx;
+        uint32_t edx;
+        uint32_t ecx;
+        uint32_t eax;
+
+        // 错误代码，如果有，由 cpu 压入
+        uint32_t err_code;
+        // 以下指令由cpu压入，参见x86/x64 532页
+        // 指向产生异常的指令
+        uint32_t eip;
+        // 16 bits
+        uint32_t cs;
+        uint32_t eflags;
+        // 如果发生了特权级切换，CPU 会压入以下两个参数
+        uint32_t old_esp;
+        uint32_t old_ss;
+    };
+
+    // 定义中断处理函数指针
+    typedef void (*interrupt_handler_t)(intr_context_t *);
+
+private:
     // 中断表最大值
     static constexpr const uint32_t INTERRUPT_MAX = 256;
+    // 8259A 相关定义
+    // Master (IRQs 0-7)
+    static constexpr const uint32_t IO_PIC1 = 0x20;
+    // Slave  (IRQs 8-15)
+    static constexpr const uint32_t IO_PIC2  = 0xA0;
+    static constexpr const uint32_t IO_PIC1C = IO_PIC1 + 1;
+    static constexpr const uint32_t IO_PIC2C = IO_PIC2 + 1;
+    // End-of-interrupt command code
+    static constexpr const uint32_t PIC_EOI = 0x20;
 
+    // 中断名数组
+    static constexpr const char *const intrnames[] = {
+        "Divide Error",
+        "Debug Exception",
+        "NMI Interrupt",
+        "Breakpoint",
+        "Overflow",
+        "BOUND Range Exceeded",
+        "Invalid Opcode (Undefined Opcode)",
+        "Device Not Available (No Math Coprocessor)",
+        "Double Fault",
+        "Coprocessor Segment Overrun (reserved)",
+        "Invalid TSS",
+        "Segment Not Present",
+        "Stack-Segment Fault",
+        "General Protection",
+        "Page Fault",
+        "(Intel reserved. Do not use.)",
+        "x87 FPU Floating-Point Error (Math Fault)",
+        "Alignment Check",
+        "Machine Check",
+        "SIMD Floating-Point Exception",
+        "Virtualization Exception",
+    };
+
+    // 中断描述符
+    // 64-ia-32-architectures-software-developer-vol-3a-manual#6.11
+    struct idt_entry32_t {
+        // 低位地址
+        uint64_t offset0 : 16;
+        // 选择子
+        uint64_t selector : 16;
+        // 保留
+        uint64_t reserved : 4;
+        // 填充 0
+        uint64_t zero : 4;
+        // 类型
+        uint64_t type : 5;
+        // 权限
+        uint64_t dpl : 2;
+        // 存在位
+        uint64_t p : 1;
+        // 中段地址
+        uint64_t offset1 : 16;
+    } __attribute__((packed));
+
+    // IDTR
+    // 64-ia-32-architectures-software-developer-vol-3a-manual#6.10
+    struct idt_ptr_t {
+        // 限长
+        uint16_t limit;
+        // 基址
+        uint32_t base;
+    } __attribute__((packed));
+
+    // 中断处理函数指针数组
+    static interrupt_handler_t interrupt_handlers[INTERRUPT_MAX]
+        __attribute__((aligned(4)));
+    // 中断描述符表
+    static idt_entry32_t idt_entry32[INTERRUPT_MAX]
+        __attribute__((aligned(16)));
+    // IDTR
+    static idt_ptr_t idt_ptr;
+
+    // 设置中断描述符
+    static void set_idt(uint8_t _num, uint32_t _base, uint16_t _selector,
+                        uint8_t _type, uint8_t _dpl, uint8_t _p);
+    // 设置 8259A 芯片
+    static void init_interrupt_chip(void);
+    // 重设 8259A 芯片
+    static void clear_interrupt_chip(uint8_t _no);
+    // 关闭 8259A 芯片的所有中断，为启动 APIC 作准备
+    static void disable_interrupt_chip(void);
+
+public:
     // External(hardware generated) interrupts.
     // 64-ia-32-architectures-software-developer-vol-3a-manual#6.3.1
     static constexpr const uint32_t INT_DIVIDE_ERROR     = 0;
@@ -72,156 +227,20 @@ namespace INTR {
     static constexpr const uint32_t IRQ14 = 46;
     // SATA 从硬盘
     static constexpr const uint32_t IRQ15 = 47;
-
     // 系统调用
     static constexpr const uint32_t IRQ128 = 128;
 
-    // 8259A 相关定义
-    // Master (IRQs 0-7)
-    static constexpr const uint32_t IO_PIC1 = 0x20;
-    // Slave  (IRQs 8-15)
-    static constexpr const uint32_t IO_PIC2  = 0xA0;
-    static constexpr const uint32_t IO_PIC1C = IO_PIC1 + 1;
-    static constexpr const uint32_t IO_PIC2C = IO_PIC2 + 1;
-    // End-of-interrupt command code
-    static constexpr const uint32_t PIC_EOI = 0x20;
-
-    class error_code_t {
-    public:
-        uint32_t ext : 1;
-        uint32_t idt : 1;
-        uint32_t ti : 1;
-        uint32_t sec_idx : 28;
-    };
-
-    class page_fault_error_code_t {
-    public:
-        uint32_t p : 1;
-        uint32_t wr : 1;
-        uint32_t us : 1;
-        uint32_t rsvd : 1;
-        uint32_t id : 1;
-        uint32_t pk : 1;
-        uint32_t reserved1 : 9;
-        uint32_t sgx : 1;
-        uint32_t reserved2 : 16;
-    };
-
-    class intr_context_t {
-    public:
-        // segment registers
-        // 16 bits
-        uint32_t gs;
-        // 16 bits
-        uint32_t fs;
-        // 16 bits
-        uint32_t es;
-        // 16 bits
-        uint32_t ds;
-
-        // registers save by pusha
-        uint32_t edi;
-        uint32_t esi;
-        uint32_t ebp;
-        uint32_t old_esp;
-        uint32_t ebx;
-        uint32_t edx;
-        uint32_t ecx;
-        uint32_t eax;
-
-        uint32_t int_no;
-        // save by `int` instruction
-        uint32_t err_code;
-        // 以下指令由cpu压入，参见x86/x64 532页
-        // 指向产生异常的指令
-        uint32_t eip;
-        // 16 bits
-        uint32_t cs;
-        uint32_t eflags;
-        // 如果发生了特权级切换，CPU 会压入以下两个参数
-        uint32_t user_esp;
-        // 16 bits
-        uint32_t user_ss;
-    };
-
-    // 中断描述符
-    // 64-ia-32-architectures-software-developer-vol-3a-manual#6.11
-    struct idt_entry32_t {
-        // 低位地址
-        uint64_t offset0 : 16;
-        // 选择子
-        uint64_t selector : 16;
-        // 保留
-        uint64_t reserved : 4;
-        // 填充 0
-        uint64_t zero : 4;
-        // 类型
-        uint64_t type : 5;
-        // 权限
-        uint64_t dpl : 2;
-        // 存在位
-        uint64_t p : 1;
-        // 中段地址
-        uint64_t offset1 : 16;
-    } __attribute__((packed));
-
-    // IDTR
-    // 64-ia-32-architectures-software-developer-vol-3a-manual#6.10
-    struct idt_ptr_t {
-        // 限长
-        uint16_t limit;
-        // 基址
-        uint32_t base;
-    } __attribute__((packed));
-
-    // 定义中断处理函数指针
-    typedef void (*interrupt_handler_t)(intr_context_t *);
-
-    // 中断处理函数指针类型
-    typedef void (*isr_irq_func_t)();
-
-    // 设置 8259A 芯片
-    void init_interrupt_chip(void);
-    // 重设 8259A 芯片
-    void clear_interrupt_chip(uint8_t _no);
-
-    // 设置中断描述符
-    void set_idt(uint8_t _num, uintptr_t _base, uint16_t _selector,
-                 uint8_t _ist, uint8_t _type, uint8_t _dpl, uint8_t _p);
-    // 中断名数组
-    static constexpr const char *const intrnames[] = {
-        "Divide Error",
-        "Debug Exception",
-        "NMI Interrupt",
-        "Breakpoint",
-        "Overflow",
-        "BOUND Range Exceeded",
-        "Invalid Opcode (Undefined Opcode)",
-        "Device Not Available (No Math Coprocessor)",
-        "Double Fault",
-        "Coprocessor Segment Overrun (reserved)",
-        "Invalid TSS",
-        "Segment Not Present",
-        "Stack-Segment Fault",
-        "General Protection",
-        "Page Fault",
-        "(Intel reserved. Do not use.)",
-        "x87 FPU Floating-Point Error (Math Fault)",
-        "Alignment Check",
-        "Machine Check",
-        "SIMD Floating-Point Exception",
-        "Virtualization Exception",
-    };
-    int32_t init(void);
-    // 注册一个中断处理函数
-    void register_interrupt_handler(uint8_t n, interrupt_handler_t h);
-    void enable_irq(uint8_t _no);
-    void disable_irq(uint8_t _no);
-    // 返回中断名
-    const char *get_intr_name(uint8_t _no);
+    static int32_t init(void);
     // 执行中断
-    int32_t call_irq(uint8_t _no, intr_context_t *_regs);
-    int32_t call_isr(uint8_t _no, intr_context_t *_regs);
+    static int32_t call_irq(uint8_t _no, intr_context_t *_intr_context);
+    static int32_t call_isr(uint8_t _no, intr_context_t *_intr_context);
+    // 注册一个中断处理函数
+    static void register_interrupt_handler(uint8_t             _no,
+                                           interrupt_handler_t _handler);
+    static void enable_irq(uint8_t _no);
+    static void disable_irq(uint8_t _no);
+    // 返回中断名
+    static const char *get_intr_name(uint8_t _no);
 };
 
 #endif /* _INTR_H_ */
