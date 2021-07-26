e65add9aaab390df1d1770ca86e55e968b3cf10a
Zone.N <Zone.Niuzh@hotmail.com>
2021-07-27 00:52:03 +0800

    pref(vmm): try to merge diff platform
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/arch/riscv64/vmm/vmm.cpp
+++ src/arch/riscv64/vmm/vmm.cpp
@@ -91,7 +91,7 @@ pt_t        VMM::curr_dir;
 
 VMM::VMM(void) {
     // 读取当前页目录
-    curr_dir = (pt_t)CPU::READ_SATP();
+    curr_dir = (pt_t)CPU::GET_PGD();
     return;
 }
 
@@ -126,9 +126,9 @@ void VMM::set_pgd(const pt_t _pgd) {
     // 更新当前页表
     curr_dir = _pgd;
     // 设置页目录
-    CPU::WRITE_SATP(curr_dir);
+    CPU::SET_PGD((uintptr_t)curr_dir);
     // 刷新缓存
-    CPU::SFENCE_VMA();
+    CPU::VMM_FLUSH(0);
     return;
 }
 
@@ -147,7 +147,7 @@ void VMM::mmap(const pt_t _pgd, const void *_va, const void *_pa,
         // pte 解引用后的值是页表项
         *pte = PA2PTE(_pa) | _flag | VMM_PAGE_VALID;
         // 刷新缓存
-        CPU::SFENCE_VMA();
+        CPU::VMM_FLUSH((uintptr_t)_va);
     }
     return;
 }
@@ -167,7 +167,7 @@ void VMM::unmmap(const pt_t _pgd, const void *_va) {
     // 置零
     *pte = 0x00;
     // 刷新缓存
-    CPU::SFENCE_VMA();
+    CPU::VMM_FLUSH((uintptr_t)_va);
     // TODO: 如果一页表都被 unmap，释放占用的物理内存
     return;
 }
