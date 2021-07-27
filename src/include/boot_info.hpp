1ce38443a16b79467df31a54c657bec181750fb4
Zone.N <Zone.Niuzh@hotmail.com>
2021-07-28 04:42:52 +0800

    comment(boot_info): update
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/include/boot_info.hpp
+++ src/include/boot_info.hpp
@@ -11,6 +11,9 @@
 #include "dtb.h"
 #include "multiboot2.h"
 
+// 由引导传递的机器信息处理
+// 如 grub2 传递的 multiboot2 结构
+// opensbi 传递的 dtb 结构
 namespace BOOT_INFO {
     // 声明，定义在具体的实现中
     // 地址
@@ -18,16 +21,21 @@ namespace BOOT_INFO {
     // 长度
     extern size_t boot_info_size;
 
+// 根据不同的信息类型分别处理
 #if defined(__i386__) || defined(__x86_64__)
+    // 别名，统一接口
     using iter_data_t = MULTIBOOT2::multiboot2_iter_data_t;
     using iter_fun_t  = MULTIBOOT2::multiboot2_iter_fun_t;
-    void iter(iter_fun_t _fun, void *_data) {
+    // 迭代器
+    inline void iter(iter_fun_t _fun, void *_data) {
         MULTIBOOT2::multiboot2_iter(_fun, _data);
         return;
     }
 #elif defined(__riscv)
+    // 别名，统一接口
     using iter_data_t = DTB::dtb_iter_data_t;
     using iter_fun_t  = DTB::dtb_iter_fun_t;
+    // 迭代器
     inline void iter(iter_fun_t _fun, void *_data) {
         DTB::dtb_iter(_fun, _data);
         return;
