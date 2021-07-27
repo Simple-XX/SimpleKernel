3d58fafeb9a1c4573b5a737c113ac91063e52e3a
Zone.N <Zone.Niuzh@hotmail.com>
2021-07-28 04:31:51 +0800

    feat(boot_info): parse multiboot2 and dtb
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/include/common.h
+++ src/include/common.h
@@ -35,22 +35,26 @@ namespace COMMON {
     static constexpr const size_t BYTE = 0x1;
     static constexpr const size_t KB   = 0x400;
     static constexpr const size_t MB   = 0x100000;
+    static constexpr const size_t GB   = 0x40000000;
     // 页大小 4KB
     static constexpr const size_t PAGE_SIZE = 4 * KB;
 
     // 对齐 向上取整
     // 针对指针
     template <class T>
-    inline T ALIGN(const T _addr, const size_t _align) {
+    inline T ALIGN(const T _addr, size_t _align) {
         uint8_t *tmp = (uint8_t *)_addr;
-        return (T)((ptrdiff_t)(tmp + _align - 1) & (~(_align - 1)));
+        return (T)((uintptr_t)(tmp + _align - 1) & (~(_align - 1)));
     }
     // 针对整数
     template <>
-    inline uint64_t ALIGN(const uint64_t _x, const size_t _align) {
+    inline uint32_t ALIGN(uint32_t _x, size_t _align) {
         return ((_x + _align - 1) & (~(_align - 1)));
     }
-
+    template <>
+    inline uint64_t ALIGN(uint64_t _x, size_t _align) {
+        return ((_x + _align - 1) & (~(_align - 1)));
+    }
 };
 
 #endif /* _COMMON_H_ */
