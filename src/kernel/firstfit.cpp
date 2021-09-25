f45d170fb24a81ae46bb213eaea547e34420c6de
digmouse233 <digmouse233@gmail.com>
2021-09-25 13:38:28 +0800

    comment
    Signed-off-by: digmouse233 <digmouse233@gmail.com>

--- src/kernel/firstfit.cpp
+++ src/kernel/firstfit.cpp
@@ -1,8 +1,18 @@
 
-// This file is a part of Simple-XX/SimpleKernel
-// (https://github.com/Simple-XX/SimpleKernel).
-//
-// first_fit.cpp for Simple-XX/SimpleKernel.
+/**
+ * @file firstfit.cpp
+ * @brief firstfit 内存分配器实现
+ * @author Zone.N (Zone.Niuzh@hotmail.com)
+ * @version 1.0
+ * @date 2021-09-18
+ * @copyright MIT LICENSE
+ * https://github.com/Simple-XX/SimpleKernel
+ * @par change log:
+ * <table>
+ * <tr><th>Date<th>Author<th>Description
+ * <tr><td>2021-09-18<td>digmouse233<td>迁移到 doxygen
+ * </table>
+ */
 
 #include "stdint.h"
 #include "string.h"
