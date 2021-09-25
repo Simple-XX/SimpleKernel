e5f569bc4ea58eba1e8c638c0a3b7459cbdd9825
digmouse233 <digmouse233@gmail.com>
2021-09-25 13:37:56 +0800

    comment
    Signed-off-by: digmouse233 <digmouse233@gmail.com>

--- src/libcxx/src/cxxabi.cpp
+++ src/libcxx/src/cxxabi.cpp
@@ -1,8 +1,19 @@
 
-// This file is a part of Simple-XX/SimpleKernel
-// (https://github.com/Simple-XX/SimpleKernel).
-// Based on https://wiki.osdev.org/C%2B%2B
-// cxxabi.cpp for Simple-XX/SimpleKernel.
+/**
+ * @file cxxabi.cpp
+ * @brief C++ abi 支持
+ * @author Zone.N (Zone.Niuzh@hotmail.com)
+ * @version 1.0
+ * @date 2021-09-18
+ * @copyright MIT LICENSE
+ * https://github.com/Simple-XX/SimpleKernel
+ * Based on https://wiki.osdev.org/C%2B%2B
+ * @par change log:
+ * <table>
+ * <tr><th>Date<th>Author<th>Description
+ * <tr><td>2021-09-18<td>digmouse233<td>迁移到 doxygen
+ * </table>
+ */
 
 #include "cxxabi.h"
 
