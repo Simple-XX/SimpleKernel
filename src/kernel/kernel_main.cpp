f2d5ecc04e90b092a98361008993733a3843bc8f
digmouse233 <digmouse233@gmail.com>
2021-09-25 13:31:55 +0800

    comment
    Signed-off-by: digmouse233 <digmouse233@gmail.com>

--- src/kernel/kernel_main.cpp
+++ src/kernel/kernel_main.cpp
@@ -1,12 +1,27 @@
 
-// This file is a part of Simple-XX/SimpleKernel
-// (https://github.com/Simple-XX/SimpleKernel).
-//
-// kernel_main.cpp for Simple-XX/SimpleKernel.
+/**
+ * @file kernel_main.cpp
+ * @brief 内核主要逻辑
+ * @author Zone.N (Zone.Niuzh@hotmail.com)
+ * @version 1.0
+ * @date 2021-09-18
+ * @copyright MIT LICENSE
+ * https://github.com/Simple-XX/SimpleKernel
+ * @par change log:
+ * <table>
+ * <tr><th>Date<th>Author<th>Description
+ * <tr><td>2021-09-18<td>Zone.N (Zone.Niuzh@hotmail.com)<td>迁移到 doxygen
+ * </table>
+ */
 
 #include "kernel.h"
 
+/**
+ * @brief 内核主要逻辑
+ * @note 这个函数不会返回
+ */
 void kernel_main(void) {
+    // 进入死循环
     while (1) {
         ;
     }
