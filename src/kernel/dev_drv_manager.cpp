70c85c51fe42ca0c91e1c23d67feaf3a10caab65
Zone.N <Zone.Niuzh@hotmail.com>
2021-11-16 22:50:05 +0800

    pref(device): change to dev
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/kernel/dev_drv_manager.cpp
+++ src/kernel/dev_drv_manager.cpp
@@ -10,7 +10,7 @@
 #include "dev_drv_manager.h"
 #include "platform_bus_dev.h"
 #include "platform_bus_drv.h"
-#include "virtio_mmio_bus_drv.h"
+#include "virtio_mmio_drv.h"
 
 void virtio_intr_handler(void) {
     warn("virtio irq handler.\n");
@@ -49,36 +49,41 @@ void DEV_DRV_MANAGER::show(void) const {
 // 然后初始化 dtb，找到所有硬件
 // 最后遍历硬件，寻找需要的驱动
 DEV_DRV_MANAGER::DEV_DRV_MANAGER(void) {
-
+    // TODO 设备管理应该更加抽象，这里只是个勉强能用的
     // 获取 virtio 设备信息
-    resource_t virtio_mmio_bus_resources[8];
+    resource_t virtio_mmio_dev_resources[8];
     // 获取信息
-    auto virtio_mmio_bus_resources_count =
-        BOOT_INFO::find_via_prefix("virtio_mmio@", virtio_mmio_bus_resources);
+    auto virtio_mmio_dev_resources_count =
+        BOOT_INFO::find_via_prefix("virtio_mmio@", virtio_mmio_dev_resources);
     // 转换为向量
-    auto virtio_mmio_bus_resources_vector = new mystl::vector<resource_t>(
-        virtio_mmio_bus_resources,
-        virtio_mmio_bus_resources + virtio_mmio_bus_resources_count);
+    auto virtio_mmio_dev_resources_vector = new mystl::vector<resource_t>(
+        virtio_mmio_dev_resources,
+        virtio_mmio_dev_resources + virtio_mmio_dev_resources_count);
+    // 添加 virtio 总线
+    resource_t tmp;
+    tmp.name = (char *)"virtio_tmp TODO";
+    auto bus = new platform_bus_dev_t(tmp);
+    add_bus(*bus);
     // 每个 resource 对应一个总线设备
-    for (auto i : *virtio_mmio_bus_resources_vector) {
+    for (auto i : *virtio_mmio_dev_resources_vector) {
         // 设置每个设备的名称与驱动名
-        platform_bus_dev_t *virtio_bus = new platform_bus_dev_t(i);
+        auto virtio_dev = new dev_t(i);
         // 设备名
-        virtio_bus->dev_name = i.name;
+        virtio_dev->dev_name = i.name;
         // 需要的驱动名
         // TODO: 这里需要 compatible 字段
-        virtio_bus->drv_name = "virtio,mmio";
-        // 所属总线名，因为这里本身就是总线，所以设为 NULL
-        virtio_bus->bus_name = "NULL";
+        virtio_dev->drv_name = "virtio,mmio";
+        // 所属总线名
+        virtio_dev->bus_name = "vortio,mmio";
         // 添加到总线向量
-        add_bus(*virtio_bus);
+        add_dev(*virtio_dev);
     }
 
-    // TODO: 添加 virtio,mmio 驱动
-    virtio_mmio_bus_drv_t *virtio_mmio_bus_drv = new virtio_mmio_bus_drv_t();
+    // TODO: 添加 virtio,mmio bus 驱动
+    virtio_mmio_drv_t *virtio_mmio_drv = new virtio_mmio_drv_t();
     // 到这里 virtio,mmio 总线初始化完成，下面为各个 virtio,mmio 设备进行初始化
     // 添加块设备驱动
-    add_drv(*(drv_t *)virtio_mmio_bus_drv);
+    add_drv(*(drv_t *)virtio_mmio_drv);
     // 初始化所有设备
     for (auto i : devs) {
         if (init(*i) != true) {
