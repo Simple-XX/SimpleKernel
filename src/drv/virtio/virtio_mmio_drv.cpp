c59f3b389a1ff1a2d1ea499b934eced0f3ee8a26
Zone.N <Zone.Niuzh@hotmail.com>
2021-11-16 22:50:05 +0800

    pref(device): change to dev
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/drv/virtio/virtio_mmio_drv.cpp
+++ src/drv/virtio/virtio_mmio_drv.cpp
@@ -2,17 +2,17 @@
 // This file is a part of Simple-XX/SimpleKernel
 // (https://github.com/Simple-XX/SimpleKernel).
 //
-// virtio_mmio_bus_drv.cpp for Simple-XX/SimpleKernel.
+// virtio_mmio_drv.cpp for Simple-XX/SimpleKernel.
 
 #include "stdint.h"
 #include "vector"
 #include "string"
 #include "virtio_queue.h"
-#include "virtio_mmio_bus_drv.h"
+#include "virtio_mmio_drv.h"
 #include "drv.h"
 
 // 设置 features
-bool virtio_mmio_bus_drv_t::set_features(
+bool virtio_mmio_drv_t::set_features(
     const mystl::vector<feature_t> &_features) {
     (void)_features;
     return true;
@@ -20,34 +20,34 @@ bool virtio_mmio_bus_drv_t::set_features(
 
 // 将队列设置传递到相应寄存器
 // _queue_sel: 第几个队列，从 0 开始
-void virtio_mmio_bus_drv_t::add_to_device(uint32_t _queue_sel) {
+void virtio_mmio_drv_t::add_to_device(uint32_t _queue_sel) {
     (void)_queue_sel;
     return;
 }
 
-virtio_mmio_bus_drv_t::virtio_mmio_bus_drv_t(void) {
+virtio_mmio_drv_t::virtio_mmio_drv_t(void) {
     name = "virtio,mmio";
     return;
 }
 
-virtio_mmio_bus_drv_t::virtio_mmio_bus_drv_t(const void *_addr) {
+virtio_mmio_drv_t::virtio_mmio_drv_t(const void *_addr) {
     (void)_addr;
     return;
 }
 
-virtio_mmio_bus_drv_t::~virtio_mmio_bus_drv_t(void) {
+virtio_mmio_drv_t::~virtio_mmio_drv_t(void) {
     return;
 }
 
 // 驱动操作
-bool virtio_mmio_bus_drv_t::init(void) {
+bool virtio_mmio_drv_t::init(void) {
     return true;
 }
 
-void virtio_mmio_bus_drv_t::read(void) {
+void virtio_mmio_drv_t::read(void) {
     return;
 }
 
-void virtio_mmio_bus_drv_t::write(void) {
+void virtio_mmio_drv_t::write(void) {
     return;
 }
