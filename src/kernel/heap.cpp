0235fb9061f2b9db7b61ec3814630871acb308c5
Zone.N <Zone.Niuzh@hotmail.com>
2021-11-23 20:27:05 +0800

    add code
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

---
+++ src/kernel/heap.cpp
@@ -0,0 +1,59 @@
+
+/**
+ * @file heap.cpp
+ * @brief 堆抽象头文件
+ * @author Zone.N (Zone.Niuzh@hotmail.com)
+ * @version 1.0
+ * @date 2021-09-18
+ * @copyright MIT LICENSE
+ * https://github.com/Simple-XX/SimpleKernel
+ * @par change log:
+ * <table>
+ * <tr><th>Date<th>Author<th>Description
+ * <tr><td>2021-09-18<td>digmouse233<td>迁移到 doxygen
+ * </table>
+ */
+
+#include "stdio.h"
+#include "common.h"
+#include "pmm.h"
+#include "heap.h"
+
+bool HEAP::init(void) {
+    static SLAB slab_allocator("SLAB Allocator", PMM::non_kernel_space_start,
+                               PMM::non_kernel_space_length *
+                                   COMMON::PAGE_SIZE);
+    allocator = (ALLOCATOR *)&slab_allocator;
+    info("heap init.\n");
+    return 0;
+}
+
+void *HEAP::malloc(size_t _byte) {
+    return (void *)allocator->alloc(_byte);
+}
+
+void HEAP::free(void *_addr) {
+    // 堆不需要 _len 参数
+    allocator->free((uintptr_t)_addr, 0);
+    return;
+}
+
+/**
+ * @brief malloc 定义
+ * @param  _size           要申请的 bytes
+ * @return void*           申请到的地址
+ */
+extern "C" void *malloc(size_t _size) {
+    return (void *)heap.malloc(_size);
+}
+
+/**
+ * @brief free 定义
+ * @param  _p              要释放的内存地址
+ */
+extern "C" void free(void *_p) {
+    heap.free(_p);
+    return;
+}
+
+HEAP heap;
--- src/libcxx/src/new.cpp
+++ src/libcxx/src/new.cpp
@@ -8,21 +8,24 @@
 #include "stdlib.h"
 
 // TODO
-void *operator new(size_t) {
-    return (void *)1;
+void *operator new(size_t _size) {
+    return malloc(_size);
 }
 
-void *operator new[](size_t) {
-    return (void *)1;
+void *operator new[](size_t _size) {
+    return malloc(_size);
 }
 
-void operator delete(void *) {
+void operator delete(void *_p) {
+    free(_p);
 }
 
-void operator delete(void *, size_t) {
+void operator delete(void *_p, size_t) {
+    free(_p);
 }
 
-void operator delete[](void *) {
+void operator delete[](void *_p) {
+    free(_p);
 }
 
 void operator delete[](void *, size_t) {
@@ -63,6 +66,7 @@ void operator delete[](void *, std::align_val_t) {
 void operator delete(void *, size_t, std::align_val_t) {
     return;
 }
+
 void operator delete[](void *, size_t, std::align_val_t) {
     return;
 }
