7f373bc72f8bf0d7f79bd0c08de5e59222f020e7
Zone.N <Zone.Niuzh@hotmail.com>
2021-11-23 22:10:20 +0800

    add code
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

---
+++ src/arch/ia32/apic/io_apic.cpp
@@ -0,0 +1,35 @@
+
+/**
+ * @file io_apic.cpp
+ * @brief IOAPIC 抽象
+ * @author Zone.N (Zone.Niuzh@hotmail.com)
+ * @version 1.0
+ * @date 2021-09-18
+ * @copyright MIT LICENSE
+ * https://github.com/Simple-XX/SimpleKernel
+ * @par change log:
+ * <table>
+ * <tr><th>Date<th>Author<th>Description
+ * <tr><td>2021-09-18<td>digmouse233<td>迁移到 doxygen
+ * </table>
+ */
+
+#include "intr.h"
+#include "cpu.hpp"
+#include "io.h"
+#include "apic.h"
+
+/// @todo
+
+IO_APIC::IO_APIC(void) {
+    return;
+}
+
+IO_APIC::~IO_APIC(void) {
+    return;
+}
+
+int32_t IO_APIC::init(void) {
+    info("io apic init.\n");
+    return 0;
+}
