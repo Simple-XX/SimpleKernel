94c6d2c6f54068b73dfa9cc731e1e18265a04287
Zone.N <Zone.Niuzh@hotmail.com>
2021-12-01 21:47:17 +0800

    pref(IO): change to singleton
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/kernel/test.cpp
+++ src/kernel/test.cpp
@@ -13,7 +13,7 @@
 
 int32_t test_pmm(void) {
     // 保存现有 pmm 空闲页数量
-    size_t free_pages = PMM::get_free_pages_count();
+    size_t free_pages = PMM::get_instance().get_free_pages_count();
     // 计算内核实际占用页数
     auto kernel_pages =
         (COMMON::ALIGN(COMMON::KERNEL_END_ADDR, COMMON::PAGE_SIZE) -
@@ -28,34 +28,34 @@ int32_t test_pmm(void) {
 #endif
     // 空闲页数应该等于物理内存大小-内核使用
     assert(free_pages ==
-           (PMM::get_pmm_length() / COMMON::PAGE_SIZE) - kernel_pages);
+           (PMM::get_instance().get_pmm_length() / COMMON::PAGE_SIZE) - kernel_pages);
     // 获取已使用页数
-    size_t used_pages = PMM::get_used_pages_count();
+    size_t used_pages = PMM::get_instance().get_used_pages_count();
     // 已使用页数应该等于内核使用页数
     assert(used_pages == kernel_pages);
     // 分配
-    auto addr1 = PMM::alloc_pages(2);
+    auto addr1 = PMM::get_instance().alloc_pages(2);
     // 已使用应该会更新
-    assert(PMM::get_used_pages_count() == 2 + kernel_pages);
+    assert(PMM::get_instance().get_used_pages_count() == 2 + kernel_pages);
     // 同上
-    auto addr2 = PMM::alloc_pages(3);
-    assert(PMM::get_used_pages_count() == 5 + kernel_pages);
+    auto addr2 = PMM::get_instance().alloc_pages(3);
+    assert(PMM::get_instance().get_used_pages_count() == 5 + kernel_pages);
     // 同上
-    auto addr3 = PMM::alloc_pages(100);
-    assert(PMM::get_used_pages_count() == 105 + kernel_pages);
+    auto addr3 = PMM::get_instance().alloc_pages(100);
+    assert(PMM::get_instance().get_used_pages_count() == 105 + kernel_pages);
     // 同上
-    auto addr4 = PMM::alloc_pages(100);
-    assert(PMM::get_used_pages_count() == 205 + kernel_pages);
+    auto addr4 = PMM::get_instance().alloc_pages(100);
+    assert(PMM::get_instance().get_used_pages_count() == 205 + kernel_pages);
     // 分配超过限度的内存，应该返回 nullptr
-    auto addr5 = PMM::alloc_pages(0xFFFFFFFF);
+    auto addr5 = PMM::get_instance().alloc_pages(0xFFFFFFFF);
     assert(addr5 == 0);
     // 全部释放
-    PMM::free_pages(addr1, 2);
-    PMM::free_pages(addr2, 3);
-    PMM::free_pages(addr3, 100);
-    PMM::free_pages(addr4, 100);
+    PMM::get_instance().free_pages(addr1, 2);
+    PMM::get_instance().free_pages(addr2, 3);
+    PMM::get_instance().free_pages(addr3, 100);
+    PMM::get_instance().free_pages(addr4, 100);
     // 现在内存使用情况应该与此函数开始时相同
-    assert(PMM::get_free_pages_count() == free_pages);
+    assert(PMM::get_instance().get_free_pages_count() == free_pages);
     info("pmm test done.\n");
     return 0;
 }
