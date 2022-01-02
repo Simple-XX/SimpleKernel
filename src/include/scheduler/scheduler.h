01c64b7eaa372556f58d921fc19328bd744db2c3
Zone.N <Zone.Niuzh@hotmail.com>
2022-01-03 01:08:13 +0800

    comment(sched): update
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/include/scheduler/scheduler.h
+++ src/include/scheduler/scheduler.h
@@ -27,35 +27,91 @@
 #include "spinlock.h"
 
 // 调度器抽象
+/**
+ * @brief 调度器
+ */
 class SCHEDULER {
 private:
+    /**
+     * @brief 获取下一个要运行的任务
+     * @return task_t*          下一个要运行的任务
+     */
     static task_t *get_next_task(void);
-    static void    switch_task(void);
+
+    /**
+     * @brief 切换任务
+     */
+    static void switch_task(void);
 
 protected:
 public:
-    static void sched(void);
-    // 当前任务
+    /// 当前任务
     static task_t *curr_task[COMMON::CORES_COUNT];
-    // 任务向量
+
+    /// 任务向量
     static mystl::queue<task_t *> *task_queue;
-    // 全局 pid
-    static pid_t   g_pid;
+
+    /// 全局 pid
+    static pid_t g_pid;
+
+    /// 内核任务
     static task_t *task_os[COMMON::CORES_COUNT];
-    // 分配 pid
+
+    /**
+     * @brief 调度
+     * @note 从内核调度线程切换到其它线程
+     */
+    static void sched(void);
+
+    /**
+     * @brief 分配 pid
+     * @return pid_t            分配出的 pid
+     */
     static pid_t alloc_pid(void);
-    // 回收 pid
+
+    /**
+     * @brief 回收 pid
+     * @param  _pid             要回收的 pid
+     */
     static void free_pid(pid_t _pid);
-    // 初始化
+
+    /**
+     * @brief 初始化
+     * @return true             成功
+     * @return false            失败
+     */
     static bool init(void);
     static bool init_other_core(void);
-    // 获取当前任务
+
+    /**
+     * @brief 获取当前 core 正在执行的任务
+     * @return task_t*
+     */
     static task_t *get_curr_task(void);
-    // 添加任务
+
+    /**
+     * @brief 添加一个任务
+     * @param  _task            要添加的任务
+     */
     static void add_task(task_t *_task);
+
     // 删除任务
+    /**
+     * @brief 删除任务
+     * @param  _task            要删除的任务
+     */
     static void rm_task(task_t *_task);
+
+    /**
+     * @brief 切换到内核调度线程
+     */
     static void switch_to_kernel(void);
+
+    /**
+     * @brief 线程退出
+     * @param  _exit_code       退出代码
+     */
+    static void exit(uint32_t _exit_code);
 };
 
 #endif /* _SCHEDULER_H_ */
