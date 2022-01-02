ca4748ac89368c2bca7d87e9e9e7ec0aa8396b93
Zone.N <Zone.Niuzh@hotmail.com>
2022-01-02 15:11:22 +0800

    feat(IO): add spinlock
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/kernel/io.cpp
+++ src/kernel/io.cpp
@@ -13,11 +13,12 @@
 #endif
 #include "io.h"
 #include "stdio.h"
-#include "spinlock.h"
 
 /// 输出缓冲区
 /// @todo 用常量替换 128
 static char buf[128];
+/// IO 自旋锁
+static spinlock_t spinlock("IO");
 
 IO &IO::get_instance(void) {
     /// 定义全局 IO 对象
@@ -28,91 +29,138 @@ IO &IO::get_instance(void) {
 // riscv 没有端口 IO
 #ifndef __riscv
 uint8_t IO::inb(const uint32_t port) {
-    return PORT::inb(port);
+    spinlock.lock();
+    uint8_t ret = PORT::inb(port);
+    spinlock.unlock();
+    return ret;
 }
 
 uint16_t IO::inw(const uint32_t port) {
-    return PORT::inw(port);
+    spinlock.lock();
+    uint16_t ret = PORT::inw(port);
+    spinlock.unlock();
+    return ret;
 }
 
 uint32_t IO::ind(const uint32_t port) {
-    return PORT::ind(port);
+    spinlock.lock();
+    uint32_t ret = PORT::ind(port);
+    spinlock.unlock();
+    return ret;
 }
 
 void IO::outb(const uint32_t port, const uint8_t data) {
+    spinlock.lock();
     PORT::outb(port, data);
+    spinlock.unlock();
     return;
 }
 
 void IO::outw(const uint32_t port, const uint16_t data) {
+    spinlock.lock();
     PORT::outw(port, data);
+    spinlock.unlock();
     return;
 }
 
 void IO::outd(const uint32_t port, const uint32_t data) {
+    spinlock.lock();
     PORT::outd(port, data);
+    spinlock.unlock();
     return;
 }
 #endif
 
 // MMIO 实现
 uint8_t IO::read8(void *_addr) {
-    return *(uint8_t *)_addr;
+    spinlock.lock();
+    uint8_t ret = *(uint8_t *)_addr;
+    spinlock.unlock();
+    return ret;
 }
 
 void IO::write8(void *_addr, uint8_t _val) {
+    spinlock.lock();
     *(uint8_t *)_addr = _val;
+    spinlock.unlock();
     return;
 }
 
 uint16_t IO::read16(void *_addr) {
-    return *(uint16_t *)_addr;
+    spinlock.lock();
+    uint16_t ret = *(uint16_t *)_addr;
+    spinlock.unlock();
+    return ret;
 }
 
 void IO::write16(void *_addr, uint16_t _val) {
+    spinlock.lock();
     *(uint16_t *)_addr = _val;
+    spinlock.unlock();
     return;
 }
 
 uint32_t IO::read32(void *_addr) {
-    return *(uint32_t *)_addr;
+    spinlock.lock();
+    uint32_t ret = *(uint32_t *)_addr;
+    spinlock.unlock();
+    return ret;
 }
 
 void IO::write32(void *_addr, uint32_t _val) {
+    spinlock.lock();
     *(uint32_t *)_addr = _val;
+    spinlock.unlock();
     return;
 }
 
 uint64_t IO::read64(void *_addr) {
-    return *(uint64_t *)_addr;
+    spinlock.lock();
+    uint64_t ret = *(uint64_t *)_addr;
+    spinlock.unlock();
+    return ret;
 }
 
 void IO::write64(void *_addr, uint64_t _val) {
+    spinlock.lock();
     *(uint64_t *)_addr = _val;
+    spinlock.unlock();
     return;
 }
 
 COLOR::color_t IO::get_color(void) {
+    spinlock.lock();
     // 通过对应的 io 方式获取颜色
-    return io.get_color();
+    COLOR::color_t ret = io.get_color();
+    spinlock.unlock();
+    return ret;
 }
 
 void IO::set_color(const COLOR::color_t color) {
+    spinlock.lock();
     // 通过对应的 io 方式设置颜色
     io.set_color(color);
+    spinlock.unlock();
     return;
 }
 
 void IO::put_char(char c) {
+    spinlock.lock();
     // 通过对应的 io 方式输出字符
     io.put_char(c);
+    spinlock.unlock();
     return;
 }
 
 int32_t IO::write_string(const char *s) {
+    spinlock.lock();
     io.write_string(s);
+    spinlock.unlock();
     return 0;
 }
+
+/// 格式化输出自旋锁
+static spinlock_t spinlock_printf("printf");
 
 /**
  * @brief printf 定义
@@ -120,6 +168,7 @@ int32_t IO::write_string(const char *s) {
  * @return int32_t        输出的长度
  */
 extern "C" int32_t printf(const char *_fmt, ...) {
+    spinlock_printf.lock();
     va_list va;
     va_start(va, _fmt);
     // 交给 src/libc/src/stdio/vsprintf.c 中的 _vsnprintf
@@ -130,6 +179,7 @@ extern "C" int32_t printf(const char *_fmt, ...) {
     IO::get_instance().write_string(buf);
     // 清空数据
     bzero(buf, 128);
+    spinlock_printf.unlock();
     return ret;
 }
 
@@ -137,6 +187,7 @@ extern "C" int32_t printf(const char *_fmt, ...) {
  * @brief 与 printf 类似，只是颜色不同
  */
 extern "C" int32_t info(const char *_fmt, ...) {
+    spinlock_printf.lock();
     COLOR::color_t curr_color = IO::get_instance().get_color();
     IO::get_instance().set_color(COLOR::CYAN);
     va_list va;
@@ -147,6 +198,7 @@ extern "C" int32_t info(const char *_fmt, ...) {
     IO::get_instance().write_string(buf);
     bzero(buf, 128);
     IO::get_instance().set_color(curr_color);
+    spinlock_printf.unlock();
     return i;
 }
 
@@ -154,6 +206,7 @@ extern "C" int32_t info(const char *_fmt, ...) {
  * @brief 与 printf 类似，只是颜色不同
  */
 extern "C" int32_t warn(const char *_fmt, ...) {
+    spinlock_printf.lock();
     COLOR::color_t curr_color = IO::get_instance().get_color();
     IO::get_instance().set_color(COLOR::YELLOW);
     va_list va;
@@ -164,6 +217,7 @@ extern "C" int32_t warn(const char *_fmt, ...) {
     IO::get_instance().write_string(buf);
     bzero(buf, 128);
     IO::get_instance().set_color(curr_color);
+    spinlock_printf.unlock();
     return i;
 }
 
@@ -171,6 +225,7 @@ extern "C" int32_t warn(const char *_fmt, ...) {
  * @brief 与 printf 类似，只是颜色不同
  */
 extern "C" int32_t err(const char *_fmt, ...) {
+    spinlock_printf.lock();
     COLOR::color_t curr_color = IO::get_instance().get_color();
     IO::get_instance().set_color(COLOR::LIGHT_RED);
     va_list va;
@@ -181,5 +236,6 @@ extern "C" int32_t err(const char *_fmt, ...) {
     IO::get_instance().write_string(buf);
     bzero(buf, 128);
     IO::get_instance().set_color(curr_color);
+    spinlock_printf.unlock();
     return i;
 }
--- src/kernel/spinlock.cpp
+++ src/kernel/spinlock.cpp
@@ -20,50 +20,6 @@
 #include "core.hpp"
 #include "cpu.hpp"
 
-// TODO
-
-spinlock_t::spinlock_t(void) {
-    name   = "";
-    locked = false;
-    hartid = -1;
-    return;
-}
-
-spinlock_t::spinlock_t(const char *_name) : name(_name) {
-    locked = false;
-    hartid = -1;
-    info("spinlock: %s init.\n", name);
-    return;
-}
-
-void spinlock_t::lock(void) {
-    push_off();
-    if (is_holding() == true) {
-        err("lock\n");
-    }
-
-    while (__atomic_test_and_set(&locked, 1) != 0) {
-        ;
-    }
-
-    __sync_synchronize();
-
-    hartid = COMMON::get_curr_core_id(CPU::READ_SP());
-    return;
-}
-
-// Release the lock.
-void spinlock_t::unlock(void) {
-    if (is_holding() == false) {
-        err("unlock\n");
-    }
-    hartid = -1;
-    __sync_synchronize();
-    __sync_lock_release(&locked);
-    pop_off();
-    return;
-}
-
 bool spinlock_t::is_holding(void) {
     bool r = (locked && (hartid == COMMON::get_curr_core_id(CPU::READ_SP())));
     return r;
@@ -83,7 +39,6 @@ void spinlock_t::push_off(void) {
 }
 
 void spinlock_t::pop_off(void) {
-    // struct cpu *c = mycpu();
     if (CPU::SSTATUS_INTR_status() == true) {
         err("pop_off - interruptible\n");
     }
@@ -98,5 +53,43 @@ void spinlock_t::pop_off(void) {
         CPU::ENABLE_INTR();
     }
 
+    return;
+}
+
+spinlock_t::spinlock_t(void) {
+    name   = "";
+    locked = false;
+    hartid = -1;
+    return;
+}
+
+spinlock_t::spinlock_t(const char *_name) : name(_name) {
+    locked = false;
+    hartid = COMMON::get_curr_core_id(CPU::READ_SP());
+    info("spinlock: %s init.\n", name);
+    return;
+}
+
+void spinlock_t::lock(void) {
+    push_off();
+    assert(is_holding() != true);
+
+    while (__atomic_test_and_set(&locked, 1) != 0) {
+        ;
+    }
+
+    __sync_synchronize();
+
+    hartid = COMMON::get_curr_core_id(CPU::READ_SP());
+    return;
+}
+
+// Release the lock.
+void spinlock_t::unlock(void) {
+    assert(is_holding() != false);
+    hartid = -1;
+    __sync_synchronize();
+    __sync_lock_release(&locked);
+    pop_off();
     return;
 }
