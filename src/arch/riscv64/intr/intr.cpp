33b7e33d8ce7ffb4c8f6dd85b6c11681dd119918
Zone.N <Zone.Niuzh@hotmail.com>
2022-01-06 17:19:59 +0800

    pref(INTR): update
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- src/arch/riscv64/intr/intr.cpp
+++ src/arch/riscv64/intr/intr.cpp
@@ -21,30 +21,6 @@
 #include "vmm.h"
 
 /**
- * @brief 缺页处理
- */
-static void pg_load_excp(void) {
-    uintptr_t addr = CPU::READ_STVAL();
-    // 映射页
-    VMM::get_instance().mmap(VMM::get_instance().get_pgd(), addr, addr,
-                             VMM_PAGE_READABLE);
-    info("pg_load_excp done: 0x%p.\n", addr);
-    return;
-}
-
-/**
- * @brief 缺页处理
- */
-static void pg_store_excp(void) {
-    uintptr_t addr = CPU::READ_STVAL();
-    // 映射页
-    VMM::get_instance().mmap(VMM::get_instance().get_pgd(), addr, addr,
-                             VMM_PAGE_WRITABLE | VMM_PAGE_READABLE);
-    info("pg_store_excp done: 0x%p.\n", addr);
-    return;
-}
-
-/**
  * @brief 中断处理函数
  * @param  _scause         原因
  * @param  _sepc           值
@@ -88,6 +64,10 @@ extern "C" void trap_handler(uint64_t _scause, uint64_t _sepc,
 
 /// 中断处理入口 intr_s.S
 extern "C" void trap_entry(void);
+/// 缺页中断处理函数 vmm.cpp
+extern void pg_load_excp(void);
+/// 缺页中断处理函数 vmm.cpp
+extern void pg_store_excp(void);
 
 /**
  * @brief 默认使用的中断处理函数
@@ -106,14 +86,14 @@ INTR &INTR::get_instance(void) {
 }
 
 int32_t INTR::init(void) {
-    // 内部中断初始化
-    CLINT::get_instance().init();
-    // 外部中断初始化
-    PLIC::get_instance().init();
     // 设置 trap vector
     CPU::WRITE_STVEC((uintptr_t)trap_entry);
     // 直接跳转到处理函数
     CPU::STVEC_DIRECT();
+    // 内部中断初始化
+    CLINT::get_instance().init();
+    // 外部中断初始化
+    PLIC::get_instance().init();
     // 设置处理函数
     for (auto &i : interrupt_handlers) {
         i = handler_default;
@@ -121,11 +101,10 @@ int32_t INTR::init(void) {
     for (auto &i : excp_handlers) {
         i = handler_default;
     }
-    // 缺页中断
+    // 注册缺页中断
     register_excp_handler(EXCP_LOAD_PAGE_FAULT, pg_load_excp);
+    // 注册缺页中断
     register_excp_handler(EXCP_STORE_PAGE_FAULT, pg_store_excp);
-    // 设置时钟中断
-    TIMER::get_instance().init();
     info("intr init.\n");
     return 0;
 }
