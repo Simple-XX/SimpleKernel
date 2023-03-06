68a57d3758f58884c14b59d3860241e92fe7e748
Zone.N <Zone.NiuZH@hotmail.com>
2023-03-06 09:10:12 +0800

    fix(cmake): update linux toolchain
    Signed-off-by: Zone.N <Zone.NiuZH@hotmail.com>

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
 
-# This file is a part of Simple-XX/SimpleKernel 
+# This file is a part of Simple-XX/SimpleKernel
 # (https://github.com/Simple-XX/SimpleKernel).
 #
 # run.sh for Simple-XX/SimpleKernel.
 # 在虚拟机中运行内核
 
-#!/bin/bash
 # shell 执行出错时终止运行
 set -e
 # 输出实际执行内容
