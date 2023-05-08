d4dbead3bbd0f6f05359d74ac5c6d065c2f25cd9
Zone.N <Zone.NiuZH@hotmail.com>
2023-05-09 07:37:23 +0800

    perf(fatfs): add codes
    Signed-off-by: Zone.N <Zone.NiuZH@hotmail.com>

--- src/fs/fatfs/include/ffconf.h
+++ src/fs/fatfs/include/ffconf.h
@@ -103,7 +103,7 @@
 / Locale and Namespace Configurations
 /---------------------------------------------------------------------------*/
 
-#define FF_CODE_PAGE	932
+#define FF_CODE_PAGE	936
 /* This option specifies the OEM code page to be used on the target system.
 /  Incorrect code page setting can cause a file open failure.
 /
