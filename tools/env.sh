2dd6cb1116f03431b057644ab2caab5cc68d4d61
KehRoche <kehroche@gmail.com>
2023-05-20 17:45:55 +0800

    aarch64
    Signed-off-by: KehRoche <kehroche@gmail.com>

--- tools/env.sh
+++ tools/env.sh
@@ -6,10 +6,9 @@
 # setup.sh for Simple-XX/SimpleKernel.
 # 指定要运行的 ARCH，并设置相关数据
 
-
 # 指定要编译的目标架构 ARCH: riscv64, x86_64, aarch64
-ARCH="aarch64"
-#ARCH="x86_64"
+#ARCH="aarch64"
+ARCH="x86_64"
 #ARCH="riscv64"
 
 DEBUG=0
@@ -17,26 +16,26 @@ DEBUG=0
 # 内核映像
 kernel='./build_'${ARCH}'/bin/kernel.elf'
 # 判断操作系统类型
-OS=`uname -s`
+OS=$(uname -s)
 # toolchain
 if [ "${OS}" == "Linux" ]; then
-    if [ "${ARCH}" == "i386" ] || [ "${ARCH}" == "x86_64" ]; then
-        TOOLS="x86_64-x86_64.cmake"
-    elif [ "${ARCH}" == "aarch64" ]; then
-        TOOLS="aarch64-x86_64.cmake"
-    elif [ "${ARCH}" == "riscv64" ]; then
-        TOOLS="toolchain_linux_riscv.cmake"
-        TOOLCHAIN_PREFIX=riscv64-linux-gnu-
-    fi
-    OPENSBI="$(pwd)/tools/opensbi/build/platform/generic/firmware/fw_jump.elf"
+  if [ "${ARCH}" == "i386" ] || [ "${ARCH}" == "x86_64" ]; then
+    TOOLS="x86_64-x86_64.cmake"
+  elif [ "${ARCH}" == "aarch64" ]; then
+    TOOLS="aarch64-x86_64.cmake"
+  elif [ "${ARCH}" == "riscv64" ]; then
+    TOOLS="toolchain_linux_riscv.cmake"
+    TOOLCHAIN_PREFIX=riscv64-linux-gnu-
+  fi
+  OPENSBI="$(pwd)/tools/opensbi/build/platform/generic/firmware/fw_jump.elf"
 elif [ "${OS}" == "Darwin" ]; then
-    if [ "${ARCH}" == "i386" ] || [ "${ARCH}" == "x86_64" ]; then
-        TOOLS="toolchain_mac_x86_64.cmake"
-    elif [ "${ARCH}" == "aarch64" ]; then
-        TOOLS="toolchain_mac_aarch64.cmake"
-    elif [ "${ARCH}" == "riscv64" ]; then
-        TOOLS="toolchain_mac_riscv.cmake"
-        TOOLCHAIN_PREFIX=riscv64-unknown-elf-
-    fi
-    OPENSBI="$(pwd)/tools/opensbi/build/platform/generic/firmware/fw_jump.elf"
+  if [ "${ARCH}" == "i386" ] || [ "${ARCH}" == "x86_64" ]; then
+    TOOLS="toolchain_mac_x86_64.cmake"
+  elif [ "${ARCH}" == "aarch64" ]; then
+    TOOLS="toolchain_mac_aarch64.cmake"
+  elif [ "${ARCH}" == "riscv64" ]; then
+    TOOLS="toolchain_mac_riscv.cmake"
+    TOOLCHAIN_PREFIX=riscv64-unknown-elf-
+  fi
+  OPENSBI="$(pwd)/tools/opensbi/build/platform/generic/firmware/fw_jump.elf"
 fi
