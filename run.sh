675736d59e715c7fb3353dd9d22672b5f1c1e4cf
KehRoche <kehroche@gmail.com>
2023-05-21 19:46:50 +0800

    cmake
    Signed-off-by: KehRoche <kehroche@gmail.com>

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This file is a part of Simple-XX/SimpleKernel
+# (https://github.com/Simple-XX/SimpleKernel).
+#
+# run.sh for Simple-XX/SimpleKernel.
+# 在虚拟机中运行内核
+
+# shell 执行出错时终止运行
+set -e
+# 输出实际执行内容
+#set -x
+
+#ARCH=riscv64
+ARCH=x86_64
+#ARCH=aarch64
+
+# 重新编译
+mkdir -p ./build_${ARCH}/
+rm -rf ./build_${ARCH}/*
+cd ./build_${ARCH}
+cmake -DARCH=${ARCH} ..
+make run
