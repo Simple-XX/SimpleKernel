d140557e17d181d02b8988c2d4d496ef2f332bd8
KehRoche <kehroche@gmail.com>
2023-07-05 21:49:26 +0800

    添加抽象
    Signed-off-by: KehRoche <kehroche@gmail.com>

--- cmake/header_files.cmake
+++ cmake/header_files.cmake
@@ -6,17 +6,17 @@
 # 将头文件路径添加到 Target 的搜索路径中
 
 function(target_include_arch_header_files Target)
-    target_include_directories(${Target} PRIVATE ${CMAKE_SOURCE_DIR}/arch/${SimpleKernelArch})
+    target_include_directories(${Target} PRIVATE ${ROOT_DIR}/src/arch/${SimpleKernelArch})
 endfunction()
 
 function(target_include_kernel_header_files Target)
-    target_include_directories(${Target} PRIVATE ${CMAKE_SOURCE_DIR}/kernel/include)
+    target_include_directories(${Target} PRIVATE ${ROOT_DIR}/src/kernel/include)
 endfunction()
 
 function(target_include_gnu_efi_header_files Target)
-    target_include_directories(${Target} PRIVATE ${CMAKE_SOURCE_DIR}/../3rd/gnu-efi/inc)
+    target_include_directories(${Target} PRIVATE ${ROOT_DIR}/3rd/gnu-efi/inc)
 endfunction()
 
 function(target_include_posix_uefi_header_files Target)
-    target_include_directories(${Target} PRIVATE ${CMAKE_SOURCE_DIR}/../../3rd/posix-uefi/uefi)
+    target_include_directories(${Target} PRIVATE ${ROOT_DIR}/3rd/posix-uefi/uefi)
 endfunction()
--- src/boot/x86_64/boot.cpp
+++ src/boot/x86_64/boot.cpp
@@ -18,8 +18,13 @@
 #    include "efi.h"
 #    include "efilib.h"
 #else
+#    include "elf.h"
 #    include "uefi.h"
 #endif
+
+#include "cstdint"
+
+#include "kernel.h"
 
 #if USE_GNU_UEFI == 1
 
@@ -45,52 +50,15 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE* systemTable) {
 
 #else
 
-/*** ELF64 defines and structs ***/
-#    define ELFMAG      "\177ELF"
-#    define SELFMAG     4
-#    define EI_CLASS    4  /* File class byte index */
-#    define ELFCLASS64  2  /* 64-bit objects */
-#    define EI_DATA     5  /* Data encoding byte index */
-#    define ELFDATA2LSB 1  /* 2's complement, little endian */
-#    define ET_EXEC     2  /* Executable file */
-#    define PT_LOAD     1  /* Loadable program segment */
 #    ifdef __x86_64__
-#        define EM_MACH 62 /* AMD x86-64 architecture */
+#        define EM_MACH EM_X86_64
 #    endif
 #    ifdef __aarch64__
-#        define EM_MACH 183 /* ARM aarch64 architecture */
+#        define EM_MACH EM_AARCH64
 #    endif
 
-typedef struct {
-    uint8_t  e_ident[16]; /* Magic number and other info */
-    uint16_t e_type;      /* Object file type */
-    uint16_t e_machine;   /* Architecture */
-    uint32_t e_version;   /* Object file version */
-    uint64_t e_entry;     /* Entry point virtual address */
-    uint64_t e_phoff;     /* Program header table file offset */
-    uint64_t e_shoff;     /* Section header table file offset */
-    uint32_t e_flags;     /* Processor-specific flags */
-    uint16_t e_ehsize;    /* ELF header size in bytes */
-    uint16_t e_phentsize; /* Program header table entry size */
-    uint16_t e_phnum;     /* Program header table entry count */
-    uint16_t e_shentsize; /* Section header table entry size */
-    uint16_t e_shnum;     /* Section header table entry count */
-    uint16_t e_shstrndx;  /* Section header string table index */
-} Elf64_Ehdr;
-
-typedef struct {
-    uint32_t p_type;   /* Segment type */
-    uint32_t p_flags;  /* Segment flags */
-    uint64_t p_offset; /* Segment file offset */
-    uint64_t p_vaddr;  /* Segment virtual address */
-    uint64_t p_paddr;  /* Segment physical address */
-    uint64_t p_filesz; /* Segment size in file */
-    uint64_t p_memsz;  /* Segment size in memory */
-    uint64_t p_align;  /* Segment alignment */
-} Elf64_Phdr;
-
 /**
- * Load an ELF executable
+ * @brief 进行系统初始化，加载内核
  */
 extern "C" int main(int argc, char** argv) {
     (void)argc;
@@ -154,8 +122,8 @@ extern "C" int main(int argc, char** argv) {
     /* execute the "kernel" */
     printf("ELF entry point %p\n", entry);
 
-    int (*entry_fp)(int, char**) = (int (*)(int, char**))entry;
-    i                            = entry_fp(0, nullptr);
+    entry_func entry_fp = (int (*)(int, char**))entry;
+    i                   = entry_fp(0, nullptr);
 
     printf("ELF returned %d\n", i);
 
