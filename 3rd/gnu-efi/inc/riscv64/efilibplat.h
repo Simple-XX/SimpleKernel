1ef607fb5589dc9e9a04bbdbe2b2d437202f2896
KehRoche <kehroche@gmail.com>
2023-09-10 09:02:36 +0800

    删除 3rd
    Signed-off-by: KehRoche <kehroche@gmail.com>

--- 3rd/gnu-efi/inc/riscv64/efilibplat.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0+ OR BSD-2-Clause */
-
-VOID
-InitializeLibPlatform (
-    IN EFI_HANDLE           ImageHandle,
-    IN EFI_SYSTEM_TABLE     *SystemTable
-    );
--- 3rd/gnu-efi/lib/arm/edk2asm.h
+++
@@ -1,6 +0,0 @@
-
-#define ASM_PFX(x)			x
-#define GCC_ASM_EXPORT(x)		\
-	.globl		x		; \
-	.type		x, %function
-
