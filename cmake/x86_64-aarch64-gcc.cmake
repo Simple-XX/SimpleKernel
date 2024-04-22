9fa4c0e2ba83b4f259c545d8f3a48910735fc137
Zone.N <zone.niuzh@hotmail.com>
2024-04-22 15:18:54 +0800

    移除兼容 osx 的相关代码
    Signed-off-by: Zone.N <zone.niuzh@hotmail.com>

--- cmake/x86_64-aarch64-gcc.cmake
+++ cmake/x86_64-aarch64-gcc.cmake
@@ -9,29 +9,9 @@ set(CMAKE_SYSTEM_NAME Generic)
 # 目标架构
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-# @todo mac 测试
 if (APPLE)
-    message(STATUS "Now is Apple system.")
-    # GCC
-    find_program(Compiler_gcc aarch64-linux-gnu-g++)
-    if (NOT Compiler_gcc)
-        message(FATAL_ERROR "aarch64-linux-gnu-g++ not found.\n"
-                "Run `brew install x86_64-elf-g++` to install.")
-    else ()
-        message(STATUS "Found aarch64-linux-gnu-g++ ${Compiler_gcc}")
-    endif ()
-
-    set(TOOLCHAIN_PREFIX aarch64-linux-gnu-)
-    set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
-    set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
-    set(CMAKE_READELF ${TOOLCHAIN_PREFIX}readelf)
-    set(CMAKE_AR ${TOOLCHAIN_PREFIX}ar)
-    set(CMAKE_LINKER ${TOOLCHAIN_PREFIX}ld)
-    set(CMAKE_NM ${TOOLCHAIN_PREFIX}nm)
-    set(CMAKE_OBJDUMP ${TOOLCHAIN_PREFIX}objdump)
-    set(CMAKE_RANLIB ${TOOLCHAIN_PREFIX}ranlib)
+    message(FATAL_ERROR "NOT support OSX, Please use linux")
 elseif (UNIX)
-    message(STATUS "Now is UNIX-like OS's.")
     # GCC
     find_program(Compiler_gcc aarch64-linux-gnu-g++)
     if (NOT Compiler_gcc)
--- cmake/x86_64-x86_64-gcc.cmake
+++ cmake/x86_64-x86_64-gcc.cmake
@@ -9,31 +9,8 @@ set(CMAKE_SYSTEM_NAME Generic)
 # 目标架构
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-# @todo mac 测试
 if (APPLE)
-    message(STATUS "Now is Apple system.")
-    # GCC
-    find_program(Compiler_gcc x86_64-elf-g++)
-    if (NOT Compiler_gcc)
-        message(FATAL_ERROR "x86_64-elf-g++ not found.\n"
-                "Run `brew install x86_64-elf-g++` to install.")
-    else ()
-        message(STATUS "Found x86_64-elf-g++ ${Compiler_gcc}")
-    endif ()
-
-    set(TOOLCHAIN_PREFIX x86_64-elf-)
-    set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
-    set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
-    set(CMAKE_READELF ${TOOLCHAIN_PREFIX}readelf)
-    set(CMAKE_AR ${TOOLCHAIN_PREFIX}ar)
-    set(CMAKE_LINKER ${TOOLCHAIN_PREFIX}ld)
-    set(CMAKE_NM ${TOOLCHAIN_PREFIX}nm)
-    set(CMAKE_OBJDUMP ${TOOLCHAIN_PREFIX}objdump)
-    set(CMAKE_RANLIB ${TOOLCHAIN_PREFIX}ranlib)
-
-    # 跳过编译器检查
-    set(CMAKE_C_COMPILER_FORCED TRUE)
-    set(CMAKE_CXX_COMPILER_FORCED TRUE)
+    message(FATAL_ERROR "NOT support OSX, Please use linux")
 elseif (UNIX)
     message(STATUS "Now is UNIX-like OS's.")
     # GCC
