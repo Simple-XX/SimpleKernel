68e8a3905e1934f61eb74a885019b0ecd5ee81ef
Zone.N <zone.niuzh@hotmail.com>
2024-05-31 11:35:19 +0800

    pref(intr)
    Signed-off-by: Zone.N <zone.niuzh@hotmail.com>

--- src/kernel/arch/riscv64/interrupt.cpp
+++ src/kernel/arch/riscv64/interrupt.cpp
@@ -20,9 +20,9 @@
 #include "fdt_parser.hpp"
 #include "opensbi_interface.h"
 
-Clint::InterruptFunc Clint::interrupt_handlers[Cpu::kInterruptMaxCount];
+Interrupt::InterruptFunc Interrupt::interrupt_handlers[Cpu::kInterruptMaxCount];
 
-Clint::InterruptFunc Clint::exception_handlers[Cpu::kExceptionMaxCount];
+Interrupt::InterruptFunc Interrupt::exception_handlers[Cpu::kExceptionMaxCount];
 
 static Interrupt interrupt __attribute__((init_priority(101)));
 
@@ -47,152 +47,37 @@ extern "C" void TrapHandler(uintptr_t sepc, uintptr_t stval, uintptr_t scause,
   // 消除 unused 警告
   (void)sepc;
   (void)stval;
-  (void)all_regs;
   (void)sie;
   (void)sstatus;
   (void)satp;
   (void)sscratch;
+  printf(
+      "sepc: 0x%p, stval: 0x%p, scause: 0x%p, all_regs(sp): 0x%p, sie: "
+      "0x%p\nsstatus: ",
+      sepc, stval, scause, all_regs, sie);
+  // std::cout << sstatus << ", \nsatp: " << satp << ", \n";
+  // printf("sscratch: 0x%p\n", sscratch);
   // 跳转到对应的处理函数
-  interrupt.clint.Do(scause, nullptr);
+  interrupt.Do(scause, (uint8_t *)all_regs);
 }
 
-Clint::Clint() {
-  if (is_inited == false) {  // 注册默认中断处理函数
+Interrupt::Interrupt() {
+  if (is_inited == false) {
+    // 注册默认中断处理函数
     for (auto &i : interrupt_handlers) {
-      i = [](uint32_t argc, uint8_t *argv) -> uint32_t {
-        printf("Default Interrupt Handler: %d, 0x%p\n", argc, argv);
+      i = [](uint64_t cause, uint8_t *context) -> uint64_t {
+        printf("[%s] %d, 0x%p\n", Cpu::kInterruptNames[cause], cause, context);
         return 0;
       };
     }
     // 注册默认异常处理函数
     for (auto &i : exception_handlers) {
-      i = [](uint32_t argc, uint8_t *argv) -> uint32_t {
-        printf("Default Exception Handler: %d, 0x%p\n", argc, argv);
+      i = [](uint64_t cause, uint8_t *context) -> uint64_t {
+        printf("[%s] %d, 0x%p\n", Cpu::kInterruptNames[cause], cause, context);
         return 0;
       };
     }
-    is_inited = true;
-  }
-  // 开启内部中断
-  Cpu::WriteSie(Cpu::ReadSie() | Cpu::kSieSsie);
-  printf("Clint init.\n");
-}
 
-uint32_t Clint::Do(uint32_t scause, uint8_t *context) {
-  (void)context;
-  auto is_interrupt = scause & Cpu::kCauseInterruptMask;
-  auto cause = scause & Cpu::kCauseCodeMask;
-  if (is_interrupt) {
-    // 中断
-    Interrupt(cause, nullptr);
-  } else {
-    // 异常
-    // 跳转到对应的处理函数
-    Exception(cause, nullptr);
-  }
-  return 0;
-}
-
-void Clint::RegisterInterruptFunc(uint32_t intr_no, InterruptFunc func) {
-  printf("[%s] %d, 0x%p\n", Cpu::kInterruptNames[intr_no], intr_no, func);
-}
-
-uint32_t Clint::Interrupt(uint32_t cause, uint8_t *context) {
-  printf("[%s] %d, 0x%p\n", Cpu::kInterruptNames[cause], cause, context);
-  return 0;
-}
-
-uint32_t Clint::Exception(uint32_t cause, uint8_t *context) {
-  printf("[%s] %d, 0x%p\n", Cpu::kInterruptNames[cause], cause, context);
-  return 0;
-}
-
-// /**
-//  * @brief 外部中断处理
-//  */
-// static int32_t external_intr(int, char **) {
-//   // 读取中断号
-//   auto no = PLIC::get_instance().get();
-//   // 根据中断号判断设备
-//   printf("external_intr: 0x%X.\n", no);
-//   return 0;
-// }
-
-// PLIC::PLIC() {
-//   // 映射 plic
-//   resource_t resource = BOOT_INFO::get_plic();
-//   base_addr = resource.mem.addr;
-//   PLIC_PRIORITY = base_addr + 0x0;
-//   PLIC_PENDING = base_addr + 0x1000;
-//   for (uintptr_t a = resource.mem.addr;
-//        a < resource.mem.addr + resource.mem.len; a += COMMON::PAGE_SIZE) {
-//     VMM::get_instance().mmap(VMM::get_instance().get_pgd(), a, a,
-//                              VMM_PAGE_READABLE | VMM_PAGE_WRITABLE);
-//   }
-//   // TODO: 多核情况下设置所有 hart
-//   // 将当前 hart 的 S 模式优先级阈值设置为 0
-//   IO::get_instance().write32((void *)PLIC_SPRIORITY(CPU::get_curr_core_id()),
-//                              0);
-//   // 注册外部中断处理函数
-//   INTR::get_instance().register_interrupt_handler(CPU::INTR_EXTERN_S,
-//                                                   external_intr);
-//   // 开启外部中断
-//   CPU::WRITE_SIE(CPU::READ_SIE() | CPU::SIE_SEIE);
-//   info("plic init.\n");
-//   return 0;
-// }
-
-// void PLIC::set(uint8_t _no, bool _status) {
-//   spinlock.lock();
-//   // 设置 IRQ 的属性为非零，即启用 plic
-//   IO::get_instance().write32((void *)(base_addr + _no * 4), _status);
-//   // TODO: 多核情况下设置所有 hart
-//   // 为当前 hart 的 S 模式设置 uart 的 enable
-//   if (_status) {
-//     IO::get_instance().write32((void *)PLIC_SENABLE(CPU::get_curr_core_id()),
-//                                IO::get_instance().read32((void
-//                                *)PLIC_SENABLE(
-//                                    CPU::get_curr_core_id())) |
-//                                    (1 << _no));
-//   } else {
-//     IO::get_instance().write32((void *)PLIC_SENABLE(CPU::get_curr_core_id()),
-//                                IO::get_instance().read32((void
-//                                *)PLIC_SENABLE(
-//                                    CPU::get_curr_core_id())) &
-//                                    ~(1 << _no));
-//   }
-//   spinlock.unlock();
-//   return;
-// }
-
-// uint8_t PLIC::get(void) {
-//   spinlock.lock();
-//   uint8_t ret =
-//       IO::get_instance().read32((void
-//       *)PLIC_SCLAIM(CPU::get_curr_core_id()));
-//   spinlock.unlock();
-//   return ret;
-// }
-
-// void PLIC::done(uint8_t _no) {
-//   spinlock.lock();
-//   IO::get_instance().write32((void *)PLIC_SCLAIM(CPU::get_curr_core_id()),
-//   _no); spinlock.unlock(); return;
-// }
-
-// void PLIC::register_externel_handler(
-//     uint8_t _no, externel_interrupt_handler_t _interrupt_handler) {
-//   externel_interrupt_handlers[_no] = _interrupt_handler;
-//   return;
-// }
-
-// void PLIC::do_externel_interrupt(uint8_t _no) {
-//   externel_interrupt_handlers[_no](_no);
-//   return;
-// }
-
-Interrupt::Interrupt() {
-  if (is_inited == false) {
     // 设置 trap vector
     Cpu::WriteStvec((uintptr_t)TrapEntry);
 
@@ -208,18 +93,45 @@ Interrupt::Interrupt() {
     is_inited = true;
   }
 
-  // 初始化本地中断
-  Clint();
+  // 开启内部中断
+  Cpu::WriteSie(Cpu::ReadSie() | Cpu::kSieSsie);
   printf("Interrupt init.\n");
 }
 
-uint32_t Interrupt::Do(uint32_t intr_no, uint8_t *context) {
-  printf("[%s] %d, 0x%p\n", Cpu::kInterruptNames[intr_no], intr_no, context);
-  return 0;
+void Interrupt::Do(uint64_t scause, uint8_t *context) {
+  auto is_interrupt = scause & Cpu::kCauseInterruptMask;
+  auto cause = scause & Cpu::kCauseCodeMask;
+  if (is_interrupt) {
+    // 中断
+    DoInterrupt(cause, context);
+  } else {
+    // 异常
+    DoException(cause, context);
+  }
 }
 
-void Interrupt::RegisterInterruptFunc(uint32_t intr_no, InterruptFunc func) {
-  printf("[%s] %d, 0x%p\n", Cpu::kInterruptNames[intr_no], intr_no, func);
+void Interrupt::RegisterInterruptFunc(uint64_t scause, InterruptFunc func) {
+  auto is_interrupt = scause & Cpu::kCauseInterruptMask;
+  auto cause = scause & Cpu::kCauseCodeMask;
+  if (is_interrupt) {
+    if (cause < Cpu::kInterruptMaxCount) {
+      interrupt_handlers[cause] = func;
+      printf("[%s] %d, 0x%p\n", Cpu::kInterruptNames[cause], cause, func);
+    }
+  } else {
+    if (cause < Cpu::kExceptionMaxCount) {
+      exception_handlers[cause] = func;
+      printf("[%s] %d, 0x%p\n", Cpu::kExceptionNames[cause], cause, func);
+    }
+  }
+}
+
+void Interrupt::DoInterrupt(uint64_t cause, uint8_t *context) {
+  interrupt_handlers[cause](cause, context);
+}
+
+void Interrupt::DoException(uint64_t cause, uint8_t *context) {
+  exception_handlers[cause](cause, context);
 }
 
 // 在 riscv64 情景下，argc 为启动核 id，argv 为 dtb 地址
@@ -227,13 +139,18 @@ uint32_t IntrInit(uint32_t argc, uint8_t *argv) {
   printf("boot hart id: %d\n", argc);
   printf("dtb info addr: %p\n", argv);
 
-  auto dtb_info = FDT_PARSER::fdt_parser((uintptr_t)argv);
-
-  auto resource_mem = FDT_PARSER::resource_t();
-  dtb_info.find_via_prefix("serial@", &resource_mem);
+  // 注册时钟中断
+  interrupt.RegisterInterruptFunc(
+      Cpu::kIntrTimerSuperMode | Cpu::kCauseCodeMask,
+      [](uint64_t, uint8_t *) -> uint64_t {
+        printf("sss\n");
+        while (1);
+        sbi_set_timer(10000000);
+        return 0;
+      });
 
   // 设置时钟中断时间
-  sbi_set_timer(1000);
+  sbi_set_timer(10000000);
 
   printf("hello IntrInit\n");
 
--- src/kernel/arch/riscv64/interrupt.h
+++ src/kernel/arch/riscv64/interrupt.h
@@ -23,112 +23,8 @@
 #include "interrupt_base.h"
 #include "stdio.h"
 
-/**
- * @brief core-local interrupt controller
- * 本地核心中断控制器
- * 用于控制 excp 与 intr
- */
-class Clint final : public InterruptBase {
- public:
-  Clint();
-
-  /// @name 构造/析构函数
-  /// @{
-  Clint(const Clint &) = delete;
-  Clint(Clint &&) = delete;
-  auto operator=(const Clint &) -> Clint & = delete;
-  auto operator=(Clint &&) -> Clint & = delete;
-  ~Clint() = default;
-  /// @}
-
-  /**
-   * @brief 执行中断处理
-   * @param  cause 中断或异常号
-   * @param  context 中断上下文
-   * @return uint32_t 返回值，0 成功
-   */
-  uint32_t Do(uint32_t cause, uint8_t *context) override;
-
-  void RegisterInterruptFunc(uint32_t intr_no, InterruptFunc func) override;
-
- private:
-  /// 中断处理函数数组
-  static InterruptFunc interrupt_handlers[Cpu::kInterruptMaxCount]
-      __attribute__((aligned(4)));
-  /// 异常处理函数数组
-  static InterruptFunc exception_handlers[Cpu::kExceptionMaxCount]
-      __attribute__((aligned(4)));
-  uint32_t Interrupt(uint32_t, uint8_t *);
-  uint32_t Exception(uint32_t, uint8_t *);
-};
-
-// /**
-//  * @brief platform-level interrupt controller
-//  * 平台级中断控制器
-//  * 用于控制外部中断
-//  */
-// class PLIC {
-//  public:
-//   PLIC();
-
-//   /// @name 构造/析构函数
-//   /// @{
-//   PLIC(const PLIC &) = delete;
-//   PLIC(PLIC &&) = delete;
-//   auto operator=(const PLIC &) -> PLIC & = delete;
-//   auto operator=(PLIC &&) -> PLIC & = delete;
-//   ~PLIC() = default;
-//   /// @}
-
-//   /**
-//    * @brief 向 PLIC 询问中断
-//    * 返回发生的外部中断号
-//    * @return uint8_t         中断号
-//    */
-//   uint8_t get(void);
-
-//   /**
-//    * @brief 告知 PLIC 已经处理了当前 IRQ
-//    * @param  _no             中断号
-//    */
-//   void done(uint8_t _no);
-
-//   /**
-//    * @brief 设置中断状态
-//    * @param  _no             中断号
-//    * @param  _status         状态
-//    * @todo 不确定
-//    */
-//   void set(uint8_t _no, bool _status);
-
-//   /**
-//    * @brief 注册外部中断处理函数
-//    * @param  _no             外部中断号
-//    * @param  _interrupt_handler 外部中断处理函数
-//    */
-//   void register_externel_handler(
-//       uint8_t _no, externel_interrupt_handler_t _interrupt_handler);
-
-//   /**
-//    * @brief 执行外部中断处理
-//    * @param  _no              外部中断号
-//    */
-//   void do_externel_interrupt(uint8_t _no);
-
-//  private:
-//   /// 外部中断处理函数指针
-//   typedef void (*externel_interrupt_handler_t)(uint8_t _no);
-//   /// 最大外部中断数量
-//   static constexpr const size_t EXTERNEL_INTERRUPR_MAX = 16;
-//   /// 外部中断处理函数数组
-//   externel_interrupt_handler_t
-//       externel_interrupt_handlers[EXTERNEL_INTERRUPR_MAX];
-// };
-
 class Interrupt final : public InterruptBase {
  public:
-  Clint clint;
-
   Interrupt();
 
   /// @name 构造/析构函数
@@ -144,13 +40,26 @@ class Interrupt final : public InterruptBase {
    * @brief 执行中断处理
    * @param  cause 中断或异常号
    * @param  context 中断上下文
-   * @return uint32_t 返回值，0 成功
    */
-  uint32_t Do(uint32_t cause, uint8_t *context) override;
+  void Do(uint64_t cause, uint8_t *context) override;
 
-  void RegisterInterruptFunc(uint32_t intr_no, InterruptFunc func) override;
+  /**
+   * @brief 注册中断处理函数
+   * @param scause 中断原因
+   * @param func 处理函数
+   */
+  void RegisterInterruptFunc(uint64_t scause, InterruptFunc func) override;
 
  private:
+  /// 中断处理函数数组
+  static InterruptFunc interrupt_handlers[Cpu::kInterruptMaxCount]
+      __attribute__((aligned(4)));
+  /// 异常处理函数数组
+  static InterruptFunc exception_handlers[Cpu::kExceptionMaxCount]
+      __attribute__((aligned(4)));
+
+  void DoInterrupt(uint64_t, uint8_t *);
+  void DoException(uint64_t, uint8_t *);
 };
 
 #endif /* SIMPLEKERNEL_SRC_KERNEL_INCLUDE_INTERRUPT_H_ */
