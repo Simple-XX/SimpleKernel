4df49c521ad6984277238bd3323f1752f5660d81
Zone.N <zone.niuzh@hotmail.com>
2024-05-31 15:30:25 +0800

    merge boot
    Signed-off-by: Zone.N <zone.niuzh@hotmail.com>

---
+++ src/kernel/libcxx/iostream.cpp
@@ -0,0 +1,43 @@
+
+/**
+ * @file iostream
+ * @brief C++ 输入输出
+ * @author Zone.N (Zone.Niuzh@hotmail.com)
+ * @version 1.0
+ * @date 2021-09-18
+ * @copyright MIT LICENSE
+ * https://github.com/Simple-XX/SimpleKernel
+ * Based on https://github.com/MRNIU/MiniCRT
+ * @par change log:
+ * <table>
+ * <tr><th>Date<th>Author<th>Description
+ * <tr><td>2021-09-18<td>digmouse233<td>迁移到 doxygen
+ * </table>
+ */
+
+#include "iostream"
+
+#include "cstdio"
+
+namespace std {
+
+ostream& ostream::operator<<(char c) {
+  printf("%c", c);
+  return *this;
+}
+
+ostream& ostream::operator<<(int n) {
+  printf("%d", n);
+  return *this;
+}
+
+ostream& ostream::operator<<(const char* lhs) {
+  printf("%s", lhs);
+  return *this;
+}
+
+ostream& ostream::operator<<(ostream& (*manip)(ostream&)) {
+  return manip(*this);
+}
+
+};  // namespace std
