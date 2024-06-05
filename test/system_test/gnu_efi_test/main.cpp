73d4e48aafbad8c8f36a045f7872b6a55385ed32
Zone.N <Zone.Niuzh@hotmail.com>
2024-06-05 10:49:31 +0800

    test: add ctor/dtor test
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

--- test/system_test/gnu_efi_test/main.cpp
+++ test/system_test/gnu_efi_test/main.cpp
@@ -21,19 +21,44 @@
 #include "cstdio"
 #include "kernel.h"
 
-void _start(uint32_t argc, uint8_t *argv) {
-  main(argc, argv);
-
-  // 进入死循环
-  while (1) {
-    ;
-  }
-}
-
 extern "C" void _putchar(char character) {
   auto serial = cpu::Serial(cpu::kCom1);
   serial.Write(character);
 }
+
+template <uint32_t V>
+class TestStaticConstructDestruct {
+ public:
+  TestStaticConstructDestruct(unsigned int &v) : _v(v) { _v |= V; }
+  ~TestStaticConstructDestruct() { _v &= ~V; }
+
+ private:
+  unsigned int &_v;
+};
+
+static int global_value_with_init = 42;
+static uint32_t global_u32_value_with_init{0xa1a2a3a4ul};
+static uint64_t global_u64_value_with_init{0xb1b2b3b4b5b6b7b8ull};
+static float global_f32_value_with_init{3.14};
+static double global_f64_value_with_init{1.44};
+static uint16_t global_u16_value_with_init{0x1234};
+static uint8_t global_u8a_value_with_init{0x42};
+static uint8_t global_u8b_value_with_init{0x43};
+static uint8_t global_u8c_value_with_init{0x44};
+static uint8_t global_u8d_value_with_init{0x45};
+static volatile bool global_bool_keep_running{true};
+
+static unsigned int global_value1_with_constructor = 1;
+static unsigned int global_value2_with_constructor = 2;
+
+static TestStaticConstructDestruct<0x200> constructor_destructor_1(
+    global_value1_with_constructor);
+static TestStaticConstructDestruct<0x200> constructor_destructor_2(
+    global_value2_with_constructor);
+static TestStaticConstructDestruct<0x100000> constructor_destructor_3{
+    global_value2_with_constructor};
+static TestStaticConstructDestruct<0x100000> constructor_destructor_4{
+    global_value1_with_constructor};
 
 static const int kPixelwidth = 4;
 static const int kPitch = 800 * kPixelwidth;
@@ -55,13 +80,22 @@ static void Fillrect(uint8_t *vram, uint8_t r, uint8_t g, unsigned char b,
 }
 
 uint32_t main(uint32_t argc, uint8_t *argv) {
-  (void)argc;
-  (void)argv;
-
   if (argc != 1) {
     printf("argc != 1 [%d]\n", argc);
     return -1;
   }
+
+  global_u8c_value_with_init++;
+  global_u32_value_with_init++;
+  global_u64_value_with_init++;
+  global_f32_value_with_init++;
+  global_u8b_value_with_init++;
+  global_f64_value_with_init++;
+  global_u8d_value_with_init++;
+  global_u16_value_with_init++;
+  global_u8a_value_with_init++;
+  global_value_with_init++;
+  global_bool_keep_running = false;
 
   BootInfo boot_info = *reinterpret_cast<BootInfo *>(argv[0]);
   printf("boot_info.framebuffer.base: 0x%X\n", boot_info.framebuffer.base);
@@ -73,4 +107,15 @@ uint32_t main(uint32_t argc, uint8_t *argv) {
     ;
   }
   return 0;
+}
+
+extern "C" void _start(uint32_t argc, uint8_t *argv) {
+  CppInit();
+  main(argc, argv);
+  CppDeInit();
+
+  // 进入死循环
+  while (1) {
+    ;
+  }
 }
