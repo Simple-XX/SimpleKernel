76ee98eed0ff9f122ed003cedb3c122c80277644
Zone.N <zone.niuzh@hotmail.com>
2024-06-05 13:04:24 +0800

    fix(test): gnu-efi for x86_64
    Signed-off-by: Zone.N <zone.niuzh@hotmail.com>

--- test/system_test/gnu_efi_test/main.cpp
+++ test/system_test/gnu_efi_test/main.cpp
@@ -20,6 +20,7 @@
 #include "cpu.hpp"
 #include "cstdio"
 #include "kernel.h"
+#include "libcxx.h"
 
 extern "C" void _putchar(char character) {
   auto serial = cpu::Serial(cpu::kCom1);
@@ -48,17 +49,17 @@ static uint8_t global_u8c_value_with_init{0x44};
 static uint8_t global_u8d_value_with_init{0x45};
 static volatile bool global_bool_keep_running{true};
 
-static unsigned int global_value1_with_constructor = 1;
-static unsigned int global_value2_with_constructor = 2;
+// static unsigned int global_value1_with_constructor = 1;
+// static unsigned int global_value2_with_constructor = 2;
 
-static TestStaticConstructDestruct<0x200> constructor_destructor_1(
-    global_value1_with_constructor);
-static TestStaticConstructDestruct<0x200> constructor_destructor_2(
-    global_value2_with_constructor);
-static TestStaticConstructDestruct<0x100000> constructor_destructor_3{
-    global_value2_with_constructor};
-static TestStaticConstructDestruct<0x100000> constructor_destructor_4{
-    global_value1_with_constructor};
+// static TestStaticConstructDestruct<0x200> constructor_destructor_1(
+// global_value1_with_constructor);
+// static TestStaticConstructDestruct<0x200> constructor_destructor_2(
+//     global_value2_with_constructor);
+// static TestStaticConstructDestruct<0x100000> constructor_destructor_3{
+//     global_value2_with_constructor};
+// static TestStaticConstructDestruct<0x100000> constructor_destructor_4{
+//     global_value1_with_constructor};
 
 static const int kPixelwidth = 4;
 static const int kPitch = 800 * kPixelwidth;
@@ -97,7 +98,7 @@ uint32_t main(uint32_t argc, uint8_t *argv) {
   global_value_with_init++;
   global_bool_keep_running = false;
 
-  BootInfo boot_info = *reinterpret_cast<BootInfo *>(argv[0]);
+  BootInfo boot_info = *reinterpret_cast<BootInfo *>(argv);
   printf("boot_info.framebuffer.base: 0x%X\n", boot_info.framebuffer.base);
   Fillrect((uint8_t *)boot_info.framebuffer.base, 255, 0, 255, 100, 100);
   printf("Hello Test\n");
