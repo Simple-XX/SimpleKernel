70e425ddd113677d046e8b818941521626dde62c
Zone.N <zone.niuzh@hotmail.com>
2024-06-11 15:08:42 +0800

    feat(libcxx): __cxa_atexit, __cxa_finalize
    Signed-off-by: Zone.N <zone.niuzh@hotmail.com>

--- src/kernel/libcxx/libcxx.cpp
+++ src/kernel/libcxx/libcxx.cpp
@@ -18,23 +18,92 @@
 
 #include <algorithm>
 
+/// 全局构造函数函数指针
 typedef void (*function_t)(void);
 // 在 link.ld 中定义
+/// 全局构造函数函数指针起点地址
 extern "C" function_t __init_array_start;
+/// 全局构造函数函数指针终点地址
 extern "C" function_t __init_array_end;
+/// 全局析构函数函数指针起点地址
 extern "C" function_t __fini_array_start;
+/// 全局析构函数函数指针终点地址
 extern "C" function_t __fini_array_end;
+/// 动态共享对象标识，内核使用静态链接，此变量在内核中没有使用
+void* __dso_handle = nullptr;
 
-extern "C" int __cxa_atexit(void (*)(void *), void *, void *) { return 0; }
+/// 最大析构函数数量
+static constexpr const size_t kMaxAtExitFuncsCount = 128;
 
-void *__dso_handle = &__dso_handle;
+/// 析构函数结构
+struct atexit_func_entry_t {
+  // 析构函数指针
+  void (*destructor_func)(void*);
+  // 要析构的对象
+  void* obj_ptr;
+  // 共享库对象，在内核中没有使用
+  void* dso_handle;
+};
 
+/// 析构函数数组
+static atexit_func_entry_t __atexit_funcs[kMaxAtExitFuncsCount];
+/// 析构函数个数
+static size_t __atexit_func_count = 0;
+
+/**
+ * 注册在程序正常终止时调用的析构函数
+ * @param destructor_func 指向要调用的析构函数的指针
+ * @param obj_ptr 传递给析构函数的参数
+ * @return 成功返回 0
+ */
+extern "C" int __cxa_atexit(void (*destructor_func)(void*), void* obj_ptr,
+                            void*) {
+  if (__atexit_func_count >= kMaxAtExitFuncsCount) {
+    return -1;
+  }
+  __atexit_funcs[__atexit_func_count].destructor_func = destructor_func;
+  __atexit_funcs[__atexit_func_count].obj_ptr = obj_ptr;
+  __atexit_funcs[__atexit_func_count].dso_handle = nullptr;
+  __atexit_func_count++;
+  return 0;
+}
+
+/**
+ * 调用析构函数
+ * @param destructor_func 要调用的析构函数指针，为 nullptr
+ * 时调用所有注册的析构函数。
+ */
+extern "C" void __cxa_finalize(void* destructor_func) {
+  if (!destructor_func) {
+    // 如果 destructor_func 为 nullptr，调用所有析构函数
+    for (auto i = __atexit_func_count - 1; i != 0; i--) {
+      if (__atexit_funcs[i].destructor_func) {
+        (*__atexit_funcs[i].destructor_func)(__atexit_funcs[i].obj_ptr);
+      }
+    }
+  } else {
+    // 不为空时只调用对应的析构函数
+    for (auto i = __atexit_func_count - 1; i != 0; i--) {
+      if ((void*)__atexit_funcs[i].destructor_func == destructor_func) {
+        (*__atexit_funcs[i].destructor_func)(__atexit_funcs[i].obj_ptr);
+        __atexit_funcs[i].destructor_func = nullptr;
+      }
+    }
+  }
+}
+
+/**
+ * c++ 全局对象构造
+ */
 void CppInit(void) {
   // 调用构造函数
   std::for_each(&__init_array_start, &__init_array_end,
                 [](function_t func) { (func)(); });
 }
 
+/**
+ * c++ 全局对象析构
+ */
 void CppDeInit(void) {
   // 调用析构函数
   std::for_each(&__fini_array_start, &__fini_array_end,
