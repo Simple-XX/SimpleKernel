8af08a75f324eac01d9b7ef93c426232efd9e324
Zone.N <zone.niuzh@hotmail.com>
2024-09-11 05:12:29 +0000

    fix: overflow
    Signed-off-by: Zone.N <zone.niuzh@hotmail.com>

--- src/kernel/arch/riscv64/include/cpu/regs.hpp
+++ src/kernel/arch/riscv64/include/cpu/regs.hpp
@@ -1234,10 +1234,10 @@ class ReadWriteField : public ReadOnlyField<Reg, RegInfo>,
    * @param value 新值
    */
   static __always_inline void Write(RegInfo::DataType value) {
-    uint64_t org_value = Reg::Read();
-    uint64_t new_value =
-        (org_value & ~RegInfo::kBitMask) |
-        (((uint64_t)value << RegInfo::kBitOffset) & RegInfo::kBitMask);
+    auto org_value = Reg::Read();
+    auto new_value = (org_value & ~RegInfo::kBitMask) |
+                     (((decltype(org_value))value << RegInfo::kBitOffset) &
+                      RegInfo::kBitMask);
     Reg::Write(new_value);
   }
 
@@ -1247,10 +1247,10 @@ class ReadWriteField : public ReadOnlyField<Reg, RegInfo>,
    * @return RegInfo::DataType 由寄存器规定的数据类型
    */
   static __always_inline RegInfo::DataType ReadWrite(RegInfo::DataType value) {
-    uint64_t org_value = Reg::Read();
-    uint64_t new_value =
-        (org_value & ~RegInfo::kBitMask) |
-        (((uint64_t)value << RegInfo::kBitOffset) & RegInfo::kBitMask);
+    auto org_value = Reg::Read();
+    auto new_value = (org_value & ~RegInfo::kBitMask) |
+                     (((decltype(org_value))value << RegInfo::kBitOffset) &
+                      RegInfo::kBitMask);
     Reg::Write(new_value);
     return (RegInfo::DataType)((org_value & RegInfo::kBitMask) >>
                                RegInfo::kBitOffset);
